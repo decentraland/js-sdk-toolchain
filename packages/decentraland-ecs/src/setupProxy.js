284a6fae67f9e7800f562531373253d5edbf8fb5
Leandro Mendoza <leandro@decentraland.org>
2021-08-05 09:07:32 -0300

    fix: find ecsPath in relative path from setupProxy.js

--- packages/decentraland-ecs/src/setupProxy.js
+++ packages/decentraland-ecs/src/setupProxy.js
@@ -16,7 +16,7 @@ module.exports = function (dcl, app, express) {
    * 5. link kernel using `npm link @dcll/kernel` this will use the folder from step 1
    */
 
-  const ecsPath = path.dirname(require.resolve('decentraland-ecs/package.json', { paths: [dcl.getWorkingDir()] }))
+  const ecsPath = path.dirname(require.resolve('decentraland-ecs/package.json', { paths: [dcl.getWorkingDir(), __dirname + '/../../'] }))
   const dclKernelPath = path.dirname(require.resolve('@dcl/kernel/package.json', { paths: [dcl.getWorkingDir(), ecsPath] }))
   const dclKernelDefaultProfilePath = path.resolve(dclKernelPath, 'default-profile')
   const dclKernelImagesDecentralandConnect = path.resolve(dclKernelPath, 'images', 'decentraland-connect')
