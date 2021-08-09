a2682c634c92add891516cd6a161e284183c5652
Leandro Mendoza <leandro@decentraland.org>
2021-08-09 11:35:40 -0300

    add test to setup export

--- test/decentraland-ecs/setup-export.spec.ts
+++ test/decentraland-ecs/setup-export.spec.ts
@@ -1,10 +1,37 @@
-import { resolve } from 'path'
+import * as path from 'path'
+import * as fs from 'fs'
+import { ensureFileExists } from '../../scripts/helpers'
 
-const ecsLocation = resolve(__dirname, '../../packages/decentraland-ecs')
+const ecsLocation = path.resolve(__dirname, '../../packages/decentraland-ecs')
 
 describe('decentraland-ecs: setupExport.js', () => {
   const setupExport = require(`${ecsLocation}/src/setupExport.js`)
-  test(`require and call successfully`, async () => {
-    await setupExport('', '', {}, {})
+  test(`export fake project`, async () => {
+    const workDir = path.resolve(__dirname, 'simple-scene')
+    const exportDir = path.resolve(workDir, 'export')
+    const mappings: string[] = []
+    const sceneJson = { display: { title: 'test' } }
+
+    if (fs.existsSync(workDir)) {
+      await fs.promises.rmdir(workDir, { recursive: true })
+    }
+
+    await fs.promises.mkdir(workDir)
+    await fs.promises.mkdir(exportDir)
+
+    await setupExport({
+      workDir,
+      exportDir,
+      mappings,
+      sceneJson
+    })
+
+    ensureFileExists('index.js', exportDir)
+    ensureFileExists('index.html', exportDir)
+    ensureFileExists('unity-renderer/unity.wasm', exportDir)
+    ensureFileExists('unity-renderer/unity.data', exportDir)
+    ensureFileExists('unity-renderer/unity.framework.js', exportDir)
+    ensureFileExists('unity-renderer/unity.loader.js', exportDir)
+    ensureFileExists('unity-renderer/index.js', exportDir)
   })
 })
