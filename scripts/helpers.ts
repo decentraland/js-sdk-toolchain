a462b6ad803590c4e4ee36e2a7f6eda0e0a8264f
Leandro Mendoza <leandro@decentraland.org>
2021-08-12 14:33:36 -0300

    recover missing await

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -33,7 +33,7 @@ export function runCommand(command: string, cwd: string, env?: Record<string, st
 }
 
 export function itExecutes(command: string, cwd: string, env?: Record<string, string>) {
-  it(command, async () => runCommand(command, cwd, env), 30000)
+  it(command, async () => await runCommand(command, cwd, env), 30000)
 }
 
 export function itDeletesFolder(folder: string, cwd: string) {
