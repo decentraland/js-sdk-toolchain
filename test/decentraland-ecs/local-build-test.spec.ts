d18e131adfbe777cd2e425ef02a7053f9290f621
Leandro Mendoza <leandro@decentraland.org>
2021-08-12 14:23:59 -0300

    execute command without testing

--- test/decentraland-ecs/local-build-test.spec.ts
+++ test/decentraland-ecs/local-build-test.spec.ts
@@ -1,27 +1,35 @@
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
-import { itExecutes, ensureFileExists, itDeletesFolder } from '../../scripts/helpers'
+import { runCommand, itExecutes, ensureFileExists, itDeletesFolder } from '../../scripts/helpers'
 
-describe('setup local build of decentraland-ecs package', () => {
+describe('setup local build of decentraland-ecs package', async () => {
   const workingDir = resolve(__dirname, '..', '..')
   const generatedEcsTgzName = 'decentraland-ecs-0.0.0.tgz'
   const generatedEcsTgzPath = resolve(workingDir, 'packages', 'decentraland-ecs', generatedEcsTgzName)
 
-  itExecutes('make prepare-pr', workingDir)
-
-  it('ensure files exist', () => {
-    ensureFileExists(generatedEcsTgzPath)
-  })
+  await runCommand('make prepare-pr', workingDir)
 
   describe('install generated package', () => {
+    expect(ensureFileExists(generatedEcsTgzPath)).toBe(true)
+
     const testPackagePath = resolve(__dirname, `ecs-test-scene-${new Date().getTime()}`)
     const testCommands = [
-        'mkdir',testPackagePath, '&&',
-        'cd', testPackagePath, '&&',
-        'cp',generatedEcsTgzPath,'./', '&&',
-        'tar -xvf', generatedEcsTgzName, '&&',
-        'npm init --yes && npm install ./package &&',
-        'cd .. && rm -r', testPackagePath
+      'mkdir',
+      testPackagePath,
+      '&&',
+      'cd',
+      testPackagePath,
+      '&&',
+      'cp',
+      generatedEcsTgzPath,
+      './',
+      '&&',
+      'tar -xvf',
+      generatedEcsTgzName,
+      '&&',
+      'npm init --yes && npm install ./package &&',
+      'cd .. && rm -r',
+      testPackagePath
     ]
     itExecutes(testCommands.join(' '), workingDir)
   })
