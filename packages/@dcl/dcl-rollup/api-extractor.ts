1104fa168a0e920ce3d5ff9c20f2f98da9438f9e
gonpombo8 <gonzalo@decentraland.org>
2021-12-08 13:50:30 -0300

    add linter for rollup

--- packages/@dcl/dcl-rollup/api-extractor.ts
+++ packages/@dcl/dcl-rollup/api-extractor.ts
@@ -2,27 +2,39 @@ import * as fs from 'fs'
 import * as path from 'path'
 import { glob } from 'glob'
 
-import { Extractor, ExtractorConfig, ExtractorLogLevel, IExtractorConfigPrepareOptions } from '@microsoft/api-extractor'
+import {
+  Extractor,
+  ExtractorConfig,
+  IExtractorConfigPrepareOptions
+} from '@microsoft/api-extractor'
 
-export async function apiExtractor(packageJsonPath: string, localBuild: boolean) {
+export async function apiExtractor(
+  packageJsonPath: string,
+  localBuild: boolean
+) {
   const cwd = path.dirname(packageJsonPath)
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString())
   console.assert(packageJson.typings, 'package.json#typings is not valid')
   const typingsFullPath = path.resolve(packageJson.typings)
 
-  const prepareOptions: IExtractorConfigPrepareOptions = ExtractorConfig.tryLoadForFolder({ startingFolder: cwd }) || {
-    configObject: {
-      projectFolder: cwd,
-      mainEntryPointFilePath: path.resolve(packageJson.main.replace(/\.js$/, '.d.ts')),
-      compiler: {
-        tsconfigFilePath: 'tsconfig.json'
-      }
-    },
-    configObjectFullPath: undefined,
-    packageJsonFullPath: packageJsonPath
-  }
+  const prepareOptions: IExtractorConfigPrepareOptions =
+    ExtractorConfig.tryLoadForFolder({ startingFolder: cwd }) || {
+      configObject: {
+        projectFolder: cwd,
+        mainEntryPointFilePath: path.resolve(
+          packageJson.main.replace(/\.js$/, '.d.ts')
+        ),
+        compiler: {
+          tsconfigFilePath: 'tsconfig.json'
+        }
+      },
+      configObjectFullPath: undefined,
+      packageJsonFullPath: packageJsonPath
+    }
 
-  prepareOptions.configObject.mainEntryPointFilePath = path.resolve(prepareOptions.configObject.mainEntryPointFilePath)
+  prepareOptions.configObject.mainEntryPointFilePath = path.resolve(
+    prepareOptions.configObject.mainEntryPointFilePath
+  )
 
   if (!prepareOptions.configObject.dtsRollup) {
     prepareOptions.configObject.dtsRollup = {
@@ -50,9 +62,13 @@ export async function apiExtractor(packageJsonPath: string, localBuild: boolean)
 
   if (
     fs.existsSync(typingsFullPath) &&
-    typingsFullPath == path.resolve(prepareOptions.configObject.mainEntryPointFilePath)
+    typingsFullPath ===
+      path.resolve(prepareOptions.configObject.mainEntryPointFilePath)
   ) {
-    newentryPoint = path.resolve(path.dirname(typingsFullPath), Math.random() + path.basename(typingsFullPath))
+    newentryPoint = path.resolve(
+      path.dirname(typingsFullPath),
+      Math.random() + path.basename(typingsFullPath)
+    )
     fs.copyFileSync(typingsFullPath, newentryPoint)
     fs.unlinkSync(typingsFullPath)
     prepareOptions.configObject.mainEntryPointFilePath = newentryPoint
@@ -69,11 +85,13 @@ export async function apiExtractor(packageJsonPath: string, localBuild: boolean)
     showVerboseMessages: true
   })
 
-  glob.sync(path.dirname(packageJson.main) + '/**/*.d.ts', { absolute: true }).forEach((file) => {
-    if (file != typingsFullPath) {
-      fs.unlinkSync(file)
-    }
-  })
+  glob
+    .sync(path.dirname(packageJson.main) + '/**/*.d.ts', { absolute: true })
+    .forEach((file) => {
+      if (file !== typingsFullPath) {
+        fs.unlinkSync(file)
+      }
+    })
 
   if (extractorResult.succeeded) {
     console.log(`API Extractor completed successfully`)
