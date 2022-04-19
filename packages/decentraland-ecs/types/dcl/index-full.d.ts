7b74643a7dc2b99b8e3aa8c6d51768fe3501ed6b
menduz <github@menduz.com>
2022-04-19 16:01:37 -0300

    chore: fix renderer type (#133)
    * fix type
    
    * fix build
    
    Co-authored-by: Gon Pombo <gonzalo@decentraland.org>

--- packages/decentraland-ecs/types/dcl/index-full.d.ts
+++ packages/decentraland-ecs/types/dcl/index-full.d.ts
@@ -3740,7 +3740,7 @@ export declare type ProfileForRenderer = {
     email: string;
     avatar: AvatarForRenderer;
     snapshots: {
-        face: string;
+        face256: string;
         body: string;
     };
     version: number;
