40ebe4576ac35386f584e2be6dff6dc6bed96b3f
William Caine <william.caineiii@gmail.com>
2022-05-09 20:55:43 -0400

    feat: adding type Awaited to solve build issue related to using es2015.iterable

--- packages/@dcl/js-runtime/index.d.ts
+++ packages/@dcl/js-runtime/index.d.ts
@@ -327,6 +327,17 @@ interface PromiseConstructor {
 
 declare var Promise: PromiseConstructor
 
+/**
+ * Recursively unwraps the "awaited type" of a type. Non-promise "thenables" should resolve to `never`. This emulates the behavior of `await`.
+ */
+ type Awaited<T> =
+ T extends null | undefined ? T : // special case for `null | undefined` when not in `--strictNullChecks` mode
+     T extends object & { then(onfulfilled: infer F): any } ? // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
+         F extends ((value: infer V, ...args: any) => any) ? // if the argument to `then` is callable, extracts the first argument
+             Awaited<V> : // recursively unwrap the value
+             never : // the argument to `then` was not callable
+     T; // non-object or non-thenable
+
 /// --- FETCH ---
 
 type RequestRedirect = 'follow' | 'error' | 'manual'
