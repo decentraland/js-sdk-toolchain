3444e371e6f2ead7c0d98cdd1f9d9c267883e929
Suduck <adrian@suduck.com>
2022-06-16 10:36:08 +0200

    feat: gtlf shape component

---
+++ packages/@dcl/ecs/test/components/GLTFShape.spec.ts
@@ -0,0 +1,28 @@
+﻿import { Engine } from '../../src/engine'
+
+describe('Generated BoxShape ProtoBuf', () => {
+  it('should serialize/deserialize BoxShape', () => {
+    const newEngine = Engine()
+    const { GLTFShape } = newEngine.baseComponents
+    const entity = newEngine.addEntity()
+    const entityB = newEngine.addEntity()
+
+    const _shape = GLTFShape.create(entity, {
+      isPointerBlocker: true,
+      visible: true,
+      withCollisions: true,
+      src: 'test/src'
+    })
+
+    GLTFShape.create(entityB, {
+      isPointerBlocker: false,
+      visible: false,
+      withCollisions: false,
+      src: 'test/sr23sc c'
+    })
+    const buffer = GLTFShape.toBinary(entity)
+    GLTFShape.updateFromBinary(entityB, buffer)
+
+    expect(_shape).toBeDeepCloseTo({ ...GLTFShape.mutable(entityB) })
+  })
+})
