594738bc055134bba057090e284cea1f64928e61
Suduck <adrian@suduck.com>
2022-06-17 12:28:06 +0200

    feat: animator component

---
+++ packages/@dcl/ecs/test/components/Animator.spec.ts
@@ -0,0 +1,39 @@
+﻿import { Engine } from '../../src/engine'
+import {PBAnimationState} from "../../src/components/generated/pb/Animator.gen";
+
+describe('Generated BoxShape ProtoBuf', () => {
+  it('should serialize/deserialize BoxShape', () => {
+    const newEngine = Engine()
+    const { Animator } = newEngine.baseComponents
+    const entity = newEngine.addEntity()
+    const entityB = newEngine.addEntity()
+
+    const _animator = Animator.create(entity, {
+      states: [{
+        name: 'test',
+        clip: 'gge',
+        playing: true,
+        loop: true,
+        shouldReset: false,
+        weight: 1,
+        speed: 1
+      }],
+    })
+
+    Animator.create(entityB, {
+      states: [{
+        name: 'test2',
+        clip: 'gfgge',
+        playing: false,
+        loop: false,
+        shouldReset: true,
+        weight: 0,
+        speed: 0
+      }],
+    })
+    const buffer = Animator.toBinary(entity)
+    Animator.updateFromBinary(entityB, buffer)
+
+    expect(_animator).toBeDeepCloseTo({ ...Animator.mutable(entityB) })
+  })
+})
