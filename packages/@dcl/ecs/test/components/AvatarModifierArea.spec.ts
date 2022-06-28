816dea88ebddc87a234ec8011ed6914cadf537b0
Suduck <adrian@suduck.com>
2022-06-28 10:23:05 +0200

    feat: new component

---
+++ packages/@dcl/ecs/test/components/AvatarModifierArea.spec.ts
@@ -0,0 +1,29 @@
+﻿import {Engine} from '../../src/engine'
+import {PBAvatarModifierArea_Modifier} from "../../src/components/generated/pb/AvatarModifierArea.gen";
+
+describe('Generated BoxShape ProtoBuf', () => {
+  it('should serialize/deserialize BoxShape', () => {
+    const newEngine = Engine()
+    const { AvatarModifierArea } = newEngine.baseComponents
+    const entity = newEngine.addEntity()
+    const entityB = newEngine.addEntity()
+
+    const _boxShape = AvatarModifierArea.create(entity, {
+      modifiers: [PBAvatarModifierArea_Modifier.DISABLE_PASSPORTS,PBAvatarModifierArea_Modifier.HIDE_AVATARS],
+      excludeIds: ["exclude", "testIdReal", "numberAndString12837127371"]
+    })
+
+    AvatarModifierArea.create(entityB, {
+      modifiers: [PBAvatarModifierArea_Modifier.UNRECOGNIZED],
+      excludeIds: ["exclude_this", "testId", "12837127371"]
+    })
+    const buffer = AvatarModifierArea.toBinary(entity)
+    AvatarModifierArea.updateFromBinary(entityB, buffer)
+
+    expect(_boxShape).toBeDeepCloseTo({ ...AvatarModifierArea.mutable(entityB) })
+
+    expect(AvatarModifierArea.createOrReplace(entityB)).not.toBeDeepCloseTo({
+      ...AvatarModifierArea.mutable(entity)
+    })
+  })
+})
