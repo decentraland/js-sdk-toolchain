51c849d49677b69c26e1c13533a6f3f50e3f530c
Leandro Mendoza <leandro@decentraland.org>
2022-07-13 10:11:38 -0300

    feat: unrepeated entities with unique incremental counter

--- packages/@dcl/ecs/src/engine/entity-utils.ts
+++ packages/@dcl/ecs/src/engine/entity-utils.ts
@@ -1,19 +1,45 @@
 import { Entity } from './entity'
 
 export namespace EntityUtils {
-  export const MAX_ENTITIES = 10000
+  /**
+   * Range is the first element and the last possible element, they both are included in the interval.
+   * [start, end]
+   */
+  export type EntityRange = readonly [number, number]
 
-  let offset: number = MAX_ENTITIES
-  export function getOffset() {
-    const off = offset + 1
-    offset += MAX_ENTITIES
-    return off
+  export const MAX_ENTITIES = 0xffffffff
+  export const DYNAMIC_ENTITIES_START_AT = 100e3
+  export const RESERVED_STATIC_ENTITIES = 512
+
+  export const RESERVED_ENTITIES_RANGE: EntityRange = [
+    0,
+    RESERVED_STATIC_ENTITIES - 1
+  ]
+
+  export const STATIC_ENTITIES_RANGE: EntityRange = [
+    RESERVED_STATIC_ENTITIES,
+    DYNAMIC_ENTITIES_START_AT - 1
+  ]
+
+  export const DYNAMIC_ENTITIES_RANGE: EntityRange = [
+    DYNAMIC_ENTITIES_START_AT,
+    MAX_ENTITIES
+  ]
+
+  function isInRange(entity: Entity, range: EntityRange): boolean {
+    return entity >= range[0] && entity <= range[1]
   }
 
-  const STATIC_ENTITIES = [0, MAX_ENTITIES]
+  export function isDynamicEntity(entity: Entity) {
+    return isInRange(entity, DYNAMIC_ENTITIES_RANGE)
+  }
+
   export function isStaticEntity(entity: Entity) {
-    const [min, max] = STATIC_ENTITIES
-    return entity >= min && entity <= max
+    return isInRange(entity, STATIC_ENTITIES_RANGE)
+  }
+
+  export function isReservedEntity(entity: Entity) {
+    return isInRange(entity, RESERVED_ENTITIES_RANGE)
   }
 }
 
