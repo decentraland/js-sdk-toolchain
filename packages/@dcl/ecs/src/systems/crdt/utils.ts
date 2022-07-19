c7541d6329e144f80f391999c7618736128d71db
Leandro Mendoza <leandro@decentraland.org>
2022-07-19 09:12:54 -0300

    refactor: crdt: move from string key to pair of number

--- packages/@dcl/ecs/src/systems/crdt/utils.ts
+++ packages/@dcl/ecs/src/systems/crdt/utils.ts
@@ -4,10 +4,6 @@ import { Entity } from '../../engine/entity'
 export namespace CrdtUtils {
   export type ComponentID = ComponentDefinition['_id']
 
-  export function getKey(entity: Entity, componentId: ComponentID): string {
-    return `${entity}.${componentId}`
-  }
-
   export enum SynchronizedEntityType {
     // synchronizes entities with the NetworkSynchronized component only, used for networked games
     NETWORKED,
