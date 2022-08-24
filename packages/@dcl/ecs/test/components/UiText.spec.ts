54d9265e8885ebb56f7e73f7d82a500939f4f8f7
Pato <pato@decentraland.org>
2022-08-24 14:14:31 -0300

    add uitext test

---
+++ packages/@dcl/ecs/test/components/UiText.spec.ts
@@ -0,0 +1,28 @@
+import { Engine } from '../../src/engine'
+
+describe('Generated UiText ProtoBuf', () => {
+  it('should serialize/deserialize UiText', () => {
+    const newEngine = Engine()
+    const { UiText } = newEngine.baseComponents
+    const entity = newEngine.addEntity()
+    const entityB = newEngine.addEntity()
+
+    const _uiText = UiText.create(entity, {
+      text: 'not-casla',
+      textColor: { r: 0, g: 0, b: 0 }
+    })
+
+    UiText.create(entityB, {
+      text: 'not-casla',
+      textColor: { r: 0, g: 0, b: 1 }
+    })
+    const buffer = UiText.toBinary(entity)
+    UiText.updateFromBinary(entityB, buffer)
+
+    expect(_uiText).toBeDeepCloseTo({ ...UiText.getMutable(entityB) } as any)
+
+    expect(UiText.createOrReplace(entityB)).not.toBeDeepCloseTo({
+      ...UiText.getMutable(entity)
+    } as any)
+  })
+})
