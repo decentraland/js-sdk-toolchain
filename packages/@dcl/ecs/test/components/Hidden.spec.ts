63761a46297ff4081fc3cf91a0047ab1f69bf30f
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2022-08-24 23:48:44 -0300

    feat: add hidden spec

---
+++ packages/@dcl/ecs/test/components/Hidden.spec.ts
@@ -0,0 +1,22 @@
+﻿import { Engine } from '../../src/engine'
+
+describe('Generated Hidden ProtoBuf', () => {
+  it('should serialize/deserialize BoxShape', () => {
+    const newEngine = Engine()
+    const { Hidden } = newEngine.baseComponents
+    const entity = newEngine.addEntity()
+    const entityB = newEngine.addEntity()
+
+    const _shape = Hidden.create(entity)
+
+    Hidden.create(entityB)
+    const buffer = Hidden.toBinary(entity)
+    Hidden.updateFromBinary(entityB, buffer)
+
+    expect(_shape).toBeDeepCloseTo({ ...Hidden.getMutable(entityB) })
+
+    expect(Hidden.createOrReplace(entityB)).not.toBeDeepCloseTo({
+      ...Hidden.getMutable(entity)
+    })
+  })
+})
