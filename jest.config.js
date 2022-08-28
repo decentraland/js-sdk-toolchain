dbc1d97f3c3956aab30564277644cf040909dc4e
menduz <github@menduz.com>
2022-08-28 16:04:01 -0300

    change thresholds

--- jest.config.js
+++ jest.config.js
@@ -8,22 +8,33 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
+  coverageDirectory: "coverage",
+  // coverageProvider: 'v8',
   coverageThreshold: {
-    "packages/@dcl/ecs/src": {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    },
     "packages/@dcl/ecs/src/components/generated/pb": {
       branches: 70,
       functions: 70,
       lines: 70,
       statements: 70
+    },
+    "packages/@dcl/ecs/src": {
+      branches: 75,
+      functions: 95,
+      lines: 90,
+      statements: 90
+    },
+    "packages/@dcl/sdk/src/cli": {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0
     }
   },
-  testPathIgnorePatterns: [],
-  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "scripts",
+    "test",
+    "packages/@dcl/sdk/src/cli/mock-catalyst",
+  ],
   verbose: true,
   testMatch: ["**/*.spec.(ts)"],
   testEnvironment: "node",
