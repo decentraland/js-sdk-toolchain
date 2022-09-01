4c7b1e57bbd0409a4f25ac8e17877b23f2d050df
Suduck <adrian@suduck.com>
2022-09-01 11:36:41 +0200

    feat: add helpers

--- packages/@dcl/ecs/src/engine/events.ts
+++ packages/@dcl/ecs/src/engine/events.ts
@@ -1,15 +1,38 @@
-import { Entity } from "./entity";
-import { engine } from "../runtime/initialization";
-import {
-  PBPointerEventsResult_PointerCommand
-} from "../components/generated/pb/PointerEventsResult.gen";
+import {Entity} from "./entity";
+import {engine} from "../runtime/initialization";
+import {PBPointerEventsResult_PointerCommand} from "../components/generated/pb/PointerEventsResult.gen";
+import {PointerEventType} from "../components/generated/pb/PointerEvents.gen";
+import {ActionButton} from "../components/generated/pb/common/ActionButton.gen";
 
-export function* getPointerEvents(): Iterable<[Entity, PBPointerEventsResult_PointerCommand] > {
-  const rootEntity = (0 as Entity)
-  const value = engine.baseComponents.PointerEventsResult.getOrNull(rootEntity)
-  if(value != null){
-    for (const command of value.commands) {
-      yield [(command.hit!.entityId || 0 ) as Entity , command]
+export function wasEntityClicked(entity: Entity, actionButton: ActionButton): boolean {
+  const commands =  engine.baseComponents.PointerEventsResult.get(entity).commands
+  const down = findLastAction(commands, PointerEventType.DOWN,actionButton)
+  const up = findLastAction(commands, PointerEventType.UP,actionButton)
+
+  if (!down) return false
+  if (!up) return false
+  if (down.timestamp < up.timestamp) return true // clicked
+  return false
+}
+
+export function isActivePointerDown(entity: Entity, actionButton: ActionButton): boolean {
+  const commands =  engine.baseComponents.PointerEventsResult.get(entity).commands
+  const down = findLastAction(commands, PointerEventType.DOWN,actionButton)
+  const up = findLastAction(commands, PointerEventType.UP,actionButton)
+
+  if (!down) return false
+  if (!up) return true // currently pressed
+  if (down.timestamp > up.timestamp) return true // up component is from an old click
+  else return false
+}
+
+function findLastAction(commands: readonly PBPointerEventsResult_PointerCommand[], pointerEventType: PointerEventType, actionButton: ActionButton): PBPointerEventsResult_PointerCommand | undefined {
+  let commandToReturn: PBPointerEventsResult_PointerCommand | undefined
+  commands.forEach((command) =>{
+    if(command.button === actionButton && command.state === pointerEventType){
+      if(!commandToReturn || command.timestamp >= commandToReturn.timestamp)
+        commandToReturn = command
     }
-  }
+  })
+  return commandToReturn !== undefined ? commandToReturn : undefined;
 }
--- test/ecs/components/PointerEvents.spec.ts
+++ test/ecs/components/PointerEvents.spec.ts
@@ -1,6 +1,6 @@
-import {Engine} from '../../../packages/@dcl/ecs/src/engine'
-import {ActionButton} from "../../../packages/@dcl/ecs/src/components/generated/pb/common/ActionButton.gen";
-import {PointerEventType} from "../../../packages/@dcl/ecs/src/components/generated/pb/PointerEvents.gen";
+import { Engine } from '../../../packages/@dcl/ecs/src/engine'
+import { ActionButton } from "../../../packages/@dcl/ecs/src/components/generated/pb/common/ActionButton.gen";
+import { PointerEventType } from "../../../packages/@dcl/ecs/src/components/generated/pb/PointerEvents.gen";
 
 describe('Generated OnPointerDown ProtoBuf', () => {
   it('should serialize/deserialize OnPointerUp', () => {
