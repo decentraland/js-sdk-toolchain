4c5e7931caf5f2bbbc5e4009fe5335e5927cb402
Suduck <adrian@suduck.com>
2022-09-02 10:56:03 +0200

    feat: added helpers

--- packages/@dcl/ecs/src/engine/events.ts
+++ packages/@dcl/ecs/src/engine/events.ts
@@ -4,26 +4,72 @@ import {PBPointerEventsResult_PointerCommand} from "../components/generated/pb/P
 import {PointerEventType} from "../components/generated/pb/PointerEvents.gen";
 import {ActionButton} from "../components/generated/pb/common/ActionButton.gen";
 
+type EventEntityTypeKey = {
+  entityId: Number,
+  actionButton: ActionButton,
+  pointerEventType: PointerEventType
+}
+
+let entityClikedMap: Map<EventEntityTypeKey, number> = new Map<EventEntityTypeKey, number>()
+
 export function wasEntityClicked(entity: Entity, actionButton: ActionButton): boolean {
   const commands =  engine.baseComponents.PointerEventsResult.get(entity).commands
+  // We search the last DOWN command sorted by timestamp
   const down = findLastAction(commands, PointerEventType.DOWN,actionButton)
+  // We search the last UP command sorted by timestamp
   const up = findLastAction(commands, PointerEventType.UP,actionButton)
 
   if (!down) return false
   if (!up) return false
-  if (down.timestamp < up.timestamp) return true // clicked
+
+  const entityClickedLastCheckTimestamp = getLastTimestamp(entityClikedMap, entity,actionButton, PointerEventType.UP)
+  // If the DOWN command has happen before the UP commands, it means that that a clicked has happen
+  if (down.timestamp < up.timestamp && up.timestamp > entityClickedLastCheckTimestamp) {
+    setLastTimestamp(entityClikedMap,entity,actionButton, PointerEventType.UP, up.timestamp)
+    return true // clicked
+  }
   return false
 }
 
-export function isActivePointerDown(entity: Entity, actionButton: ActionButton): boolean {
+let entityPointerActiveMap: Map<EventEntityTypeKey, number> = new Map<EventEntityTypeKey, number>()
+export function isPointerEventActive(entity: Entity, actionButton: ActionButton, pointerEventType: PointerEventType): boolean {
   const commands =  engine.baseComponents.PointerEventsResult.get(entity).commands
-  const down = findLastAction(commands, PointerEventType.DOWN,actionButton)
-  const up = findLastAction(commands, PointerEventType.UP,actionButton)
+  // We search the last pointer Event command sorted by timestamp
+  const command = findLastAction(commands, pointerEventType,actionButton)
 
-  if (!down) return false
-  if (!up) return true // currently pressed
-  if (down.timestamp > up.timestamp) return true // up component is from an old click
-  else return false
+  if (!command) return false
+
+  const entityClickedLastCheckTimestamp = getLastTimestamp(entityPointerActiveMap, entity,actionButton, PointerEventType.UP)
+
+  if (command.timestamp > entityClickedLastCheckTimestamp) {
+    setLastTimestamp(entityPointerActiveMap,entity,actionButton, PointerEventType.UP, command.timestamp)
+    return true // up component is from an old click
+  }
+  else {
+    return false
+  }
+}
+
+function setLastTimestamp(map: Map<EventEntityTypeKey, number>, entityId: number, actionButton: ActionButton, pointerEventType: PointerEventType, timestamp: number){
+  const eventKey: EventEntityTypeKey = {
+    entityId: entityId,
+    actionButton: actionButton,
+    pointerEventType: pointerEventType
+  }
+  map.set(eventKey,timestamp)
+}
+
+function getLastTimestamp(map: Map<EventEntityTypeKey, number>, entityId: number, actionButton: ActionButton, pointerEventType: PointerEventType){
+  const eventKey: EventEntityTypeKey = {
+    entityId: entityId,
+    actionButton: actionButton,
+    pointerEventType: pointerEventType
+  }
+  const timestamp = map.get(eventKey)
+  if(timestamp)
+    return timestamp
+  else
+    return -1
 }
 
 function findLastAction(commands: readonly PBPointerEventsResult_PointerCommand[], pointerEventType: PointerEventType, actionButton: ActionButton): PBPointerEventsResult_PointerCommand | undefined {
