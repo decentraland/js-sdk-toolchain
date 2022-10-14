6cff82ab3141bb914944a4a3ffcfd501971d4daa
Leandro Mendoza <leandro@decentraland.org>
2022-10-14 10:49:45 -0300

    update billboard with descriptions

--- test/ecs/snippets/billboard/index.ts
+++ test/ecs/snippets/billboard/index.ts
@@ -1,4 +1,4 @@
-function createCube(x: number, y: number, z: number): Entity {
+function createPlaneTexture(x: number, y: number, z: number): Entity {
   const meshEntity = engine.addEntity()
 
   Material.create(meshEntity, {
@@ -16,20 +16,72 @@ function createCube(x: number, y: number, z: number): Entity {
   return meshEntity
 }
 
-Billboard.create(createCube(8, 3, 2), {
-  billboardMode: BillboardMode.BM_Y_AXE
-})
-Billboard.create(createCube(12, 3, 2))
-createCube(4, 3, 2)
+function createBillboards() {
+  Billboard.create(createPlaneTexture(8, 3, 1), {
+    billboardMode: BillboardMode.BM_Y_AXE
+  })
+  Billboard.create(createPlaneTexture(12, 3, 1))
+  createPlaneTexture(4, 3, 1)
 
-Billboard.create(createCube(8, 3, 8), {
-  oppositeDirection: true,
-  billboardMode: BillboardMode.BM_Y_AXE
-})
-Billboard.create(createCube(12, 3, 8), {
-  oppositeDirection: true,
-  billboardMode: BillboardMode.BM_ALL_AXES
-})
-createCube(4, 3, 8)
+  Billboard.create(createPlaneTexture(8, 3, 8), {
+    oppositeDirection: true,
+    billboardMode: BillboardMode.BM_Y_AXE
+  })
+  Billboard.create(createPlaneTexture(12, 3, 8), {
+    oppositeDirection: true,
+    billboardMode: BillboardMode.BM_ALL_AXES
+  })
+  createPlaneTexture(4, 3, 8)
+}
+
+function createTextShape(text: string, position: Vector3.ReadonlyVector3) {
+  const entity = engine.addEntity()
+  Transform.create(entity, { position })
+  return TextShape.create(entity, { text })
+}
+
+function createTextShapes() {
+  const text1 = createTextShape(
+    'Regular, only Y-rotation',
+    Vector3.create(8, 1, 1)
+  )
+  text1.fontSize = 3
+  text1.textColor = { r: 1, g: 0.2, b: 0.8, a: 0.8 }
+  text1.outlineColor = { r: 0, g: 0, b: 0 }
+  text1.outlineWidth = 0.1
+
+  const text2 = createTextShape('Regular', Vector3.create(12, 1, 1))
+  text2.fontSize = 3
+  text2.textColor = { r: 1, g: 0.2, b: 0.8, a: 0.8 }
+  text2.outlineColor = { r: 0, g: 0, b: 0 }
+  text2.outlineWidth = 0.1
+
+  const text3 = createTextShape('Without billboard', Vector3.create(4, 1, 1))
+  text3.fontSize = 3
+  text3.textColor = { r: 1, g: 0.2, b: 0.8, a: 0.8 }
+  text3.outlineColor = { r: 0, g: 0, b: 0 }
+  text3.outlineWidth = 0.1
+
+  const text4 = createTextShape('Opposite, only Y', Vector3.create(8, 1, 8))
+  text4.fontSize = 3
+  text4.textColor = { r: 0.8, g: 0.2, b: 1.0, a: 0.8 }
+  text4.outlineColor = { r: 0, g: 0, b: 0 }
+  text4.outlineWidth = 0.1
+
+  const text5 = createTextShape('Opposite', Vector3.create(12, 1, 8))
+  text5.fontSize = 3
+  text5.textColor = { r: 0.8, g: 0.2, b: 1.0, a: 0.8 }
+  text5.outlineColor = { r: 0, g: 0, b: 0 }
+  text5.outlineWidth = 0.1
+
+  const text6 = createTextShape('Without billboard', Vector3.create(4, 1, 8))
+  text6.fontSize = 3
+  text6.textColor = { r: 0.8, g: 0.2, b: 1.0, a: 0.8 }
+  text6.outlineColor = { r: 0, g: 0, b: 0 }
+  text6.outlineWidth = 0.1
+}
+
+createBillboards()
+createTextShapes()
 
 export {}
