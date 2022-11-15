9c7979a6ebc1a914467f9eb81622189bf1f5ff1e
Lean Mendoza <leandro@decentraland.org>
2022-11-15 07:15:25 -0300

    feat: pbr and unlit material (#321)
    * feat: pbr and unlit material
    
    * update @dcl/protocol

--- test/ecs/components/Material.spec.ts
+++ test/ecs/components/Material.spec.ts
@@ -1,9 +1,22 @@
-import { MaterialTransparencyMode } from '../../../packages/@dcl/ecs/src/components/generated/pb/decentraland/sdk/components/material.gen'
+import {
+  MaterialTransparencyMode,
+  PBMaterial,
+  PBMaterial_PbrMaterial
+} from '../../../packages/@dcl/ecs/src/components/generated/pb/decentraland/sdk/components/material.gen'
 import {
   TextureFilterMode,
   TextureWrapMode
 } from '../../../packages/@dcl/ecs/src/components/generated/pb/decentraland/common/texture.gen'
 import { Engine } from '../../../packages/@dcl/ecs/src/engine'
+
+function createPbrMaterial(pbr: PBMaterial_PbrMaterial): PBMaterial {
+  return {
+    material: {
+      $case: 'pbr',
+      pbr
+    }
+  }
+}
 
 describe('Generated Material ProtoBuf', () => {
   it('should serialize/deserialize Material', () => {
@@ -12,96 +25,103 @@ describe('Generated Material ProtoBuf', () => {
     const entity = newEngine.addEntity()
     const entityB = newEngine.addEntity()
 
-    const _material = Material.create(entity, {
-      ...Material.default(),
-      texture: {
-        tex: {
-          $case: 'avatarTexture',
-          avatarTexture: {
-            userId: 'user-id-dummy',
-            wrapMode: TextureWrapMode.TWM_CLAMP,
-            filterMode: undefined
+    const _material = Material.create(
+      entity,
+      createPbrMaterial({
+        texture: {
+          tex: {
+            $case: 'avatarTexture',
+            avatarTexture: {
+              userId: 'user-id-dummy',
+              wrapMode: TextureWrapMode.TWM_CLAMP,
+              filterMode: undefined
+            }
           }
-        }
-      },
-      alphaTexture: {
-        tex: {
-          $case: 'texture',
-          texture: {
-            filterMode: undefined,
-            wrapMode: undefined,
-            src: 'not-casla'
+        },
+        alphaTexture: {
+          tex: {
+            $case: 'texture',
+            texture: {
+              filterMode: undefined,
+              wrapMode: undefined,
+              src: 'not-casla'
+            }
           }
-        }
-      },
-      castShadows: true,
-      glossiness: 1,
-      metallic: 1,
-      roughness: 1,
-      specularIntensity: 0,
-      albedoColor: { r: 0, g: 1, b: 1 },
-      alphaTest: 0,
-      directIntensity: 1,
-      emissiveIntensity: 1,
-      emissiveColor: { r: 0, g: 1, b: 1 },
-      reflectivityColor: { r: 0, g: 1, b: 1 },
-      transparencyMode: MaterialTransparencyMode.MTM_ALPHA_BLEND
-    })
+        },
+        castShadows: true,
+        glossiness: 1,
+        metallic: 1,
+        roughness: 1,
+        specularIntensity: 0,
+        albedoColor: { r: 0, g: 1, b: 1 },
+        alphaTest: 0,
+        directIntensity: 1,
+        emissiveIntensity: 1,
+        emissiveColor: { r: 0, g: 1, b: 1 },
+        reflectivityColor: { r: 0, g: 1, b: 1 },
+        transparencyMode: MaterialTransparencyMode.MTM_ALPHA_BLEND,
+        bumpTexture: undefined,
+        emissiveTexture: undefined
+      })
+    )
 
-    Material.create(entityB, {
-      albedoColor: { r: 0, g: 1, b: 1 },
-      alphaTest: 1,
-      alphaTexture: {
-        tex: {
-          $case: 'texture',
-          texture: {
-            wrapMode: TextureWrapMode.TWM_CLAMP,
-            filterMode: TextureFilterMode.TFM_BILINEAR,
-            src: 'not-casla'
+    Material.create(
+      entityB,
+      createPbrMaterial({
+        albedoColor: { r: 0, g: 1, b: 1 },
+        alphaTest: 1,
+        alphaTexture: {
+          tex: {
+            $case: 'texture',
+            texture: {
+              wrapMode: TextureWrapMode.TWM_CLAMP,
+              filterMode: TextureFilterMode.TFM_BILINEAR,
+              src: 'not-casla'
+            }
           }
-        }
-      },
-      bumpTexture: {
-        tex: {
-          $case: 'texture',
-          texture: {
-            wrapMode: TextureWrapMode.TWM_MIRROR,
-            filterMode: TextureFilterMode.TFM_POINT,
-            src: 'not-casla'
+        },
+        bumpTexture: {
+          tex: {
+            $case: 'texture',
+            texture: {
+              wrapMode: TextureWrapMode.TWM_MIRROR,
+              filterMode: TextureFilterMode.TFM_POINT,
+              src: 'not-casla'
+            }
           }
-        }
-      },
-      emissiveTexture: {
-        tex: {
-          $case: 'texture',
-          texture: {
-            wrapMode: TextureWrapMode.TWM_MIRROR_ONCE,
-            filterMode: TextureFilterMode.TFM_TRILINEAR,
-            src: 'not-casla'
+        },
+        emissiveTexture: {
+          tex: {
+            $case: 'texture',
+            texture: {
+              wrapMode: TextureWrapMode.TWM_MIRROR_ONCE,
+              filterMode: TextureFilterMode.TFM_TRILINEAR,
+              src: 'not-casla'
+            }
           }
-        }
-      },
-      texture: {
-        tex: {
-          $case: 'avatarTexture',
-          avatarTexture: {
-            userId: '',
-            wrapMode: TextureWrapMode.TWM_REPEAT,
-            filterMode: undefined
+        },
+        texture: {
+          tex: {
+            $case: 'avatarTexture',
+            avatarTexture: {
+              userId: '',
+              wrapMode: TextureWrapMode.TWM_REPEAT,
+              filterMode: undefined
+            }
           }
-        }
-      },
-      castShadows: true,
-      directIntensity: 1,
-      emissiveColor: { r: 0, g: 0, b: 1 },
-      reflectivityColor: { r: 0, g: 0, b: 1 },
-      emissiveIntensity: 0,
-      glossiness: 0.5,
-      metallic: 1,
-      roughness: 1,
-      specularIntensity: 0,
-      transparencyMode: MaterialTransparencyMode.MTM_ALPHA_BLEND
-    })
+        },
+        castShadows: true,
+        directIntensity: 1,
+        emissiveColor: { r: 0, g: 0, b: 1 },
+        reflectivityColor: { r: 0, g: 0, b: 1 },
+        emissiveIntensity: 0,
+        glossiness: 0.5,
+        metallic: 1,
+        roughness: 1,
+        specularIntensity: 0,
+        transparencyMode: MaterialTransparencyMode.MTM_ALPHA_BLEND
+      })
+    )
     const buffer = Material.toBinary(entity)
     Material.updateFromBinary(entityB, buffer)
 
