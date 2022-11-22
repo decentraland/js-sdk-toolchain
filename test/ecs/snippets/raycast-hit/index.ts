d3f3f594fa46577b7bc9c3c1e1c59bb680de0056
Pravus <pravusjif@gmail.com>
2022-11-22 02:43:05 +0100

    applied linter auto-fix

--- test/ecs/snippets/raycast-hit/index.ts
+++ test/ecs/snippets/raycast-hit/index.ts
@@ -1,6 +1,11 @@
-import { EventsSystem } from "../../../../packages/@dcl/ecs/src";
+import { EventsSystem } from '../../../../packages/@dcl/ecs/src'
 
-function createCube(x: number, y: number, z: number, scaleMultiplier: number = 1) {
+function createCube(
+  x: number,
+  y: number,
+  z: number,
+  scaleMultiplier: number = 1
+) {
   const cubeEntity = engine.addEntity()
 
   Transform.create(cubeEntity, {
@@ -19,27 +24,37 @@ const cubeEntity = createCube(8, 1, 8)
 const raycastEntity = engine.addEntity()
 
 // Add OnPointerDown component to cube entity to trigger ray casting on interaction
-EventsSystem.onPointerDown(cubeEntity, () => {
-  Raycast.createOrReplace(raycastEntity, {
-    origin: Vector3.create(8, 1, 0.1),
-    direction: Vector3.create(0, 0, 1),
-    maxDistance: 16,
-    queryType: RaycastQueryType.RQT_HIT_FIRST
-  })
-}, {
-  button: InputAction.IA_POINTER,
-  hoverText: "CAST RAY"
-})
+EventsSystem.onPointerDown(
+  cubeEntity,
+  () => {
+    Raycast.createOrReplace(raycastEntity, {
+      origin: Vector3.create(8, 1, 0.1),
+      direction: Vector3.create(0, 0, 1),
+      maxDistance: 16,
+      queryType: RaycastQueryType.RQT_HIT_FIRST
+    })
+  },
+  {
+    button: InputAction.IA_POINTER,
+    hoverText: 'CAST RAY'
+  }
+)
 
 // System to detect new raycast responses and instantiate a cube where the ray hits
-var lastRaycastTimestamp = -1;
+let lastRaycastTimestamp = -1
 engine.addSystem(() => {
   for (const [entity, result] of engine.getEntitiesWith(RaycastResult)) {
-    if (result.hits?.length == 0 || result.timestamp <= lastRaycastTimestamp) continue
+    if (result.hits?.length == 0 || result.timestamp <= lastRaycastTimestamp)
+      continue
     lastRaycastTimestamp = result.timestamp
 
-    if(result.hits[0] && result.hits[0].position) {
-      createCube(result.hits[0].position.x, result.hits[0].position.y, result.hits[0].position.z, 0.3)
+    if (result.hits[0] && result.hits[0].position) {
+      createCube(
+        result.hits[0].position.x,
+        result.hits[0].position.y,
+        result.hits[0].position.z,
+        0.3
+      )
     }
 
     log(`Hits (this should be '1'): '${result.hits.length}'`)
