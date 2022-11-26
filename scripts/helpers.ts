6fa573b51e566cced84c04c9f60cbd2f5721b5bd
menduz <github@menduz.com>
2022-11-26 20:01:38 -0300

    cleanup

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -1,7 +1,14 @@
 import { exec } from 'child_process'
 import { sync as globSync } from 'glob'
 import { resolve, relative } from 'path'
-import { existsSync, readFileSync, writeFileSync, lstatSync, removeSync, copySync } from 'fs-extra'
+import {
+  existsSync,
+  readFileSync,
+  writeFileSync,
+  lstatSync,
+  removeSync,
+  copySync
+} from 'fs-extra'
 import { sync as rimraf } from 'rimraf'
 
 /**
@@ -22,12 +29,24 @@ export function ensureFileExistsSilent(file: string, root?: string) {
   return existsSync(x) ? x : false
 }
 
-export function runCommand(command: string, cwd: string, env?: Record<string, string>): Promise<string> {
+export function runCommand(
+  command: string,
+  cwd: string,
+  env?: Record<string, string>
+): Promise<string> {
   return new Promise<string>((onSuccess, onError) => {
-    process.stdout.write('\u001b[36min ' + relative(process.cwd(), cwd) + ':\u001b[0m ' + command + '\n')
+    process.stdout.write(
+      '\u001b[36min ' +
+        relative(process.cwd(), cwd) +
+        ':\u001b[0m ' +
+        command +
+        '\n'
+    )
     exec(command, { cwd, env }, (error, stdout, stderr) => {
-      stdout.trim().length && process.stdout.write('  ' + stdout.replace(/\n/g, '\n  ') + '\n')
-      stderr.trim().length && process.stderr.write('! ' + stderr.replace(/\n/g, '\n  ') + '\n')
+      stdout.trim().length &&
+        process.stdout.write('  ' + stdout.replace(/\n/g, '\n  ') + '\n')
+      stderr.trim().length &&
+        process.stderr.write('! ' + stderr.replace(/\n/g, '\n  ') + '\n')
       if (error) {
         onError(stderr)
       } else {
@@ -37,7 +56,11 @@ export function runCommand(command: string, cwd: string, env?: Record<string, st
   })
 }
 
-export function itExecutes(command: string, cwd: string, env?: Record<string, string>) {
+export function itExecutes(
+  command: string,
+  cwd: string,
+  env?: Record<string, string>
+) {
   it(command, async () => await runCommand(command, cwd, env), 60000)
 }
 
@@ -60,9 +83,16 @@ export function readJson(file: string, cwd: string): any {
   return JSON.parse(readFileSync(resolve(cwd, file)).toString())
 }
 
-export function patchJson(file: string, cwd: string, redux: (previous: any) => any): any {
+export function patchJson(
+  file: string,
+  cwd: string,
+  redux: (previous: any) => any
+): any {
   const path = resolve(cwd, file)
-  return writeFileSync(path, JSON.stringify(redux(JSON.parse(readFileSync(path).toString())), null, 2))
+  return writeFileSync(
+    path,
+    JSON.stringify(redux(JSON.parse(readFileSync(path).toString())), null, 2)
+  )
 }
 
 export function itInstallsADependencyFromFolderAndCopiesTheVersion(
@@ -100,7 +130,12 @@ export function itInstallsADependencyFromFolderAndCopiesTheVersion(
 }
 
 export function copyFile(from: string, to: string) {
-  process.stdout.write(`> copying ${relative(process.cwd(), from)} to ${relative(process.cwd(), to)}\n`)
+  process.stdout.write(
+    `> copying ${relative(process.cwd(), from)} to ${relative(
+      process.cwd(),
+      to
+    )}\n`
+  )
 
   if (!existsSync(from)) {
     throw new Error(`${from} does not exist`)
@@ -125,7 +160,10 @@ export async function delay(timeMs: number) {
   return new Promise<void>((resolve) => setTimeout(resolve, timeMs))
 }
 
-export async function waitForFileExist(filePath: string, timeoutMs: number = 120000) {
+export async function waitForFileExist(
+  filePath: string,
+  timeoutMs: number = 120000
+) {
   const stepMs = 1000
   let remaining = timeoutMs
   while (remaining > 0 && !existsSync(filePath)) {
