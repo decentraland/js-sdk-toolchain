fb5b256656ad93683c9df90b7d7d360486c68fb0
menduz <github@menduz.com>
2022-11-28 18:32:14 -0300

    fix: pack all packages and fix versions before release

--- scripts/prepare.spec.ts
+++ scripts/prepare.spec.ts
@@ -3,7 +3,9 @@ import {
   commonChecks,
   SDK_PATH,
   ROLLUP_CONFIG_PATH,
-  JS_RUNTIME
+  JS_RUNTIME,
+  ECS7_PATH,
+  REACT_ECS
 } from './common'
 
 import {
@@ -20,12 +22,16 @@ flow('build-all', () => {
       SDK_PATH,
       ROLLUP_CONFIG_PATH
     )
+    itInstallsADependencyFromFolderAndCopiesTheVersion(SDK_PATH, ECS7_PATH)
+    itInstallsADependencyFromFolderAndCopiesTheVersion(SDK_PATH, REACT_ECS)
     itInstallsADependencyFromFolderAndCopiesTheVersion(SDK_PATH, JS_RUNTIME)
   })
 
   flow('pack every package', () => {
     itExecutes('npm pack', SDK_PATH)
     itExecutes('npm pack', JS_RUNTIME)
+    itExecutes('npm pack', ECS7_PATH)
+    itExecutes('npm pack', REACT_ECS)
     itExecutes('npm pack', ROLLUP_CONFIG_PATH)
   })
 })
