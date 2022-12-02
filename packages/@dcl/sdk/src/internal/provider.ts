32d0ee8d4f51eeb9dfe4eec7fe47084a651984ca
menduz <github@menduz.com>
2022-12-02 12:29:34 -0300

    initial implementation of ethereum provider (#345)
    * initial implementation of provider
    
    * lint and merge conflict

---
+++ packages/@dcl/sdk/src/internal/provider.ts
@@ -0,0 +1,54 @@
+import type {
+  SendAsyncRequest,
+  SendAsyncResponse
+} from '~system/EthereumController'
+
+export type RPCSendableMessage = {
+  jsonrpc: '2.0'
+  id: number
+  method: string
+  params: any[]
+}
+
+export interface MessageDict {
+  [key: string]: string
+}
+
+export type SendAsyncType = (
+  params: SendAsyncRequest
+) => Promise<SendAsyncResponse>
+
+export function getEthereumProvider(sendAsync: SendAsyncType) {
+  async function request(message: RPCSendableMessage) {
+    const response = await sendAsync({
+      id: message.id,
+      method: message.method,
+      jsonParams: JSON.stringify(message.params)
+    })
+    return JSON.parse(response.jsonAnyResponse)
+  }
+
+  return {
+    // @internal
+    send(
+      message: RPCSendableMessage,
+      callback?: (error: Error | null, result?: any) => void
+    ): void {
+      if (message && callback && callback instanceof Function) {
+        request(message)
+          .then((x: any) => callback(null, x))
+          .catch(callback)
+      } else {
+        throw new Error('Decentraland provider only allows async calls')
+      }
+    },
+    sendAsync(
+      message: RPCSendableMessage,
+      callback: (error: Error | null, result?: any) => void
+    ): void {
+      request(message)
+        .then((x: any) => callback(null, x))
+        .catch(callback)
+    }
+  }
+}
