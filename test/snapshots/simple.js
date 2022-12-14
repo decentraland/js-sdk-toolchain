29af84baddbb499a4d947738f460d26deb031e26
menduz <github@menduz.com>
2022-12-14 19:43:40 -0300

    remove generated files

--- test/snapshots/simple.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-async function onUpdate() { }
-async function onStart() { }
-
-exports.onStart = onStart;
-exports.onUpdate = onUpdate;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2ltcGxlLmpzIiwic291cmNlcyI6WyJkY2w6Ly8vYmluL3NpbXBsZS50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgYXN5bmMgZnVuY3Rpb24gb25VcGRhdGUoKSB7fVxuZXhwb3J0IGFzeW5jIGZ1bmN0aW9uIG9uU3RhcnQoKSB7fVxuIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQU8sZUFBZSxRQUFRLE1BQUs7QUFDNUIsZUFBZSxPQUFPOzs7OzsifQ==
