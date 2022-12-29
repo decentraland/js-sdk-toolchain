91c872e78a0e00683f09f5aa314de84c1ff07924
menduz <github@menduz.com>
2022-12-29 20:38:09 -0300

    revert uber-package

--- packages/@dcl/playground-assets/src/index.ts
+++ packages/@dcl/playground-assets/src/index.ts
@@ -1,1 +1,20 @@
-export * from '@dcl/sdk/uber-package'
+export * from '@dcl/sdk/math'
+export * from '@dcl/sdk/ecs'
+export * from '@dcl/sdk/react-ecs'
+export * from '@dcl/sdk/messageBus'
+export * from '@dcl/sdk/ethereum-provider'
+
+// legacy
+export * from '@dcl/sdk/observables'
+export {
+  Observer,
+  Observable,
+  ObserverEventState
+} from '@dcl/sdk/internal/Observable'
+
+// react
+import ReactEcs from '@dcl/sdk/react-ecs'
+export { ReactEcs }
+
+// expose onUpdate
+export * from '@dcl/sdk'
