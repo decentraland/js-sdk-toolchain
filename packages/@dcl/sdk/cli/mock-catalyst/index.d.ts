3699d61f05d5f67e02c8c2b0beaee22fe6f3ac0a
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-01-11 21:35:47 -0300

    update

--- packages/@dcl/sdk/cli/mock-catalyst/index.d.ts
+++
@@ -1,2 +0,0 @@
-export const __esModule: true;
-export function mockCatalyst(app: any, baseFolders: any): void;
--- packages/@dcl/sdk/cli/wearables.d.ts
+++
@@ -1,4 +0,0 @@
-export const __esModule: true;
-export const wearableValidator: any;
-export function getAllPreviewWearables(_a: any): any[];
-export function mockPreviewWearables(app: any, baseFolders: any): void;
