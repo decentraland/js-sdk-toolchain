59d747af359feedef5edbd756c59b94a1d038856
Pato <pato@decentraland.org>
2023-01-12 13:49:56 -0300

    feat: add `crdtGetState` to `EngineApi` service

--- packages/@dcl/playground-assets/etc/playground-assets.api.md
+++ packages/@dcl/playground-assets/etc/playground-assets.api.md
@@ -1556,6 +1556,11 @@ export const onRealmChangedObservable: Observable<{
 // @public (undocumented)
 export const onSceneReadyObservable: Observable<unknown>;
 
+// Warning: (ae-missing-release-tag) "onStart" is part of the package's API, but it is missing a release tag (@alpha, @beta, @public, or @internal)
+//
+// @public (undocumented)
+export function onStart(): Promise<void>;
+
 // Warning: (ae-missing-release-tag) "onUpdate" is part of the package's API, but it is missing a release tag (@alpha, @beta, @public, or @internal)
 //
 // @public (undocumented)
--- packages/@dcl/sdk/src/index.ts
+++ packages/@dcl/sdk/src/index.ts
@@ -2,11 +2,17 @@
 
 import { engine } from '@dcl/ecs'
 import { pollEvents, setSubscribeFunction } from './observables'
-import { subscribe, sendBatch, crdtSendToRenderer } from '~system/EngineApi'
+import {
+  subscribe,
+  sendBatch,
+  crdtSendToRenderer,
+  crdtGetState
+} from '~system/EngineApi'
 import { createRendererTransport } from './internal/transports/rendererTransport'
 
 // Attach CRDT transport
-engine.addTransport(createRendererTransport({ crdtSendToRenderer }))
+const rendererTransport = createRendererTransport({ crdtSendToRenderer })
+engine.addTransport(rendererTransport)
 
 // attach engineApi.subscribe function for events. This is only a transition
 // patch until events are completely migrated to CRDT messages
@@ -15,4 +21,15 @@ setSubscribeFunction(subscribe)
 export async function onUpdate(deltaTime: number) {
   await engine.update(deltaTime)
   await pollEvents(sendBatch)
+}
+
+export async function onStart() {
+  const response = await crdtGetState({ data: new Uint8Array() })
+  if (!!rendererTransport.onmessage) {
+    if (response && response.data && response.data.length) {
+      for (const byteArray of response.data) {
+        rendererTransport.onmessage(byteArray)
+      }
+    }
+  }
 }
