0c08bd5517a4a2edc07cacb1301299622842dd88
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-01-18 22:41:48 -0300

    add more values

--- test/ecs/components/VideoPlayer.spec.ts
+++ test/ecs/components/VideoPlayer.spec.ts
@@ -7,7 +7,12 @@ describe('VideoPlayer component', () => {
     const entity = newEngine.addEntity()
 
     VideoPlayer.create(entity, {
-      src: 'le-video.mp4'
+      src: 'le-video.mp4',
+      loop: true,
+      playbackRate: 0.5,
+      position: 65.3,
+      playing: true,
+      volume: 0.3
     })
 
     const buffer = VideoPlayer.toBinary(entity)
