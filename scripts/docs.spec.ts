3559d2f6ecbe41c23b17a9e0a6a207e8d4aad0c2
gonpombo8 <gonzalo@decentraland.org>
2023-01-30 15:44:10 -0300

    update make docs command for cloudflare pages

--- scripts/docs.spec.ts
+++ scripts/docs.spec.ts
@@ -2,15 +2,16 @@ import { readFileSync, writeFileSync } from 'fs'
 import { resolve } from 'path'
 
 import { flow } from './common'
-import { itExecutes } from './helpers'
+import { itExecutes, runCommand } from './helpers'
 
 flow('build docs site', () => {
   const SDK_TOOLCHAIN_PATH = resolve(process.cwd())
-
+  itExecutes('make install', SDK_TOOLCHAIN_PATH)
+  it('make build', async () => await runCommand('make build', SDK_TOOLCHAIN_PATH), 600000)
   itExecutes('./node_modules/.bin/typedoc', SDK_TOOLCHAIN_PATH)
 
   it('fix html page', async () => {
-    const cssPath = resolve(SDK_TOOLCHAIN_PATH, 'generated-docs/assets/style.css')
+    const cssPath = resolve(SDK_TOOLCHAIN_PATH, 'api-docs/assets/style.css')
     const cssContent = readFileSync(cssPath).toString()
     const replaceCss = cssContent.replace(/1024px/g, '860px')
     const customCss = `
