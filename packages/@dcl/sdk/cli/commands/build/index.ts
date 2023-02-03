03d8f4d553570d3e336dd5e7a5b16e37dd6ba724
menduz <github@menduz.com>
2023-02-03 16:49:29 -0300

    fix watch mode

--- packages/@dcl/sdk/cli/commands/build/index.ts
+++ packages/@dcl/sdk/cli/commands/build/index.ts
@@ -89,7 +89,9 @@ export async function main(options: Options) {
   // track stuff...
   // https://github.com/decentraland/cli/blob/main/src/commands/build.ts
 
-  // rollup watcher leaves many open FSWatcher even in build mode. we must call
-  // process.exit at this point to prevent the program halting forever
-  process.exit(process.exitCode)
+  if (!watch) {
+    // rollup watcher leaves many open FSWatcher even in build mode. we must call
+    // process.exit at this point to prevent the program halting forever
+    process.exit(process.exitCode)
+  }
 }
