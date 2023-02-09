7c953eaff120fd7f56a1c9467b415c629a0af62c
Leandro Mendoza <leandro@decentraland.org>
2023-02-09 16:33:01 -0300

    back to npm v8

--- packages/@dcl/sdk/cli/commands/preview/bff.ts
+++ packages/@dcl/sdk/cli/commands/preview/bff.ts
@@ -1,6 +1,6 @@
 import { handleSocketLinearProtocol } from '@dcl/mini-comms/dist/logic/handle-linear-protocol'
 import { PreviewComponents } from './types'
-import { AboutResponse } from '@dcl/protocol/out-ts/bff/http-endpoints.gen'
+import { AboutResponse } from '@dcl/protocol/out-ts/decentraland/bff/http_endpoints.gen'
 import { Router } from '@well-known-components/http-server'
 import { upgradeWebSocketResponse } from '@well-known-components/http-server/dist/ws'
 
@@ -14,13 +14,17 @@ export function setupBffAndComms(components: PreviewComponents, router: Router<P
     const host = ctx.url.host
 
     const body: AboutResponse = {
-      bff: { healthy: false },
+      acceptingUsers: true,
+      bff: { healthy: false, publicUrl: host },
       comms: {
         healthy: true,
         protocol: 'v3',
         fixedAdapter: `ws-room:${ctx.url.protocol.replace(/^http/, 'ws')}//${host}/mini-comms/room-1`
       },
       configurations: {
+        networkId: 0,
+        globalScenesUrn: [],
+        scenesUrn: [],
         realmName: 'LocalPreview'
       },
       content: {
