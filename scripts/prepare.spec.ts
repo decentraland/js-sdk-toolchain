b79153c0ce02f6e1c43c4c65c03627f3ec2566ee
menduz <github@menduz.com>
2023-02-15 16:29:16 -0300

    fix unresolved version in publish

--- scripts/prepare.spec.ts
+++ scripts/prepare.spec.ts
@@ -1,3 +1,5 @@
+import { readFile } from 'fs/promises'
+import { resolve } from 'path'
 import {
   flow,
   commonChecks,
@@ -21,7 +23,10 @@ flow('build-all', () => {
     itInstallsADependencyFromFolderAndCopiesTheVersion(SDK_PATH, ECS7_PATH)
     itInstallsADependencyFromFolderAndCopiesTheVersion(SDK_PATH, REACT_ECS)
     itInstallsADependencyFromFolderAndCopiesTheVersion(SDK_PATH, JS_RUNTIME)
+    itInstallsADependencyFromFolderAndCopiesTheVersion(ECS7_PATH, JS_RUNTIME)
     itInstallsADependencyFromFolderAndCopiesTheVersion(PLAYGROUND_ASSETS_PATH, SDK_PATH)
+
+    checkNoLocalPackages(ROLLUP_CONFIG_PATH, SDK_PATH, ECS7_PATH, REACT_ECS, JS_RUNTIME, PLAYGROUND_ASSETS_PATH)
   })
 
   flow('pack every package', () => {
@@ -33,3 +38,22 @@ flow('build-all', () => {
     itExecutes('npm pack', INSPECTOR_PATH)
   })
 })
+
+function checkNoLocalPackages(...paths: string[]) {
+  for (const path of paths) {
+    const packageJson = resolve(path, 'package.json')
+    it('checking ' + packageJson, async () => {
+      const { dependencies } = JSON.parse(await readFile(packageJson, 'utf-8'))
+      for (const [key, value] of Object.entries(dependencies as Record<string, string>)) {
+        if (
+          value.startsWith('file:') ||
+          value.startsWith('http:') ||
+          value.startsWith('https:') ||
+          value.startsWith('git:')
+        ) {
+          throw new Error(`Dependency ${key} is not pointing to a published version: ${value}`)
+        }
+      }
+    })
+  }
+}
