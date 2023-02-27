b96766936213e7f94be95405f2b95854e153773f
menduz <github@menduz.com>
2023-02-27 18:01:57 -0300

    wip

---
+++ packages/@dcl/inspector/src/lib/babylon/setup/index.ts
@@ -0,0 +1,155 @@
+import * as BABYLON from '@babylonjs/core'
+import { initKeyboard } from './input'
+
+// if NODE_ENV == development
+require('@babylonjs/inspector')
+
+export namespace ambientConfigurations {
+  // TODO: move this configurations inside EnvironmentHelper(options)
+  export const groundColor = new BABYLON.Color3(0.1, 0.1, 0.1)
+  export const sunColor = new BABYLON.Color3(1, 1, 1)
+  export const sunPosition = new BABYLON.Vector3(-1, 0.01, 0.3).scaleInPlace(500)
+  export const sunPositionColor = new BABYLON.Color3(sunPosition.x, sunPosition.y, sunPosition.z)
+
+  export const RED = BABYLON.Color3.FromHexString('#ff004f')
+  export const GREEN = BABYLON.Color3.FromHexString('#00e57a')
+  export const BLUE = BABYLON.Color3.FromHexString('#00beff')
+}
+
+export function initEngine(canvas: HTMLCanvasElement) {
+  const babylon = new BABYLON.Engine(canvas, true, {
+    audioEngine: true,
+    autoEnableWebVR: true,
+    deterministicLockstep: true,
+    lockstepMaxSteps: 4,
+    alpha: false,
+    antialias: true,
+    stencil: true
+  })
+
+  /**
+   * This is the main scene of the engine.
+   */
+  const scene = new BABYLON.Scene(babylon)
+  const audioEngine = BABYLON.Engine.audioEngine
+  const effectLayers: BABYLON.EffectLayer[] = []
+
+  const highlightLayer: BABYLON.HighlightLayer = new BABYLON.HighlightLayer('highlight', scene)
+
+  {
+    if (!scene.effectLayers.includes(highlightLayer)) {
+      scene.addEffectLayer(highlightLayer)
+    }
+
+    highlightLayer.innerGlow = false
+    highlightLayer.outerGlow = true
+
+    effectLayers.push(highlightLayer)
+  }
+
+  scene.clearColor = BABYLON.Color3.FromInts(31, 29, 35).toColor4(1)
+  scene.collisionsEnabled = true
+
+  scene.autoClear = false // Color buffer
+  scene.autoClearDepthAndStencil = false // Depth and stencil
+  scene.setRenderingAutoClearDepthStencil(0, false)
+  scene.setRenderingAutoClearDepthStencil(1, true, true, false)
+
+  canvas.style.width = '100%'
+  canvas.style.height = '100%'
+
+  // scene.gravity = new BABYLON.Vector3(0, playerConfigurations.gravity, 0)
+  // scene.enablePhysics(scene.gravity, new BABYLON.OimoJSPlugin(2))
+  scene.audioEnabled = true
+  scene.headphone = true
+
+  scene.actionManager = new BABYLON.ActionManager(scene)
+
+  babylon.disableManifestCheck = true
+
+  scene.getBoundingBoxRenderer().showBackLines = false
+
+  scene.onReadyObservable.addOnce(() => {
+    const gl = new BABYLON.GlowLayer('glow', scene)
+    effectLayers.push(gl)
+
+    effectLayers.forEach(($) => scene.effectLayers.includes($) || scene.addEffectLayer($))
+
+    scene.removeEffectLayer = function (this: any, layer: BABYLON.EffectLayer) {
+      if (effectLayers.includes(layer)) return
+      // eslint-disable-next-line prefer-rest-params
+      scene.constructor.prototype.removeEffectLayer.apply(this, arguments)
+    } as any
+
+    scene.addEffectLayer = function (this: any, layer: BABYLON.EffectLayer) {
+      if (effectLayers.includes(layer)) return
+      // eslint-disable-next-line prefer-rest-params
+      scene.constructor.prototype.addEffectLayer.apply(this, arguments)
+    } as any
+  })
+
+  BABYLON.Database.IDBStorageEnabled = true
+  babylon.enableOfflineSupport = true
+
+  const camera = new BABYLON.ArcRotateCamera(
+    'editorCamera',
+    -Math.PI / 2,
+    Math.PI / 2.5,
+    15,
+    new BABYLON.Vector3(0, 0, 0)
+  )
+
+  camera.attachControl(canvas, true)
+
+  const editorColor = BABYLON.Color3.FromHexString('#0e0c12')
+  const editorEnvHelper = scene.createDefaultEnvironment({
+    environmentTexture: void 0,
+    groundColor: editorColor,
+    skyboxColor: editorColor
+  })!
+
+  editorEnvHelper!.ground!.position.y = 0
+  editorEnvHelper!.rootMesh.position.y = -0.1
+
+  scene.fogEnabled = false
+  editorEnvHelper.ground!.visibility = 0
+
+  const hemiLight = new BABYLON.HemisphericLight('default light', ambientConfigurations.sunPosition, scene)
+
+  hemiLight.diffuse = BABYLON.Color3.White()
+  hemiLight.groundColor = ambientConfigurations.groundColor.clone()
+  hemiLight.specular = ambientConfigurations.sunColor.clone()
+
+  initKeyboard(scene, camera)
+
+  function update() {
+    scene.render()
+  }
+
+  // Register a render loop to repeatedly render the scene
+  babylon.runRenderLoop(update)
+
+  // Watch for browser/canvas resize events
+  function resize() {
+    babylon.resize()
+  }
+  window.addEventListener('resize', resize)
+
+  function dispose() {
+    babylon.dispose()
+    if (window) {
+      window.removeEventListener('resize', resize)
+    }
+  }
+
+  return {
+    editorCamera: camera,
+    canvas,
+    babylon,
+    scene,
+    audioEngine,
+    effectLayers,
+    highlightLayer,
+    dispose
+  }
+}
