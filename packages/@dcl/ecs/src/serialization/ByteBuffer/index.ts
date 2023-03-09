d54fcb014e4ff238ca68b747d5efd69bf009f4f6
Leandro Mendoza <leandro@decentraland.org>
2023-03-08 22:29:40 -0300

    change to serialization to little endian

--- packages/@dcl/ecs/src/serialization/ByteBuffer/index.ts
+++ packages/@dcl/ecs/src/serialization/ByteBuffer/index.ts
@@ -20,6 +20,8 @@ const defaultInitialCapacity = 10240
  *
  * - Use read and write function to generate or consume data.
  * - Use set and get only if you are sure that you're doing.
+ * 
+ * It always passes littleEndian param as true
  */
 export class ReadWriteByteBuffer implements ByteBuffer {
   _buffer: Uint8Array
@@ -91,41 +93,41 @@ export class ReadWriteByteBuffer implements ByteBuffer {
     return this.woffset - this.roffset
   }
   readFloat32(): number {
-    return this.view.getFloat32(this.#roAdd(4))
+    return this.view.getFloat32(this.#roAdd(4), true) // littleEndian = true
   }
   readFloat64(): number {
-    return this.view.getFloat64(this.#roAdd(8))
+    return this.view.getFloat64(this.#roAdd(8), true) // littleEndian = true
   }
   readInt8(): number {
     return this.view.getInt8(this.#roAdd(1))
   }
   readInt16(): number {
-    return this.view.getInt16(this.#roAdd(2))
+    return this.view.getInt16(this.#roAdd(2), true) // littleEndian = true
   }
   readInt32(): number {
-    return this.view.getInt32(this.#roAdd(4))
+    return this.view.getInt32(this.#roAdd(4), true) // littleEndian = true
   }
   readInt64(): bigint {
-    return this.view.getBigInt64(this.#roAdd(8))
+    return this.view.getBigInt64(this.#roAdd(8), true) // littleEndian = true
   }
   readUint8(): number {
     return this.view.getUint8(this.#roAdd(1))
   }
   readUint16(): number {
-    return this.view.getUint16(this.#roAdd(2))
+    return this.view.getUint16(this.#roAdd(2), true) // littleEndian = true
   }
   readUint32(): number {
-    return this.view.getUint32(this.#roAdd(4))
+    return this.view.getUint32(this.#roAdd(4), true) // littleEndian = true
   }
   readUint64(): bigint {
-    return this.view.getBigUint64(this.#roAdd(8))
+    return this.view.getBigUint64(this.#roAdd(8), true) // littleEndian = true
   }
   readBuffer() {
-    const length = this.view.getUint32(this.#roAdd(4))
+    const length = this.view.getUint32(this.#roAdd(4), true) // littleEndian = true
     return this._buffer.subarray(this.#roAdd(length), this.#roAdd(0))
   }
   readUtf8String() {
-    const length = this.view.getUint32(this.#roAdd(4))
+    const length = this.view.getUint32(this.#roAdd(4), true) // littleEndian = true
     return utf8.read(this._buffer, this.#roAdd(length), this.#roAdd(0))
   }
   incrementWriteOffset(amount: number): number {
@@ -158,11 +160,11 @@ export class ReadWriteByteBuffer implements ByteBuffer {
   }
   writeFloat32(value: number): void {
     const o = this.#woAdd(4)
-    this.view.setFloat32(o, value)
+    this.view.setFloat32(o, value, true) // littleEndian = true
   }
   writeFloat64(value: number): void {
     const o = this.#woAdd(8)
-    this.view.setFloat64(o, value)
+    this.view.setFloat64(o, value, true) // littleEndian = true
   }
   writeInt8(value: number): void {
     const o = this.#woAdd(1)
@@ -170,15 +172,15 @@ export class ReadWriteByteBuffer implements ByteBuffer {
   }
   writeInt16(value: number): void {
     const o = this.#woAdd(2)
-    this.view.setInt16(o, value)
+    this.view.setInt16(o, value, true) // littleEndian = true
   }
   writeInt32(value: number): void {
     const o = this.#woAdd(4)
-    this.view.setInt32(o, value)
+    this.view.setInt32(o, value, true) // littleEndian = true
   }
   writeInt64(value: bigint): void {
     const o = this.#woAdd(8)
-    this.view.setBigInt64(o, value)
+    this.view.setBigInt64(o, value, true) // littleEndian = true
   }
   writeUint8(value: number): void {
     const o = this.#woAdd(1)
@@ -186,76 +188,76 @@ export class ReadWriteByteBuffer implements ByteBuffer {
   }
   writeUint16(value: number): void {
     const o = this.#woAdd(2)
-    this.view.setUint16(o, value)
+    this.view.setUint16(o, value, true) // littleEndian = true
   }
   writeUint32(value: number): void {
     const o = this.#woAdd(4)
-    this.view.setUint32(o, value)
+    this.view.setUint32(o, value, true) // littleEndian = true
   }
   writeUint64(value: bigint): void {
     const o = this.#woAdd(8)
-    this.view.setBigUint64(o, value)
+    this.view.setBigUint64(o, value, true) // littleEndian = true
   }
   // DataView Proxy
   getFloat32(offset: number): number {
-    return this.view.getFloat32(offset)
+    return this.view.getFloat32(offset, true) // littleEndian = true
   }
   getFloat64(offset: number): number {
-    return this.view.getFloat64(offset)
+    return this.view.getFloat64(offset, true) // littleEndian = true
   }
   getInt8(offset: number): number {
     return this.view.getInt8(offset)
   }
   getInt16(offset: number): number {
-    return this.view.getInt16(offset)
+    return this.view.getInt16(offset, true) // littleEndian = true
   }
   getInt32(offset: number): number {
-    return this.view.getInt32(offset)
+    return this.view.getInt32(offset, true) // littleEndian = true
   }
   getInt64(offset: number): bigint {
-    return this.view.getBigInt64(offset)
+    return this.view.getBigInt64(offset, true) // littleEndian = true
   }
   getUint8(offset: number): number {
     return this.view.getUint8(offset)
   }
   getUint16(offset: number): number {
-    return this.view.getUint16(offset)
+    return this.view.getUint16(offset, true) // littleEndian = true
   }
   getUint32(offset: number): number {
-    return this.view.getUint32(offset) >>> 0
+    return this.view.getUint32(offset, true) // littleEndian = true >>> 0
   }
   getUint64(offset: number): bigint {
-    return this.view.getBigUint64(offset)
+    return this.view.getBigUint64(offset, true) // littleEndian = true
   }
   setFloat32(offset: number, value: number): void {
-    this.view.setFloat32(offset, value)
+    this.view.setFloat32(offset, value, true) // littleEndian = true
   }
   setFloat64(offset: number, value: number): void {
-    this.view.setFloat64(offset, value)
+    this.view.setFloat64(offset, value, true) // littleEndian = true
   }
   setInt8(offset: number, value: number): void {
     this.view.setInt8(offset, value)
   }
   setInt16(offset: number, value: number): void {
-    this.view.setInt16(offset, value)
+    this.view.setInt16(offset, value, true) // littleEndian = true
   }
   setInt32(offset: number, value: number): void {
-    this.view.setInt32(offset, value)
+    this.view.setInt32(offset, value, true) // littleEndian = true
   }
   setInt64(offset: number, value: bigint): void {
-    this.view.setBigInt64(offset, value)
+    this.view.setBigInt64(offset, value, true) // littleEndian = true
   }
   setUint8(offset: number, value: number): void {
     this.view.setUint8(offset, value)
   }
   setUint16(offset: number, value: number): void {
-    this.view.setUint16(offset, value)
+    this.view.setUint16(offset, value, true) // littleEndian = true
   }
   setUint32(offset: number, value: number): void {
-    this.view.setUint32(offset, value)
+    this.view.setUint32(offset, value, true) // littleEndian = true
   }
   setUint64(offset: number, value: bigint): void {
-    this.view.setBigUint64(offset, value)
+    this.view.setBigUint64(offset, value, true) // littleEndian = true
   }
 }
 
