716632a87f0619665133486ac10eb52659250c58
Juan Cazala <juancazala@gmail.com>
2023-03-10 11:22:21 -0300

    chore: ignore line

--- packages/@dcl/inspector/src/catalog.ts
+++ packages/@dcl/inspector/src/catalog.ts
@@ -1,4 +1,5 @@
 import { ITheme } from './components/AssetsCatalog'
 
 // TODO: move this to data service
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 export const catalog = require('./components/AssetsCatalog/catalog.json') as ITheme[]
