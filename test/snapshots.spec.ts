6803a00ce384839bbd75d2b39873992fcabefaef
Leandro Mendoza <leandro@decentraland.org>
2023-03-10 11:59:19 -0300

    chore: add bundle js size for snapshots (tests)

--- test/snapshots.spec.ts
+++ test/snapshots.spec.ts
@@ -1,5 +1,6 @@
+import { version as vmVersion } from '@dcl/quickjs-emscripten/package.json'
 import { exec } from 'child_process'
-import { existsSync, readFileSync, writeFileSync } from 'fs-extra'
+import { existsSync, readFileSync, stat, writeFileSync } from 'fs-extra'
 import glob from 'glob'
 import path from 'path'
 import { CrdtMessageType, engine } from '../packages/@dcl/ecs/src'
@@ -7,7 +8,6 @@ import { ReadWriteByteBuffer } from '../packages/@dcl/ecs/src/serialization/Byte
 import { CrdtMessage } from '../packages/@dcl/ecs/src/serialization/crdt'
 import { readMessage } from '../packages/@dcl/ecs/src/serialization/crdt/message'
 import { withQuickJsVm } from './vm'
-import { version as vmVersion } from '@dcl/quickjs-emscripten/package.json'
 
 const ENV: Record<string, string> = { ...process.env } as any
 const writeToFile = process.env.UPDATE_SNAPSHOTS
@@ -21,8 +21,21 @@ describe('Runs the snapshots', () => {
 
 function testFileSnapshot(fileName: string, workingDirectory: string) {
   it(`tests the file ${fileName}`, async () => {
+    await compile(fileName, workingDirectory, true)
+    const jsSizeBytesProd = (await stat(fileName.replace(/\.ts$/, '.js'))).size
+    const jsProdSize = (jsSizeBytesProd / 1000).toLocaleString('en', { maximumFractionDigits: 2 })
+
     await compile(fileName, workingDirectory)
-    const { result, leaking } = await run(fileName.replace(/\.ts$/, '.js'))
+    const jsSizeBytesDev = (await stat(fileName.replace(/\.ts$/, '.js'))).size
+    const jsDevSize = (jsSizeBytesDev / 1000).toLocaleString('en', { maximumFractionDigits: 2 })
+
+    const { result: resultFromRun, leaking } = await run(fileName.replace(/\.ts$/, '.js'))
+
+    const result =
+      `SCENE_COMPILED_JS_SIZE_DEV=${jsDevSize}k bytes\n` +
+      `SCENE_COMPILED_JS_SIZE_PROD=${jsProdSize}k bytes\n` +
+      `This run is in DEV mode.\n` +
+      resultFromRun
 
     const compareToFileName = fileName + '.crdt'
     const compareFileExists = existsSync(compareToFileName)
@@ -193,9 +206,14 @@ async function run(fileName: string) {
   })
 }
 
-async function compile(filename: string, workingDirectory: string) {
+async function compile(filename: string, workingDirectory: string, production?: boolean) {
   const cwd = path.resolve(workingDirectory)
-  await runCommand(`npm run build --silent -- --single ${JSON.stringify(path.relative(cwd, filename))}`, cwd, ENV)
+  const producctionBuild = production ? '--production' : ''
+  await runCommand(
+    `npm run build --silent -- ${producctionBuild} --single ${JSON.stringify(path.relative(cwd, filename))}`,
+    cwd,
+    ENV
+  )
 }
 
 export function runCommand(command: string, cwd: string, env?: Record<string, string>): Promise<string> {
