ac99efc276c188ddfdea2ac2d8af9ff0c9dbc245
Leandro Mendoza <leandro@decentraland.org>
2023-03-15 10:53:50 -0300

    fix inspector build

--- packages/@dcl/inspector/src/lib/data-layer/remote-data-layer.ts
+++ packages/@dcl/inspector/src/lib/data-layer/remote-data-layer.ts
@@ -3,4 +3,4 @@
  */
 export { createEngine } from './logic/engine'
 export { initRpcMethods } from './logic/rpc-methods'
-export * as DataLayerProto from './proto'
+export * as DataLayerProto from './proto/gen/data-layer.gen'
