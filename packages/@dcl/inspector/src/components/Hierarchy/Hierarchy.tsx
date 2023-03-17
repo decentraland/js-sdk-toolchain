c36d51e1b01e2ada34c7acccaa1c6269dd5d4f1a
Juan Cazala <juancazala@gmail.com>
2023-03-17 15:25:28 -0300

    fix: prevent re-rendering react every 16ms by using hooks (#473)

--- packages/@dcl/inspector/src/components/Hierarchy/Hierarchy.tsx
+++ packages/@dcl/inspector/src/components/Hierarchy/Hierarchy.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react'
-import { ROOT, useTree } from '../../hooks/sdk/tree'
-import { InspectorEngine } from '../../lib/sdk/engine'
+import { useTree } from '../../hooks/sdk/useTree'
+import { ROOT } from '../../lib/sdk/tree'
 import { Tree } from '../Tree'
 
-type Props = {
-  inspectorEngine: InspectorEngine
-}
-
-const Hierarchy: React.FC<Props> = (props) => {
+const Hierarchy: React.FC = () => {
   const { tree, addChild, setParent, remove, rename, toggle, getId, getChildren, getLabel, isOpen, isSelected } =
-    useTree(props.inspectorEngine)
+    useTree()
   return (
     <>
       {Array.from(tree.get(ROOT)!).map((entity) => (
--- packages/@dcl/inspector/src/index.tsx
+++ packages/@dcl/inspector/src/index.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-import { InspectorEngine } from './lib/sdk/engine'
-
 import { AssetsCatalog } from './components/AssetsCatalog'
 import Hierarchy from './components/Hierarchy/Hierarchy'
+import { SdkProvider } from './components/SdkProvider'
 import { Renderer } from './components/Renderer/Renderer'
 import { useCatalog } from './hooks/catalog/useCatalog'
 
@@ -15,14 +13,14 @@ import './index.css'
 async function initScene() {
   const App = () => {
     const [catalog] = useCatalog()
-    const [inspectorEngine, setInspectorEngine] = useState<InspectorEngine>()
-    const handleOnLoad = (value: InspectorEngine) => setInspectorEngine(value)
 
     return (
       <>
-        <div className="left">{inspectorEngine && <Hierarchy inspectorEngine={inspectorEngine} />}</div>
+        <div className="left">
+          <Hierarchy />
+        </div>
         <div className="right">
-          <Renderer onLoad={handleOnLoad} />
+          <Renderer />
           {catalog && <AssetsCatalog value={catalog} />}
         </div>
       </>
@@ -32,7 +30,9 @@ async function initScene() {
   const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
   root.render(
     <DndProvider backend={HTML5Backend}>
-      <App />
+      <SdkProvider>
+        <App />
+      </SdkProvider>
     </DndProvider>
   )
 }
