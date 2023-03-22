569ecab7d901f0b4508fac4a189890039418f39d
Juan Cazala <juancazala@gmail.com>
2023-03-22 12:37:48 -0300

    fix: prevent cycles in tree + tests (#475)

--- packages/@dcl/inspector/src/lib/sdk/tree.ts
+++ packages/@dcl/inspector/src/lib/sdk/tree.ts
@@ -8,11 +8,34 @@ export const ROOT = 0 as Entity
  */
 export const getTreeFromEngine = (engine: IEngine, Transform: TransformComponent) => {
   // We build a map of children by their parent entity
-  const childrenByParent = new Map<Entity, Set<Entity>>()
-  childrenByParent.set(ROOT, new Set<Entity>())
+  const childrenByParent = getEmptyTree()
 
   // This is a map of parent -> orphans[], it's used to keep track of orphans until the parent shows up
   const orphansByParent = new Map<Entity, Set<Entity>>()
+
+  // This map allows to easily traverse the parent chain to detect cycles
+  const parentByEntity = new Map<Entity, Entity>()
+
+  // Helper to set a parent
+  function setParent(entity: Entity, parent: Entity) {
+    if (!childrenByParent.has(parent)) {
+      throw Error(`Entity ${parent} has not been processed yet`)
+    }
+    childrenByParent.get(parent)!.add(entity)
+    parentByEntity.set(entity, parent)
+  }
+
+  // Helper to check if an entity is an ancestor (parent or parent of parent, etc) of another entity
+  function isAncestor(entity: Entity, ancestor: Entity) {
+    let current = entity
+    while (parentByEntity.has(current)) {
+      current = parentByEntity.get(current)!
+      if (current === ancestor) {
+        return true
+      }
+    }
+    return false
+  }
 
   // Get all components in the engine
   for (const component of engine.componentsIter()) {
@@ -28,12 +51,12 @@ export const getTreeFromEngine = (engine: IEngine, Transform: TransformComponent
       if (Transform.has(entity)) {
         const transform = Transform.get(entity)
         const parent = transform.parent || ROOT
-        // If the parent has already been processed we link to it
-        if (childrenByParent.has(parent)) {
-          childrenByParent.get(parent)!.add(entity)
+        // If the parent has already been processed we set it as parent of the current entity as long as it does not create a cycle
+        if (childrenByParent.has(parent) && !isAncestor(entity, parent)) {
+          setParent(entity, parent)
         } else {
           // If the parent is not known we link to the root (entity=0). This could happen because the parent has not been processed yet, or because it was deleted
-          childrenByParent.get(ROOT)!.add(entity)
+          setParent(entity, ROOT)
           // We flag the entity as an orphan for later processing if the parent is processed afterwards
           if (orphansByParent.has(parent)) {
             orphansByParent.get(parent)!.add(entity)
@@ -52,11 +75,15 @@ export const getTreeFromEngine = (engine: IEngine, Transform: TransformComponent
       if (orphansByParent.has(entity)) {
         const orphans = Array.from(orphansByParent.get(entity)!)
         for (const orphan of orphans) {
-          // Add child to parent's children
-          childrenByParent.get(entity)!.add(orphan)
-          // Delete child from root's children
+          if (isAncestor(entity, orphan)) {
+            // If the orphan is already an ancestor of the entity, we skip it otherwise we would create a cycle
+            continue
+          }
+          // Add orphan to parent's children
+          setParent(orphan, entity)
+          // Delete orphan from root's children
           childrenByParent.get(ROOT)!.delete(orphan)
-          // Delete child from orphans list
+          // Delete orphan from orphans list
           orphansByParent.get(entity)!.delete(orphan)
         }
       }
