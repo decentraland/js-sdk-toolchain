6ac4dddceb4b4d44a122f093ac6e98dcdb170f3d
menduz <github@menduz.com>
2023-03-28 11:37:03 -0300

    coverage

--- jest.config.js
+++ jest.config.js
@@ -19,33 +19,6 @@ module.exports = {
       functions: 100,
       lines: 100,
       statements: 100,
-    },
-    "packages/@dcl/ecs/src/components/generated/pb": {
-      branches: 1,
-      functions: 1,
-      lines: 1,
-      statements: 1
-    },
-    "packages/@dcl/ecs/src/composite/proto/gen": {
-      branches: 1,
-      functions: 1,
-      lines: 1,
-      statements: 1
-    },
-    "packages/@dcl/ecs/src/components/generated/index.gen.ts": {
-      functions: 10,
-    },
-    "packages/@dcl/sdk/src/internal": {
-      branches: 26,
-      functions: 26,
-      lines: 26,
-      statements: 26
-    },
-    "packages/@dcl/sdk-commands": {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
     }
   },
   coveragePathIgnorePatterns: [
