b3e7c6ce49211755a070d00b697b4714301f0103
menduz <github@menduz.com>
2023-04-05 16:02:05 -0300

    merge from main

--- test/sdk-commands/blackbox/build.spec.ts
+++ test/sdk-commands/blackbox/build.spec.ts
@@ -9,8 +9,8 @@ describe('blackbox: build', () => {
 
   test('build integration test with single scene', async () => {
     const components = await initComponents()
-    await runSdkCommand(components, 'build', ['--dir=test/build-ecs/fixtures/ecs7-scene'])
-  })
+    await runSdkCommand(components, 'build', ['--dir=test/build-ecs/fixtures/ecs7-scene', '--skip-install'])
+  }, 15000)
 
   test('build integration test with --single file', async () => {
     const components = await initComponents()
@@ -18,7 +18,7 @@ describe('blackbox: build', () => {
       '--dir=test/snapshots',
       '--single=development-bundles/testing-fw.test.ts'
     ])
-  })
+  }, 15000)
 
   test('build integration test with --single file --production', async () => {
     const components = await initComponents()
@@ -27,10 +27,10 @@ describe('blackbox: build', () => {
       '--production',
       '--single=development-bundles/testing-fw.test.ts'
     ])
-  })
+  }, 15000)
 
   test('build integration test with --single wildcard', async () => {
     const components = await initComponents()
     await runSdkCommand(components, 'build', ['--dir=test/snapshots', '--single=development-bundles/*.test.ts'])
-  })
+  }, 15000)
 })
