18bcb7d405715af29d96f2451a184801fbe60048
menduz <github@menduz.com>
2023-04-05 16:04:38 -0300

    export-static must happen after build

--- test/sdk-commands/blackbox/build-and-export-static.spec.ts
+++ test/sdk-commands/blackbox/build-and-export-static.spec.ts
@@ -1,10 +1,42 @@
-import path from 'path'
 import { initComponents } from '../../../packages/@dcl/sdk-commands/src/components'
-import { Entity } from '../../../packages/@dcl/sdk-commands/node_modules/@dcl/schemas/dist'
+import { Entity } from '../../../packages/@dcl/sdk-commands/node_modules/@dcl/schemas'
 import { runSdkCommand } from '../../../packages/@dcl/sdk-commands/src/run-command'
 import rimraf from 'rimraf'
+import path from 'path'
 
-describe('blackbox: export-static', () => {
+describe('blackbox: build', () => {
+  test('build integration test with workspace', async () => {
+    const components = await initComponents()
+    await runSdkCommand(components, 'build', ['--dir=test/build-ecs/fixtures'])
+  })
+
+  test('build integration test with single scene', async () => {
+    const components = await initComponents()
+    await runSdkCommand(components, 'build', ['--dir=test/build-ecs/fixtures/ecs7-scene', '--skip-install'])
+  }, 15000)
+
+  test('build integration test with --single file', async () => {
+    const components = await initComponents()
+    await runSdkCommand(components, 'build', [
+      '--dir=test/snapshots',
+      '--single=development-bundles/testing-fw.test.ts'
+    ])
+  }, 15000)
+
+  test('build integration test with --single file --production', async () => {
+    const components = await initComponents()
+    await runSdkCommand(components, 'build', [
+      '--dir=test/snapshots',
+      '--production',
+      '--single=development-bundles/testing-fw.test.ts'
+    ])
+  }, 15000)
+
+  test('build integration test with --single wildcard', async () => {
+    const components = await initComponents()
+    await runSdkCommand(components, 'build', ['--dir=test/snapshots', '--single=development-bundles/*.test.ts'])
+  }, 15000)
+
   test('export-static integration test', async () => {
     const components = await initComponents()
 
