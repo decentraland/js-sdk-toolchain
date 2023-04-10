4877cbb572317b224004a4ad284c999705a0592e
Lean Mendoza <leandro@decentraland.org>
2023-04-10 17:17:14 -0300

    add threshold

--- packages/@dcl/inspector/jest.config.js
+++ packages/@dcl/inspector/jest.config.js
@@ -8,6 +8,14 @@ module.exports = {
   },
   coverageDirectory: "coverage",
   coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
+    }
+  },
   collectCoverageFrom: ["src/**/*.ts"],
   verbose: true,
   testMatch: ["**/*.spec.(ts)"],
