f3782dbf416ba92dd89fc6e738165c9ed3997984
gonpombo8 <gonzalo@decentraland.org>
2023-04-20 11:51:50 -0300

    mock XMLHttpRequest

--- packages/@dcl/inspector/src/lib/babylon/setup/setup.spec.ts
+++ packages/@dcl/inspector/src/lib/babylon/setup/setup.spec.ts
@@ -2,9 +2,19 @@ import { NullEngine } from '@babylonjs/core/Engines/nullEngine'
 import { setupEngine } from '.'
 
 describe('When setting up the engine', () => {
+  const xhrMockClass = () => ({
+    open            : jest.fn(),
+    send            : jest.fn(),
+    setRequestHeader: jest.fn(),
+    addEventListener: jest.fn()
+  })
+
+  ;(globalThis as any).XMLHttpRequest = jest.fn().mockImplementation(xhrMockClass)
+
   it('should create a scene', () => {
     const engine = new NullEngine()
     const { scene } = setupEngine(engine)
     expect(scene).toBeDefined()
+    engine.stopRenderLoop()
   })
 })
