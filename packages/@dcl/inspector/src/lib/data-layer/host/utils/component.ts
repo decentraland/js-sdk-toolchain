22d51e422faa23e58e064e4c262342d7a6ad1864
Gon Pombo <gonzalo@decentraland.org>
2023-04-28 19:26:49 -0300

    add undo-redo for file system (#550)
    * add undo-redo for file system
    
    * fix undo/redo for gltfs
    
    * fix undo-redo for file system
    
    * remove unnecessary condition
    
    ---------
    
    Co-authored-by: menduz <github@menduz.com>

---
+++ packages/@dcl/inspector/src/lib/data-layer/host/utils/component.ts
@@ -0,0 +1,22 @@
+import { ComponentDefinition, CompositeDefinition, DeepReadonlyObject, Entity } from '@dcl/ecs'
+import { ReadWriteByteBuffer } from '@dcl/ecs/dist/serialization/ByteBuffer'
+import { dataCompare } from '@dcl/ecs/dist/systems/crdt/utils'
+
+export function isEqual(component: ComponentDefinition<unknown>, prevValue: unknown, newValue: unknown) {
+  if (prevValue === newValue || (!prevValue && !newValue)) return true
+  if ((!prevValue && newValue) || (prevValue && !newValue)) return false
+  const prevBuffer = new ReadWriteByteBuffer()
+  const newBuffer = new ReadWriteByteBuffer()
+  component.schema.serialize(prevValue as DeepReadonlyObject<unknown>, prevBuffer)
+  component.schema.serialize(newValue as DeepReadonlyObject<unknown>, newBuffer)
+  return dataCompare(prevBuffer.toBinary(), newBuffer.toBinary()) === 0
+}
+
+export function findPrevValue(composite: CompositeDefinition, componentName: string, entity: Entity) {
+  const component = composite.components.find((c) => c.name === componentName)
+  const value = component?.data.get(entity)
+  if (value?.data?.$case !== 'json') {
+    return null
+  }
+  return value.data.json
+}
