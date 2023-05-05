c3ff6fc35bcc9055dad70f52e06e275eaad47466
Pravus <pravusjif@gmail.com>
2023-05-05 14:51:37 +0200

    added video event serialization test

---
+++ test/ecs/components/VideoEvent.spec.ts
@@ -0,0 +1,30 @@
+import { components, Engine, VideoState } from '../../../packages/@dcl/ecs/src'
+import { testSchemaSerializationIdentity } from './assertion'
+
+describe('Generated VideoEvent ProtoBuf', () => {
+  it('should serialize', () => {
+    const newEngine = Engine()
+    const VideoEvent = components.VideoEvent(newEngine)
+
+    testSchemaSerializationIdentity(VideoEvent.schema, {
+      timestamp: 5,
+      state: VideoState.VS_LOADING,
+      videoLength: 50,
+      currentOffset: 1
+    })
+
+    testSchemaSerializationIdentity(VideoEvent.schema, {
+      timestamp: 10,
+      state: VideoState.VS_PLAYING,
+      videoLength: 50,
+      currentOffset: 3
+    })
+
+    testSchemaSerializationIdentity(VideoEvent.schema, {
+      timestamp: 30,
+      state: VideoState.VS_PLAYING,
+      videoLength: 50,
+      currentOffset: 13
+    })
+  })
+})
