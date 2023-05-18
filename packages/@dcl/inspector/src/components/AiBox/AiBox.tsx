9eb2bb76c976101814445a3ad0db8d5c61170c17
Braian Mellor <braianj@gmail.com>
2023-05-18 18:13:30 -0300

    feat: update loading animation

--- packages/@dcl/inspector/src/components/AiBox/AiBox.tsx
+++ packages/@dcl/inspector/src/components/AiBox/AiBox.tsx
@@ -1,29 +1,35 @@
 /* eslint-disable no-console */
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { RxCross2 } from 'react-icons/rx'
 import { IoIosImage } from 'react-icons/io'
-import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
+
+import { Player } from '@lottiefiles/react-lottie-player'
+import { Dimmer } from 'decentraland-ui/dist/components/Dimmer/Dimmer'
 
 import { withSdk } from '../../hoc/withSdk'
 import { Container } from '../Container'
 import { TextField } from '../EntityInspector/TextField'
-import { Block } from '../Block'
 import Button from '../Button'
-import { useFileSystem } from '../../hooks/catalog/useFileSystem'
+
+import loadingJson from './loading.json'
 
 import './AiBox.css'
-import classNames from 'classnames'
-import { Dimmer } from 'decentraland-ui/dist/components/Dimmer/Dimmer'
 
 interface PropTypes {
   onSave(): void
 }
 
+type PlayerFix = React.Component
+
+const PlayerFixed = Player as any as {
+  new (): PlayerFix
+}
+
 const AiBox = withSdk<PropTypes>(({ sdk, onSave }) => {
   const [file, setFile] = useState<File>()
   const [assetPackageName, setAssetPackageName] = useState<string>('')
-  const [systemFiles] = useFileSystem()
   const [value, setValue] = useState<string>('')
+  const [error, setError] = useState<boolean>(false)
   const [isLoading, setLoading] = useState(false)
 
   const destFolder = 'assets/'
@@ -34,9 +40,6 @@ const AiBox = withSdk<PropTypes>(({ sdk, onSave }) => {
       const binary: ArrayBuffer = reader.result as ArrayBuffer
       const content: Map<string, Uint8Array> = new Map()
       content.set(file.name, new Uint8Array(binary))
-
-      console.log(content)
-      console.log(destFolder)
 
       await sdk!.dataLayer.importAsset({
         content,
@@ -59,37 +62,63 @@ const AiBox = withSdk<PropTypes>(({ sdk, onSave }) => {
       /* const response = await fetch(`https://2218-181-13-71-243.sa.ngrok.io/prompt?text=${value}`, {
         method: 'GET'
       }) */
-      const filename = value.replace(/\s+/gi, '-')
-      setLoading(true)
-      const response = await fetch(`http://0.0.0.0:3000/render?prompt=${encodeURI(value)}&sampler=shap_e`)
-      const blob = (await response.blob()) as File
-      const file = new File([blob], `${filename}.glb`)
-      setLoading(false)
-      setFile(file)
-      setAssetPackageName(`ai`)
+      try {
+        const filename = value.replace(/\s+/gi, '-')
+        setLoading(true)
+        setError(false)
+        const response = await fetch(`http://localhost:3000/render?prompt=${encodeURI(value)}&sampler=shap_e`)
+        const blob = (await response.blob()) as File
+        const file = new File([blob], `${filename}.glb`)
+        setLoading(false)
+        setFile(file)
+        setAssetPackageName(`ai`)
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+        setError(true)
+      }
     }
   }, [])
+
+  const playerProps = useMemo(() => {
+    return {
+      autoplay: true,
+      loop: true,
+      Controls: false,
+      src: loadingJson,
+      style: { height: '300px', width: '300px' }
+    }
+  }, [loadingJson])
 
   return (
     <div className="AiBox">
       {!file && (
         <div className="ai-box__container">
           <div className="row">
-            <TextField
-              label="Prompt"
-              type="text"
-              value={value}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value)}
-            />
-            <Button onClick={() => submitAiRequest(value)}>Submit</Button>
+            {!isLoading && (
+              <>
+                <TextField
+                  label="Prompt"
+                  type="text"
+                  value={value}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value)}
+                />
+                <Button onClick={() => submitAiRequest(value)}>Submit</Button>
+              </>
+            )}
             {isLoading && (
               <>
                 <Dimmer active>
-                  <Loader active />
+                  <PlayerFixed {...playerProps} />
                 </Dimmer>
               </>
             )}
           </div>
+          {error && (
+            <div className="row">
+              <label className="ai-box__error">There is an issue, try later...</label>
+            </div>
+          )}
         </div>
       )}
       {file && (
