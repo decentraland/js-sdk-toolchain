156f895e3ded33abad01d6970cd7ba8df5476b34
AN-DCL <aleksei.nokhrin@decentraland.org>
2023-06-03 19:05:52 +0400

    Add tests for preferences IO.

---
+++ packages/@dcl/inspector/src/lib/logic/preferences/io.spec.ts
@@ -0,0 +1,74 @@
+import {
+  parseInspectorPreferences,
+  readPreferencesFromFile,
+  InvalidPreferences,
+  serializeInspectorPreferences
+} from './io'
+import { getDefaultInspectorPreferences } from './types'
+import { createFsInMemory } from '../in-memory-storage'
+
+describe('Parsing inspector preferences', () => {
+  it('throws InvalidPreferences on malformed JSON', () => {
+    expect(() => {
+      parseInspectorPreferences('{"value":}')
+    }).toThrow(InvalidPreferences)
+  })
+
+  it('throws InvalidPreferences if shell is malformed', () => {
+    expect(() => {
+      parseInspectorPreferences('{"version": 1}')
+    }).toThrow(InvalidPreferences)
+  })
+
+  it('throws InvalidPreferences if version is not set to 1', () => {
+    expect(() => {
+      parseInspectorPreferences('{"version": 2, "data": {}}')
+    }).toThrow(InvalidPreferences)
+  })
+
+  it('throws InvalidPreferences if v1 data is invalid', () => {
+    expect(() => {
+      parseInspectorPreferences('{"version": 1, "data": {"cameraInvertXAxis": 1}}')
+    }).toThrow(InvalidPreferences)
+  })
+
+  it('correctly parses v1 data and returns full set of preferences even if some are omitted in input', () => {
+    const preferences = getDefaultInspectorPreferences()
+    preferences.cameraInvertXAxis = true
+    expect(parseInspectorPreferences('{"version": 1, "data": {"cameraInvertXAxis": true}}')).toEqual(preferences)
+  })
+})
+
+describe('Reading preferences file', () => {
+  const memoryFs = createFsInMemory({
+    goodFile: Buffer.from('{"version": 1, "data": {"cameraInvertXAxis": true}}'),
+    badFile: Buffer.from('{"value":}')
+  })
+
+  it('returns default preferences if file doesnt exist', async () => {
+    const preferences = getDefaultInspectorPreferences()
+    expect(await readPreferencesFromFile(memoryFs, 'noneFile')).toEqual(preferences)
+  })
+
+  it('returns default preferences if file is malformed', async () => {
+    const preferences = getDefaultInspectorPreferences()
+    expect(await readPreferencesFromFile(memoryFs, 'badFile')).toEqual(preferences)
+  })
+
+  it('returns correct preferences if file is well-formed', async () => {
+    const preferences = getDefaultInspectorPreferences()
+    preferences.cameraInvertXAxis = true
+    expect(await readPreferencesFromFile(memoryFs, 'goodFile')).toEqual(preferences)
+  })
+})
+
+describe('Writing preferences file', () => {
+  const memoryFs = createFsInMemory({})
+
+  it('correctly writes preferences to a file', async () => {
+    const preferences = getDefaultInspectorPreferences()
+    preferences.cameraInvertXAxis = true
+    await memoryFs.writeFile('goodFile', serializeInspectorPreferences(preferences))
+    expect(await readPreferencesFromFile(memoryFs, 'goodFile')).toEqual(preferences)
+  })
+})
