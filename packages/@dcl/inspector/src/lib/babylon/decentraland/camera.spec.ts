b4b63bef1c080fd474cee27944269cf963a0846f
AN-DCL <aleksei.nokhrin@decentraland.org>
2023-06-07 22:25:08 +0400

    Lint CameraManager's test.

--- packages/@dcl/inspector/src/lib/babylon/decentraland/camera.spec.ts
+++ packages/@dcl/inspector/src/lib/babylon/decentraland/camera.spec.ts
@@ -1,40 +1,46 @@
 import * as BABYLON from '@babylonjs/core'
-import { SceneContext } from './SceneContext'
-import { TransformComponentExtended } from '@dcl/ecs'
-import { Operations } from '../../sdk/operations'
 import { CameraManager } from './camera'
 
 describe('Camera manager', () => {
   let engine: BABYLON.Engine
   let scene: BABYLON.Scene
-  let context: SceneContext
   beforeEach(() => {
     engine = new BABYLON.NullEngine()
     scene = new BABYLON.Scene(engine)
-    context = {
-      scene,
-      operations: {
-        updateValue: jest.fn(),
-        dispatch: jest.fn()
-      } as unknown as Operations,
-      Transform: { getOrNull: jest.fn() } as unknown as TransformComponentExtended
-    } as SceneContext
   })
 
   describe('when changing camera speed via mouse input', () => {
     let cameraManager: CameraManager
-    let mouseDown = {type: BABYLON.PointerEventTypes.POINTERDOWN, event: {pointerType: "mouse"}} as unknown as BABYLON.PointerInfo
-    let mouseWheelUp = {type: BABYLON.PointerEventTypes.POINTERWHEEL, event: {deltaY: -240}} as unknown as BABYLON.PointerInfo
-    let mouseWheelDown = {type: BABYLON.PointerEventTypes.POINTERWHEEL, event: {deltaY: 240}} as unknown as BABYLON.PointerInfo         
+    let mouseDown = {
+      type: BABYLON.PointerEventTypes.POINTERDOWN,
+      event: { pointerType: 'mouse' }
+    } as unknown as BABYLON.PointerInfo
+    let mouseWheelUp = {
+      type: BABYLON.PointerEventTypes.POINTERWHEEL,
+      event: { deltaY: -240 }
+    } as unknown as BABYLON.PointerInfo
+    let mouseWheelDown = {
+      type: BABYLON.PointerEventTypes.POINTERWHEEL,
+      event: { deltaY: 240 }
+    } as unknown as BABYLON.PointerInfo
 
     beforeEach(() => {
       cameraManager = new CameraManager(scene, document.createElement('canvas'), [0, 1, 2], 1, 1, 1)
-      mouseDown = {type: BABYLON.PointerEventTypes.POINTERDOWN, event: {pointerType: "mouse"}} as unknown as BABYLON.PointerInfo
-      mouseWheelUp = {type: BABYLON.PointerEventTypes.POINTERWHEEL, event: {deltaY: -240}} as unknown as BABYLON.PointerInfo
-      mouseWheelDown = {type: BABYLON.PointerEventTypes.POINTERWHEEL, event: {deltaY: 240}} as unknown as BABYLON.PointerInfo         
+      mouseDown = {
+        type: BABYLON.PointerEventTypes.POINTERDOWN,
+        event: { pointerType: 'mouse' }
+      } as unknown as BABYLON.PointerInfo
+      mouseWheelUp = {
+        type: BABYLON.PointerEventTypes.POINTERWHEEL,
+        event: { deltaY: -240 }
+      } as unknown as BABYLON.PointerInfo
+      mouseWheelDown = {
+        type: BABYLON.PointerEventTypes.POINTERWHEEL,
+        event: { deltaY: 240 }
+      } as unknown as BABYLON.PointerInfo
     })
     it('should start with correct default speed', () => {
-      expect(cameraManager.getSpeed()).toBe(1)  
+      expect(cameraManager.getSpeed()).toBe(1)
     })
     it('should not change speed if mouse button is not down', () => {
       scene.onPointerObservable.notifyObservers(mouseWheelDown)
