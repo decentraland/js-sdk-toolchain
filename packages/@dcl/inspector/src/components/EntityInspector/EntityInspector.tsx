d663f010d7b28be16d1630a25b8630c89d6ea5d1
AN-DCL <aleksei.nokhrin@decentraland.org>
2023-06-12 20:44:50 +0400

    Refactor EntityInspector.

--- packages/@dcl/inspector/src/components/EntityInspector/EntityInspector.tsx
+++ packages/@dcl/inspector/src/components/EntityInspector/EntityInspector.tsx
@@ -2,30 +2,30 @@ import { Entity } from '@dcl/ecs'
 
 import { useSelectedEntity } from '../../hooks/sdk/useSelectedEntity'
 import { withSdk } from '../../hoc/withSdk'
+import { SdkContextValue } from '../../lib/sdk/context'
 
 import { SceneInspector } from './SceneInspector'
 import { TransformInspector } from './TransformInspector'
 import { GltfInspector } from './GltfInspector'
 import './EntityInspector.css'
 
+const getLabel = (sdk: SdkContextValue, entity: Entity) => {
+  const nameComponent = sdk.components.Name.getOrNull(entity)
+  return entity === 0 ? 'Scene' : (nameComponent ? nameComponent.value : entity.toString())
+}
+
 export const EntityInspector = withSdk(({ sdk }) => {
   const entity = useSelectedEntity()
+  const inspectors = [SceneInspector, TransformInspector, GltfInspector]
 
-  if (entity !== null) {
-    const nameComponent = sdk.components.Name.getOrNull(entity)
-    const label = entity === 0 as Entity ? 'Scene' : (nameComponent ? nameComponent.value : entity.toString())
+  const label = entity !== null ? getLabel(sdk, entity) : null
 
-    const inspectors = [SceneInspector, TransformInspector, GltfInspector]
-
-    return (
-      <div className="EntityInspector" key={entity}>
-        <div className="entity-label">{label}</div>
-        {inspectors.map((Inspector, index) => entity !== null && <Inspector key={index} entity={entity} />)}
-      </div>
-    )
-  } else {
-    return <div className="EntityInspector"></div>
-  }
+  return (
+    <div className="EntityInspector" key={entity}>
+      <div className="entity-label">{label}</div>
+      {inspectors.map((Inspector, index) => entity !== null && <Inspector key={index} entity={entity} />)}
+    </div>
+  )
 })
 
 export default EntityInspector
