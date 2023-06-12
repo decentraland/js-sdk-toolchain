25fdfdd506b4d7dd430a0ea7bece05d5cb24b6c9
AN-DCL <aleksei.nokhrin@decentraland.org>
2023-06-12 21:42:23 +0400

    Entity inspector should reflect entity's renaming.

--- packages/@dcl/inspector/src/components/EntityInspector/EntityInspector.tsx
+++ packages/@dcl/inspector/src/components/EntityInspector/EntityInspector.tsx
@@ -1,24 +1,39 @@
+import { useEffect, useState } from 'react'
+
 import { Entity } from '@dcl/ecs'
 
 import { useSelectedEntity } from '../../hooks/sdk/useSelectedEntity'
 import { withSdk } from '../../hoc/withSdk'
-import { SdkContextValue } from '../../lib/sdk/context'
+import { SdkContextEvents, SdkContextValue } from '../../lib/sdk/context'
+import { useChange } from '../../hooks/sdk/useChange'
 
 import { SceneInspector } from './SceneInspector'
 import { TransformInspector } from './TransformInspector'
 import { GltfInspector } from './GltfInspector'
 import './EntityInspector.css'
 
-const getLabel = (sdk: SdkContextValue, entity: Entity) => {
+const getLabel = (sdk: SdkContextValue, entity: Entity | null) => {
+  if (entity === null) return null
   const nameComponent = sdk.components.Name.getOrNull(entity)
-  return entity === 0 ? 'Scene' : (nameComponent ? nameComponent.value : entity.toString())
+  return entity === 0 ? 'Scene' : (nameComponent && nameComponent.value.length > 0 ? nameComponent.value : entity.toString())
 }
 
 export const EntityInspector = withSdk(({ sdk }) => {
   const entity = useSelectedEntity()
-  const inspectors = [SceneInspector, TransformInspector, GltfInspector]
+  const [label, setLabel] = useState<string | null>()
 
-  const label = entity !== null ? getLabel(sdk, entity) : null
+  useEffect(() => {
+    setLabel(getLabel(sdk, entity))
+  }, [sdk, entity])
+
+  const handleUpdate = (event: SdkContextEvents['change']) => {
+    if (event.entity === entity && event.component === sdk.components.Name) {
+      setLabel(getLabel(sdk, entity))
+    }
+  }
+  useChange(handleUpdate, [entity])
+
+  const inspectors = [SceneInspector, TransformInspector, GltfInspector]
 
   return (
     <div className="EntityInspector" key={entity}>
