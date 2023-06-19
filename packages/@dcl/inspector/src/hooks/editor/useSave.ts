62f0e28bc053f5c30c2b85a34a3568723b3dd8e9
Gonzalo DCL <gonzalo@decentraland.org>
2023-06-19 16:17:03 -0300

    fix tests

--- packages/@dcl/inspector/src/hooks/editor/useSave.ts
+++ packages/@dcl/inspector/src/hooks/editor/useSave.ts
@@ -3,12 +3,14 @@ import { useCallback, useState } from 'react'
 
 import { useAppSelector } from '../../redux/hooks'
 import { getDataLayer } from '../../redux/data-layer'
+import { useSdk } from '../sdk/useSdk'
 
 type SaveEvent = { change: boolean }
 export const saveEvent = mitt<SaveEvent>()
 
 export const useSave = (): [() => Promise<void>, boolean] => {
   const dataLayer = useAppSelector(getDataLayer)
+  const sdk = useSdk()
   const [isDirty, setIsDirty] = useState(false)
 
   const saveFn = useCallback(async () => {
