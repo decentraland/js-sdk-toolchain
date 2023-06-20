feaecfe635e033494d42452ba6b317f564631552
AN-DCL <120640942+AN-DCL@users.noreply.github.com>
2023-06-20 17:54:51 +0400

    fix: gizmos sometimes not updating Transform (#655)

--- packages/@dcl/inspector/src/lib/logic/math.ts
+++ packages/@dcl/inspector/src/lib/logic/math.ts
@@ -1,4 +1,5 @@
 import { Entity, TransformComponentExtended } from '@dcl/ecs'
+import { Quaternion, Vector3, Epsilon } from '@dcl/ecs-math'
 import { Matrix } from '@dcl/ecs-math/dist/Matrix'
 
 export function getWorldMatrix(entity: Entity, transformComponent: TransformComponentExtended): Matrix.ReadonlyMatrix {
@@ -9,4 +10,24 @@ export function getWorldMatrix(entity: Entity, transformComponent: TransformComp
   if (!transform.parent) return localMatrix
 
   return Matrix.multiply(localMatrix, getWorldMatrix(transform.parent, transformComponent))
+}
+
+export function areSRTMatrixesEqualWithEpsilon(m1: Matrix.ReadonlyMatrix, m2: Matrix.ReadonlyMatrix) {
+  const tOut1 = Vector3.create()
+  const sOut1 = Vector3.create()
+  const rOut1 = Quaternion.create()
+
+  if (!Matrix.decompose(m1, sOut1, rOut1, tOut1)) throw new Error(`${Matrix.toArray(m1)} is not an SRT matrix`)
+
+  const tOut2 = Vector3.create()
+  const sOut2 = Vector3.create()
+  const rOut2 = Quaternion.create()
+
+  if (!Matrix.decompose(m2, sOut2, rOut2, tOut2)) throw new Error(`${Matrix.toArray(m2)} is not an SRT matrix`)
+
+  return (
+    Vector3.equalsWithEpsilon(tOut1, tOut2) &&
+    Vector3.equalsWithEpsilon(sOut1, sOut2) &&
+    Math.abs(Quaternion.angle(rOut1, rOut2)) < Epsilon
+  )
 }
