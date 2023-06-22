537950eea5c48fb6b67917a28f2c80413e53fb1e
Gonzalo DCL <gonzalo@decentraland.org>
2023-06-22 09:15:49 -0300

    wip normalize paths

--- packages/@dcl/sdk-commands/src/commands/start/data-layer/fs.ts
+++ packages/@dcl/sdk-commands/src/commands/start/data-layer/fs.ts
@@ -2,32 +2,48 @@ import { FileSystemInterface } from '@dcl/inspector'
 import path from 'path'
 import { CliComponents } from '../../../components'
 
+/**
+ * Normalizes a path based on the platform.
+ * We managed posix paths, so for windows we need to normalize it
+ */
+function normalizePath(value: string): string {
+  return path.normalize(value)
+}
+
+/**
+ * Convert paths to posix stlye
+ * .i.e: scene\\assets\\main.composite -> scene/assets/main.composite
+ */
+function pathToPosix(value: string): string {
+  return normalizePath(value).replace(/\\/g, '/')
+}
+
 export function createFileSystemInterfaceFromFsComponent({ fs }: Pick<CliComponents, 'fs'>): FileSystemInterface {
   return {
-    dirname(_path: string): string {
-      return path.dirname(_path)
+    dirname(value: string): string {
+      return pathToPosix(path.dirname(value))
     },
-    basename(_path: string): string {
-      return path.basename(_path)
+    basename(value: string): string {
+      return pathToPosix(path.basename(normalizePath(value)))
     },
     join(...paths: string[]): string {
       return path.join(...paths)
     },
     async existFile(filePath: string): Promise<boolean> {
-      return fs.fileExists(filePath)
+      return fs.fileExists(normalizePath(filePath))
     },
     async readFile(filePath: string): Promise<Buffer> {
-      return fs.readFile(filePath)
+      return fs.readFile(normalizePath(filePath))
     },
     async writeFile(filePath: string, content: Buffer): Promise<void> {
-      const folder = path.dirname(filePath)
+      const folder = normalizePath(path.dirname(filePath))
       if (!(await fs.directoryExists(folder))) {
         await fs.mkdir(folder, { recursive: true })
       }
-      await fs.writeFile(filePath, content)
+      await fs.writeFile(normalizePath(filePath), content)
     },
     async rm(filePath: string) {
-      await fs.rm(filePath)
+      await fs.rm(normalizePath(filePath))
     },
     async readdir(dirPath: string): Promise<{ name: string; isDirectory: boolean }[]> {
       if (dirPath.indexOf('/../') !== -1) {
@@ -40,13 +56,13 @@ export function createFileSystemInterfaceFromFsComponent({ fs }: Pick<CliCompone
       const result = await fs.readdir(resolvedPath)
       return Promise.all(
         result.map(async (name) => ({
-          name: name,
+          name: pathToPosix(name),
           isDirectory: await fs.directoryExists(path.resolve(dirPath, name))
         }))
       )
     },
     cwd(): string {
-      return path.basename(process.cwd())
+      return pathToPosix(path.basename(process.cwd()))
     }
   }
 }
