d479c73a3aa6fe2391acc95837e059dfb89b6ec9
Juan Cazala <juancazala@gmail.com>
2023-06-27 13:51:22 -0300

    fix: remove unused var

--- e2e.bash
+++ e2e.bash
@@ -2,6 +2,6 @@ npx http-server packages/@dcl/inspector/public -p 8000 &
 sleep 2
 server_pid=$!
 echo "server_pid=$server_pid"
-cd ./packages/@dcl/inspector && IS_E2E=true ./../../../node_modules/.bin/jest --detectOpenHandles --colors --config ./jest.config.js $(FILES)
+cd ./packages/@dcl/inspector && IS_E2E=true ./../../../node_modules/.bin/jest --detectOpenHandles --colors --config ./jest.config.js
 kill $server_pid
 sleep 1
