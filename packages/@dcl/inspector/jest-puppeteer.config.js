2a0d5dcaa1a3ac13ce262065d0621ac21d068af9
Juan Cazala <juancazala@gmail.com>
2023-06-28 15:27:32 -0300

    chore: headless

--- packages/@dcl/inspector/jest-puppeteer.config.js
+++ packages/@dcl/inspector/jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   launch: {
-    headless: false,//'new', // Change this to false to run the test on a non-headless Chrome
+    headless: 'new', // Change this to false to run the test on a non-headless Chrome
     // slowMo: 100
   },
   browserContext: 'default',
