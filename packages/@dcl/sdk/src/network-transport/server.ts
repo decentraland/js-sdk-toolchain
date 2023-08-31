1291a2250c989ff0f6a46a38e254a55b91681630
Gonzalo DCL <gonzalo@decentraland.org>
2023-08-31 18:15:18 -0300

    add network transport

---
+++ packages/@dcl/sdk/src/network-transport/server.ts
@@ -0,0 +1,58 @@
+import { engine, Transport } from '@dcl/ecs'
+import { serializeCrdtMessages } from '../internal/transports/logger'
+import { engineToCrdt } from './state'
+import { syncFilter, createNetworkEntityFactory } from './utils'
+import { NetworkEntityFactory } from './types'
+
+const connectedClients = new Set<string>()
+
+export async function createServerTransport(): Promise<NetworkEntityFactory> {
+  engine.addTransport({
+    send: async (message) => {
+      if (message.byteLength) {
+        globalThis.updateCRDTState(engineToCrdt())
+      }
+    },
+    filter: syncFilter
+  })
+  globalThis.registerClientObserver((event) => {
+    const { type } = event
+    if (type === 'open') {
+      const { clientId, client } = event
+      const transport: Transport = {
+        filter: (message) => {
+          if (!connectedClients.has(clientId)) return false
+          return syncFilter(message)
+        },
+        send: async (message) => {
+          if (message.byteLength > 0) {
+            await client.sendCrdtMessage(message)
+          }
+
+          if (transport.onmessage) {
+            const messages = client.getMessages()
+            for (const byteArray of messages) {
+              // Log messages
+              const logMessages = Array.from(serializeCrdtMessages('RecievedMessages', byteArray, engine))
+              if (logMessages.length) {
+                console.log(logMessages)
+              }
+              // Log messages
+
+              transport.onmessage(byteArray)
+            }
+          }
+        }
+      }
+
+      engine.addTransport(transport)
+      connectedClients.add(event.clientId)
+    } else if (type === 'close') {
+      connectedClients.delete(event.clientId)
+    }
+  })
+
+  // TODO: add this to the server context?
+  // This numbers should be fetched by the server
+  return createNetworkEntityFactory(2060, [2061, 2061 + 512])
+}
---
+++ packages/@dcl/sdk/src/network-transport/types.ts
@@ -0,0 +1,31 @@
+import { Entity } from '@dcl/ecs'
+
+export type Socket = WebSocket & {
+  binaryType: string
+  send(data: string | Uint8Array): void
+}
+
+export type NetworkEntityFactory = {
+  addEntity(): Entity
+}
+
+export enum MessageType {
+  Auth = 1,
+  Init = 2,
+  Crdt = 3
+}
+
+declare global {
+  type ClientEvent =
+    | {
+        type: 'open'
+        clientId: string
+        client: {
+          sendCrdtMessage(message: Uint8Array): Promise<void>
+          getMessages(): Uint8Array[]
+        }
+      }
+    | { type: 'close'; clientId: string }
+  var updateCRDTState: (crdt: Uint8Array) => void
+  var registerClientObserver: (fn: (event: ClientEvent) => void) => void
+}
