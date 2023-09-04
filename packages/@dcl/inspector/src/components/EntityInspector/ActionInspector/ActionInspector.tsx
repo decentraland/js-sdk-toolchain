25767d299a2e54217e654c235963c02bd533d71c
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-09-04 09:44:52 -0300

    fix: Save ActionInspector status

--- packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/ActionInspector.tsx
+++ packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/ActionInspector.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { Item } from 'react-contexify'
 import { AiFillDelete as DeleteIcon, AiOutlinePlus as AddIcon, AiOutlineMinus as RemoveIcon } from 'react-icons/ai'
 
@@ -22,15 +22,32 @@ import './ActionInspector.css'
 export default withSdk<Props>(
   withContextMenu<Props & WithSdkProps>(({ sdk, entity, contextMenuId }) => {
     const { Actions } = sdk.components
-    const [componentValue, setComponentValue] = useComponentValue<EditorComponentsTypes['Actions']>(entity, Actions)
+    const [componentValue, setComponentValue, isComponentEqual] = useComponentValue<EditorComponentsTypes['Actions']>(
+      entity,
+      Actions
+    )
     const { handleAction } = useContextMenu()
+    const [actions, setActions] = useState<Action[]>(componentValue === null ? [] : componentValue.value)
+    const [isFocused, setIsFocused] = useState(false)
+
     const hasActions = useHasComponent(entity, Actions)
 
+    const areValidActions = useCallback(
+      (updatedActions: Action[]) =>
+        updatedActions.length > 0 &&
+        !updatedActions.some((action) => !action.type || !action?.animation || !action.name),
+      []
+    )
+
     useEffect(() => {
-      if (hasActions && componentValue.value.length === 0) {
-        handleAddNewAction()
+      if (areValidActions(actions)) {
+        if (isComponentEqual({ value: actions }) || isFocused) {
+          return
+        }
+
+        setComponentValue({ value: [...actions] })
       }
-    }, [hasActions])
+    }, [actions, isFocused])
 
     const entityAnimations = useMemo(() => {
       return sdk.sceneContext.getEntityOrNull(entity)?.gltfAssetContainer?.animationGroups || []
@@ -49,55 +66,75 @@ export default withSdk<Props>(
     }, [])
 
     const handleAddNewAction = useCallback(() => {
-      setComponentValue((prev: EditorComponentsTypes['Actions']) => {
-        return { value: [...prev.value, { type: AvailableActions.PLAY_ANIMATION, name: '' }] }
+      setActions((prev: Action[]) => {
+        return [...prev, { type: AvailableActions.PLAY_ANIMATION, name: '' }]
       })
-    }, [setComponentValue])
+    }, [setActions])
 
     const handleChangeAnimation = useCallback(
       ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>, idx: number) => {
-        setComponentValue((prev: EditorComponentsTypes['Actions']) => {
-          const data = [...prev.value]
-          data[idx].animation = value
-          return { value: data }
+        setActions((prev: Action[]) => {
+          const data = [...prev]
+          data[idx] = {
+            ...data[idx],
+            animation: value
+          }
+          return data
         })
       },
-      [setComponentValue]
+      [setActions]
     )
 
     const handleChangeType = useCallback(
       ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>, idx: number) => {
-        setComponentValue((prev: EditorComponentsTypes['Actions']) => {
-          const data = [...prev.value]
-          data[idx].type = value as AvailableActions
-          return { value: data }
+        setActions((prev: Action[]) => {
+          const data = [...prev]
+          data[idx] = {
+            ...data[idx],
+            type: value as AvailableActions
+          }
+          return data
         })
       },
-      [setComponentValue]
+      [setActions]
     )
 
     const handleChangeName = useCallback(
       (e: React.ChangeEvent<HTMLElement>, idx: number) => {
         const { value } = e.target as HTMLInputElement
-        setComponentValue((prev: EditorComponentsTypes['Actions']) => {
-          const data = [...prev.value]
-          data[idx].name = value
-          return { value: data }
+        setActions((prev: Action[]) => {
+          const data = [...prev]
+          data[idx] = {
+            ...data[idx],
+            name: value
+          }
+          return data
         })
       },
-      [setComponentValue]
+      [setActions]
+    )
+
+    const handleFocusInput = useCallback(
+      ({ type }: React.FocusEvent<HTMLInputElement>) => {
+        if (type === 'focus') {
+          setIsFocused(true)
+        } else {
+          setIsFocused(false)
+        }
+      },
+      [setIsFocused]
     )
 
     const handleRemoveAction = useCallback(
       (e: React.MouseEvent, idx: number) => {
         e.stopPropagation()
-        setComponentValue((prev: EditorComponentsTypes['Actions']) => {
-          const data = [...prev.value]
+        setActions((prev: Action[]) => {
+          const data = [...prev]
           data.splice(idx, 1)
-          return { value: data }
+          return data
         })
       },
-      [setComponentValue]
+      [setActions]
     )
 
     if (!hasActions) {
@@ -112,11 +149,12 @@ export default withSdk<Props>(
           </Item>
         </ContextMenu>
 
-        {componentValue.value.map((action: Action, idx: number) => {
+        {actions.map((action: Action, idx: number) => {
           return (
             <Block key={`action-${idx}`}>
               <Dropdown
                 label={'Action'}
+                disabled={availableActions.length === 0}
                 options={availableActions}
                 value={action.type}
                 onChange={(e) => handleChangeType(e, idx)}
@@ -124,12 +162,22 @@ export default withSdk<Props>(
               {action.type === AvailableActions.PLAY_ANIMATION && entityAnimations.length > 0 ? (
                 <Dropdown
                   label={'Animations'}
-                  options={entityAnimations.map((animation) => animation.name)}
+                  options={[
+                    { value: '', text: 'Select an Animation' },
+                    ...entityAnimations.map((animation) => ({ text: animation.name, value: animation.name }))
+                  ]}
                   value={action.animation}
                   onChange={(e) => handleChangeAnimation(e, idx)}
                 />
               ) : null}
-              <TextField label="Name" type="text" value={action.name} onChange={(e) => handleChangeName(e, idx)} />
+              <TextField
+                label="Name"
+                type="text"
+                value={action.name}
+                onChange={(e) => handleChangeName(e, idx)}
+                onFocus={handleFocusInput}
+                onBlur={handleFocusInput}
+              />
               <button className="RemoveButton" onClick={(e) => handleRemoveAction(e, idx)}>
                 <RemoveIcon />
               </button>
