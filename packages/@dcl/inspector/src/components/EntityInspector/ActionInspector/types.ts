b17adbc702f3edc83f57d06d93c8248caa94aa0f
Juan Cazala <juancazala@gmail.com>
2023-09-05 19:21:35 -0300

    feat: use @dcl/asset-packs

--- packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/types.ts
+++ packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/types.ts
@@ -3,13 +3,3 @@ import { Entity } from '@dcl/ecs'
 export interface Props {
   entity: Entity
 }
-
-export enum Actions {
-  PLAY_ANIMATION = 'play_animation'
-}
-
-export type Action = {
-  name: string
-  type: Actions
-  animation?: string
-}
