5f2bd57b531ce41777a066b594fed4385cd3d034
pbosio <pato@decentraland.org>
2023-09-26 12:48:06 -0300

    chore: upgrade `@dcl/protocol` (#751)

--- packages/@dcl/ecs/src/components/extended/Animator.ts
+++ packages/@dcl/ecs/src/components/extended/Animator.ts
@@ -12,31 +12,31 @@ export interface AnimatorComponentDefinitionExtended extends LastWriteWinElement
    *
    * Get a `mutable` version of animator clip
    * @param entity - entity with Animator component
-   * @param name - the field `name` of the component
+   * @param clipName - the field `clip` of the component
    * @returns the clip or fails if it isn't found
    */
-  getClip(entity: Entity, name: string): PBAnimationState
+  getClip(entity: Entity, clipName: string): PBAnimationState
 
   /**
    * @public
    *
    * Get a `mutable` version of animator clip
    * @param entity - entity with Animator component
-   * @param name - the field `name` of the component
+   * @param clipName - the field `clip` of the component
    * @returns the clip or null if it isn't found
    */
-  getClipOrNull(entity: Entity, name: string): PBAnimationState | null
+  getClipOrNull(entity: Entity, clipName: string): PBAnimationState | null
 
   /**
    * @public
    *
    * Set playing=true the animation `$name`
    * @param entity - entity with Animator component
-   * @param name - animation name
+   * @param clipName - animation name
    * @param resetCursor - the animation starts at 0 or continues from the current cursor position
    * @returns true in successful playing, false if it doesn't find the Animator or clip
    */
-  playSingleAnimation(entity: Entity, name: string, resetCursor?: boolean): boolean
+  playSingleAnimation(entity: Entity, clipName: string, resetCursor?: boolean): boolean
 
   /**
    * @public
@@ -57,36 +57,36 @@ export function defineAnimatorComponent(
   /**
    * @returns The tuple [animator, clip]
    */
-  function getClipAndAnimator(entity: Entity, name: string): [PBAnimator | null, PBAnimationState | null] {
+  function getClipAndAnimator(entity: Entity, clipName: string): [PBAnimator | null, PBAnimationState | null] {
     const anim = theComponent.getMutableOrNull(entity)
     if (!anim) return [null, null]
 
-    const state = anim.states.find((item) => item.name === name || item.clip === name)
+    const state = anim.states.find((item) => item.clip === clipName)
     if (!state) return [anim, null]
     return [anim, state]
   }
 
   return {
     ...theComponent,
-    getClipOrNull(entity: Entity, name: string): PBAnimationState | null {
-      const [_, state] = getClipAndAnimator(entity, name)
+    getClipOrNull(entity: Entity, clipName: string): PBAnimationState | null {
+      const [_, state] = getClipAndAnimator(entity, clipName)
       return state
     },
-    getClip(entity: Entity, name: string): PBAnimationState {
-      const [animator, state] = getClipAndAnimator(entity, name)
+    getClip(entity: Entity, clipName: string): PBAnimationState {
+      const [animator, state] = getClipAndAnimator(entity, clipName)
 
       if (!animator) {
         throw new Error(`There is no Animator found in the entity ${entity}`)
       }
 
       if (!state) {
-        throw new Error(`The Animator component of ${entity} has no the state ${name}`)
+        throw new Error(`The Animator component of ${entity} has no the state ${clipName}`)
       }
 
       return state
     },
-    playSingleAnimation(entity: Entity, name: string, shouldReset: boolean = true): boolean {
-      const [animator, state] = getClipAndAnimator(entity, name)
+    playSingleAnimation(entity: Entity, clipName: string, shouldReset: boolean = true): boolean {
+      const [animator, state] = getClipAndAnimator(entity, clipName)
       if (!animator || !state) return false
 
       // Reset all other animations
