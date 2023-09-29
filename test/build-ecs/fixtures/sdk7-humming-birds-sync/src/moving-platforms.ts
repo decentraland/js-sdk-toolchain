96dc4b9ece3bd6660f9923bdeb084ab09fe1e31d
Gonzalo DCL <gonzalo@decentraland.org>
2023-09-29 16:05:13 -0300

    add isCompleted and tweenChanged helpers

--- test/build-ecs/fixtures/sdk7-humming-birds-sync/src/moving-platforms.ts
+++ test/build-ecs/fixtures/sdk7-humming-birds-sync/src/moving-platforms.ts
@@ -13,6 +13,7 @@ import {
 import { Vector3 } from '@dcl/sdk/math'
 import { engine } from '@dcl/sdk/ecs'
 import { NetworkManager } from '@dcl/sdk/network-transport/types'
+import { isServer } from '~system/EngineApi'
 
 export function createMovingPlatforms(networkedEntityFactory: NetworkManager) {
   //// triggerable platform
@@ -98,53 +99,121 @@ export function createMovingPlatforms(networkedEntityFactory: NetworkManager) {
   })
 }
 
-const cacheTween = new Map<Entity, PBTween>()
-const tweenFrames = new Map<Entity, number>()
-// Tween Helpers
-engine.addSystem(() => {
-  for (const [entity, tweenSequence, tween, tweenState] of engine.getEntitiesWith(TweenSequence, Tween, TweenState)) {
-    const prevTween = cacheTween.get(entity)
-    const sameTween = JSON.stringify(prevTween) === JSON.stringify(tween)
-    const frame = tweenFrames.get(entity) || tweenFrames.set(entity, 0).get(entity)!
-    tweenFrames.set(entity, frame + 1)
-    if (!sameTween) {
-      cacheTween.set(entity, tween)
-      tweenFrames.set(entity, 0)
-      continue // 0,99
-    }
-    if (
-      sameTween &&
-      tweenState.state === TweenStateStatus.TS_COMPLETED &&
-      // TODO: hackishhhhhhh for networking (avoid updating again the tween when we recieved a message and ours is about to finish too)
-      tweenFrames.get(entity)! > 10
-    ) {
-      tweenFrames.set(entity, 0)
-      const { sequence } = tweenSequence
-      if (sequence && sequence.length) {
-        const [nextTweenSequence, ...otherTweens] = sequence
-        Tween.createOrReplace(entity, nextTweenSequence)
-        const mutableTweenHelper = TweenSequence.getMutable(entity)
-        mutableTweenHelper.sequence = otherTweens
-        if (tweenSequence.loop) {
-          mutableTweenHelper.sequence.push(tween)
-        }
-      } else if (tweenSequence.loop) {
-        const newTween = backwardsTween(tween)
-        Tween.createOrReplace(entity, newTween)
-      }
-    }
-  }
+void isServer({}).then(({ isServer }) => {
+  if (isServer) return
+  engine.addSystem(testingSystem)
 })
 
-function backwardsTween(tween: PBTween): PBTween {
-  if (tween.mode?.$case === 'move' && tween.mode.move) {
-    return { ...tween, mode: { ...tween.mode, move: { start: tween.mode.move.end, end: tween.mode.move.start } } }
+function testingSystem() {
+  for (const [entity, _tween] of engine.getEntitiesWith(Tween)) {
+    if (tweenUtils.tweenCompleted(entity)) {
+      console.log('[TestingSystem]: tween completed', entity)
+    }
+    if (tweenUtils.tweenChanged(entity)) {
+      console.log('[TestingSystem]: tween changed', entity)
+    }
   }
-  if (tween.mode?.$case === 'rotate' && tween.mode.rotate) {
-    return { ...tween, mode: { ...tween.mode, rotate: { start: tween.mode.rotate.end, end: tween.mode.rotate.start } } }
+}
+
+// 2- Helpers TweenMove, TweenRotate
+// 3- Perpetual motion
+const tweenUtils = TweenUtils()
+
+function TweenUtils() {
+  // Used to detect new tweens for the same entity and reset the frames & completed values
+  const cacheTween = new Map<Entity, PBTween>()
+
+  // Used to avoid updaing again the tween in the case we receieve a network tween from other client
+  const tweenFrames = new Map<Entity, number>()
+
+  // Trigger the isCompleted only once per tween
+  const tweenCompleted = new Map<Entity, boolean>()
+
+  function isCompleted(entity: Entity) {
+    const tweenState = TweenState.getOrNull(entity)
+    const tween = Tween.getOrNull(entity)
+
+    if (!tweenState || !tween) return false
+
+    if (
+      // Renderer notified that the tween is completed
+      (tweenChanged(entity) || tweenState.state === TweenStateStatus.TS_COMPLETED) &&
+      // Avoid sending isCompleted multiple times
+      !tweenCompleted.get(entity) &&
+      // Amount of frames needed to consider a tween completed
+      tweenFrames.get(entity)! > 2
+    ) {
+      return true
+    }
+
+    return false
   }
-  if (tween.mode?.$case === 'scale' && tween.mode.scale) {
-    return { ...tween, mode: { ...tween.mode, scale: { start: tween.mode.scale.end, end: tween.mode.scale.start } } }
+
+  function tweenChanged(entity: Entity) {
+    const currentTween = Tween.getOrNull(entity)
+    const prevTween = cacheTween.get(entity)
+
+    // Maybe store the buffer and compare buffers ?
+    return JSON.stringify(prevTween?.mode) !== JSON.stringify(currentTween?.mode)
   }
-  throw new Error('Invalid tween')
+
+  // Logic for sequence tweens
+  engine.addSystem(() => {
+    for (const [entity, tween] of engine.getEntitiesWith(Tween)) {
+      tweenFrames.set(entity, (tweenFrames.get(entity) ?? 0) + 1)
+      if (tweenChanged(entity)) {
+        // Maybe store de buffer ?
+        cacheTween.set(entity, JSON.parse(JSON.stringify(tween)))
+        tweenCompleted.set(entity, false)
+        tweenFrames.set(entity, 0)
+        continue
+      }
+
+      if (isCompleted(entity)) {
+        // Reset tween frames.
+        tweenFrames.set(entity, 0)
+        // set the tween completed to avoid calling this again for the same tween
+        tweenCompleted.set(entity, true)
+
+        const tweenSequence = TweenSequence.getOrNull(entity)
+        if (!tweenSequence) continue
+        const { sequence } = tweenSequence
+
+        if (sequence && sequence.length) {
+          const [nextTweenSequence, ...otherTweens] = sequence
+          Tween.createOrReplace(entity, nextTweenSequence)
+          const mutableTweenHelper = TweenSequence.getMutable(entity)
+          mutableTweenHelper.sequence = otherTweens
+          if (tweenSequence.loop) {
+            mutableTweenHelper.sequence.push(tween)
+          }
+        } else if (tweenSequence.loop) {
+          const newTween = backwardsTween(tween)
+          Tween.createOrReplace(entity, newTween)
+        }
+      }
+    }
+  }, Number.NEGATIVE_INFINITY)
+
+  function backwardsTween(tween: PBTween): PBTween {
+    if (tween.mode?.$case === 'move' && tween.mode.move) {
+      return { ...tween, mode: { ...tween.mode, move: { start: tween.mode.move.end, end: tween.mode.move.start } } }
+    }
+    if (tween.mode?.$case === 'rotate' && tween.mode.rotate) {
+      return {
+        ...tween,
+        mode: { ...tween.mode, rotate: { start: tween.mode.rotate.end, end: tween.mode.rotate.start } }
+      }
+    }
+    if (tween.mode?.$case === 'scale' && tween.mode.scale) {
+      return { ...tween, mode: { ...tween.mode, scale: { start: tween.mode.scale.end, end: tween.mode.scale.start } } }
+    }
+    throw new Error('Invalid tween')
+  }
+
+  return {
+    // This event is fired only once per tween
+    tweenCompleted: isCompleted,
+    tweenChanged
+  }
 }
