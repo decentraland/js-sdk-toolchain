034e43f7a5d7baaf1f59d3853c855737bac2cc97
Juan Cazala <juancazala@gmail.com>
2023-10-02 13:02:09 -0300

    feat: allow deleting components

--- packages/@dcl/inspector/src/components/EntityInspector/CounterInspector/CounterInspector.tsx
+++ packages/@dcl/inspector/src/components/EntityInspector/CounterInspector/CounterInspector.tsx
@@ -1,7 +1,12 @@
+import { useCallback } from 'react'
+import { Item, Menu } from 'react-contexify'
+import { AiFillDelete as DeleteIcon } from 'react-icons/ai'
 import { useHasComponent } from '../../../hooks/sdk/useHasComponent'
 import { useComponentInput } from '../../../hooks/sdk/useComponentInput'
+import { useContextMenu } from '../../../hooks/sdk/useContextMenu'
 import { ROOT } from '../../../lib/sdk/tree'
-import { withSdk } from '../../../hoc/withSdk'
+import { withContextMenu } from '../../../hoc/withContextMenu'
+import { WithSdkProps, withSdk } from '../../../hoc/withSdk'
 import { Container } from '../../Container'
 import { TextField } from '../TextField'
 import { fromCounter, isValidInput, toCounter } from './utils'
@@ -9,19 +14,33 @@ import { Props } from './types'
 
 import './CounterInspector.css'
 
-export default withSdk<Props>(({ sdk, entity }) => {
-  const { Counter } = sdk.components
+export default withSdk<Props>(
+  withContextMenu<WithSdkProps & Props>(({ sdk, entity, contextMenuId }) => {
+    const { Counter } = sdk.components
 
-  const hasCounter = useHasComponent(entity, Counter)
-  const { getInputProps } = useComponentInput(entity, Counter, fromCounter, toCounter, isValidInput)
+    const hasCounter = useHasComponent(entity, Counter)
+    const { getInputProps } = useComponentInput(entity, Counter, fromCounter, toCounter, isValidInput)
 
-  if (!hasCounter || entity === ROOT) {
-    return null
-  }
+    const { handleAction } = useContextMenu()
 
-  return (
-    <Container label="Counter" className="CounterInspector">
-      <TextField label="Value" type="numeric" {...getInputProps('value')} />
-    </Container>
-  )
-})
+    const handleRemove = useCallback(async () => {
+      sdk.operations.removeComponent(entity, Counter)
+      await sdk.operations.dispatch()
+    }, [sdk])
+
+    if (!hasCounter || entity === ROOT) {
+      return null
+    }
+
+    return (
+      <Container label="Counter" className="CounterInspector">
+        <Menu id={contextMenuId}>
+          <Item id="delete" onClick={handleAction(handleRemove)}>
+            <DeleteIcon /> Delete
+          </Item>
+        </Menu>
+        <TextField label="Value" type="numeric" {...getInputProps('value')} />
+      </Container>
+    )
+  })
+)
