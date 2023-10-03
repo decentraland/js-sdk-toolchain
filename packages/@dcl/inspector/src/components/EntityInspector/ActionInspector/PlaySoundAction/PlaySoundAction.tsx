9775001d031ae5abe29c6d8dc47fa33d4f2def93
Juan Cazala <juancazala@gmail.com>
2023-10-03 17:58:46 -0300

    feat: added play mode and drag n drop file on field

--- packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/PlaySoundAction/PlaySoundAction.tsx
+++ packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/PlaySoundAction/PlaySoundAction.tsx
@@ -1,15 +1,58 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { ActionPayload, ActionType } from '@dcl/asset-packs'
 import { recursiveCheck } from 'jest-matcher-deep-close-to/lib/recursiveCheck'
+import cx from 'classnames'
+import { useDrop } from 'react-dnd'
 
+import { DropTypesEnum, ProjectAssetDrop, getNode } from '../../../../lib/sdk/drag-drop'
+import { withAssetDir } from '../../../../lib/data-layer/host/fs-utils'
+import { isAudio } from '../../AudioSourceInspector/utils'
 import { TextField } from '../../TextField'
+import { SelectField } from '../../SelectField'
+import { useAppSelector } from '../../../../redux/hooks'
+import { selectAssetCatalog } from '../../../../redux/app'
 import { isValid } from './utils'
 import type { Props } from './types'
+
+import './PlaySoundAction.css'
+import { Container } from '../../../Container'
+
+enum PLAY_MODE {
+  PLAY_ONCE = 'play-once',
+  LOOP = 'loop'
+}
+
+const options = [
+  {
+    label: 'Play Once',
+    value: PLAY_MODE.PLAY_ONCE
+  },
+  {
+    label: 'Loop',
+    value: PLAY_MODE.LOOP
+  }
+]
 
 const PlaySoundAction: React.FC<Props> = ({ value, onUpdate }: Props) => {
   const [payload, setPayload] = useState<ActionPayload<ActionType.PLAY_SOUND>>({
     ...value
   })
+
+  const files = useAppSelector(selectAssetCatalog)
+
+  const removeBase = useCallback(
+    (path: string) => {
+      return files?.basePath ? path.replace(files.basePath + '/', '') : path
+    },
+    [files]
+  )
+
+  const addBase = useCallback(
+    (path: string) => {
+      return files?.basePath ? `${files.basePath}/${path}` : path
+    },
+    [files]
+  )
 
   useEffect(() => {
     if (!recursiveCheck(payload, value, 2) || !isValid(payload)) return
@@ -18,20 +61,61 @@ const PlaySoundAction: React.FC<Props> = ({ value, onUpdate }: Props) => {
 
   const handleChangeSrc = useCallback(
     ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => {
-      setPayload({ ...payload, src: value })
+      setPayload({ ...payload, src: addBase(value) })
     },
     [payload, setPayload]
   )
 
+  const handleChangePlayMode = useCallback(
+    ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => {
+      setPayload({ ...payload, loop: value === PLAY_MODE.LOOP })
+    },
+    [payload, setPayload]
+  )
+
+  const [{ isHover }, drop] = useDrop(
+    () => ({
+      accept: [DropTypesEnum.ProjectAsset],
+      drop: ({ value, context }: ProjectAssetDrop, monitor) => {
+        if (monitor.didDrop()) return
+        const node = context.tree.get(value)!
+        const audio = getNode(node, context.tree, isAudio)
+        if (audio) {
+          setPayload({ ...payload, src: withAssetDir(audio.asset.src) })
+        }
+      },
+      canDrop: ({ value, context }: ProjectAssetDrop) => {
+        const node = context.tree.get(value)!
+        return !!getNode(node, context.tree, isAudio)
+      },
+      collect: (monitor) => ({
+        isHover: monitor.canDrop() && monitor.isOver()
+      })
+    }),
+    [files]
+  )
+
+  const error = useMemo(() => {
+    return !files || !files.assets.some(($) => $.path === payload.src)
+  }, [files, payload])
+
   return (
-    <div className="PlaySoundActionContainer">
+    <Container className={cx('PlaySoundActionContainer', { hover: isHover })}>
       <div className="row">
-        <div className="field">
+        <div className="field" ref={drop}>
           <label>Path</label>
-          <TextField value={payload.src} onChange={handleChangeSrc} />
+          <TextField value={removeBase(payload.src)} onChange={handleChangeSrc} error={error} />
+        </div>
+        <div className="field">
+          <label>Play Mode</label>
+          <SelectField
+            value={payload.loop ? PLAY_MODE.LOOP : PLAY_MODE.PLAY_ONCE}
+            options={options}
+            onChange={handleChangePlayMode}
+          />
         </div>
       </div>
-    </div>
+    </Container>
   )
 }
 
