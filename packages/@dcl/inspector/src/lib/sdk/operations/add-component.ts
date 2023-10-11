6f59a33858777543033902303ddd8f1531924d6a
Juan Cazala <juancazala@gmail.com>
2023-10-11 17:04:08 -0300

    fix: remap component ids and triggers when doing a duplicate entity

--- packages/@dcl/inspector/src/lib/sdk/operations/add-component.ts
+++ packages/@dcl/inspector/src/lib/sdk/operations/add-component.ts
@@ -2,14 +2,14 @@ import { Entity, IEngine } from '@dcl/ecs'
 import { isLastWriteWinComponent } from '../../../hooks/sdk/useComponentValue'
 import { ComponentName, getNextId } from '@dcl/asset-packs'
 
-export const withId: string[] = [ComponentName.ACTIONS, ComponentName.STATES, ComponentName.COUNTER]
+export const COMPONENTS_WITH_ID: string[] = [ComponentName.ACTIONS, ComponentName.STATES, ComponentName.COUNTER]
 
 export function addComponent(engine: IEngine) {
   return function addComponent(entity: Entity, componentId: number) {
     const component = engine.getComponent(componentId)
     if (isLastWriteWinComponent<{ id?: number }>(component)) {
       component.create(entity)
-      if (withId.includes(component.componentName)) {
+      if (COMPONENTS_WITH_ID.includes(component.componentName)) {
         const value = component.getMutable(entity)
         value.id = getNextId(engine as any)
       }
