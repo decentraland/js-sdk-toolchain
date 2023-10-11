320b95801e7925191ab695b9e802d43d1d8b40cd
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-10-11 18:38:21 -0300

    feat: Add segment key to the inspector config

--- packages/@dcl/inspector/src/lib/logic/config.ts
+++ packages/@dcl/inspector/src/lib/logic/config.ts
@@ -6,6 +6,7 @@ export type InspectorConfig = {
   binIndexJsUrl: string | null
   disableSmartItems: boolean
   contentUrl: string
+  segmentKey: string
 }
 
 export type GlobalWithConfig = typeof globalThis & {
@@ -16,6 +17,10 @@ export const CONTENT_URL = version.includes('commit')
   ? 'https://builder-items.decentraland.zone'
   : 'https://builder-items.decentraland.org'
 
+export const SEGMENT_KEY = version.includes('commit')
+  ? 'RmfAUVW7o1EEuvYJGqmVjK9q48BUpnCr'
+  : '8PrGcB0XsogabYgo02gXqfqVM20KKzva'
+
 export function getConfig(): InspectorConfig {
   const config = (globalThis as GlobalWithConfig).InspectorConfig
   const params = new URLSearchParams(globalThis?.location?.search || '')
@@ -25,6 +30,7 @@ export function getConfig(): InspectorConfig {
       params.get('parent') || params.get('dataLayerRpcParentUrl') || config?.dataLayerRpcParentUrl || null,
     binIndexJsUrl: params.get('binIndexJsUrl') || config?.binIndexJsUrl || null,
     disableSmartItems: params.has('disableSmartItems') || !!config?.disableSmartItems,
-    contentUrl: params.get('contentUrl') || config?.contentUrl || CONTENT_URL
+    contentUrl: params.get('contentUrl') || config?.contentUrl || CONTENT_URL,
+    segmentKey: params.get('segmentKey') || config?.segmentKey || SEGMENT_KEY
   }
 }
