465bb46c6b8f5ebf662de5465473b6494627e901
Gabriel Díaz <gabrieldiaz31@gmail.com>
2023-10-31 11:04:14 -0300

    feat: Add FileUploadField (#816)
    * feat: Add FileUploadFile component
    
    * fix: TextField placeholder color
    
    * feat: Use the FileUploadField component for the gltf component

--- packages/@dcl/inspector/src/components/EntityInspector/GltfInspector/GltfInspector.tsx
+++ packages/@dcl/inspector/src/components/EntityInspector/GltfInspector/GltfInspector.tsx
@@ -1,6 +1,5 @@
 import { useCallback } from 'react'
 import { Item } from 'react-contexify'
-import { useDrop } from 'react-dnd'
 import { AiFillDelete as DeleteIcon } from 'react-icons/ai'
 
 import { ContextMenu as Menu } from '../../ContexMenu'
@@ -9,18 +8,14 @@ import { WithSdkProps, withSdk } from '../../../hoc/withSdk'
 import { useHasComponent } from '../../../hooks/sdk/useHasComponent'
 import { useComponentInput } from '../../../hooks/sdk/useComponentInput'
 import { useContextMenu } from '../../../hooks/sdk/useContextMenu'
-import { ProjectAssetDrop, getNode } from '../../../lib/sdk/drag-drop'
 import { Block } from '../../Block'
 import { Container } from '../../Container'
 import { SelectField } from '../SelectField'
-import { TextField } from '../../ui/TextField'
+import { FileUploadField } from '../../ui'
 import { Props } from './types'
 import { fromGltf, toGltf, isValidInput, COLLISION_LAYERS, isModel } from './utils'
-import { withAssetDir } from '../../../lib/data-layer/host/fs-utils'
 import { useAppSelector } from '../../../redux/hooks'
 import { selectAssetCatalog } from '../../../redux/app'
-
-const DROP_TYPES = ['project-asset']
 
 export default withSdk<Props>(
   withContextMenu<WithSdkProps & Props>(({ sdk, entity, contextMenuId }) => {
@@ -46,31 +41,12 @@ export default withSdk<Props>(
       sdk.operations.removeComponent(entity, GltfContainer)
       await sdk.operations.dispatch()
     }, [])
+
     const handleDrop = useCallback(async (src: string) => {
       const { operations } = sdk
       operations.updateValue(GltfContainer, entity, { src })
       await operations.dispatch()
     }, [])
-
-    const [{ isHover }, drop] = useDrop(
-      () => ({
-        accept: DROP_TYPES,
-        drop: ({ value, context }: ProjectAssetDrop, monitor) => {
-          if (monitor.didDrop()) return
-          const node = context.tree.get(value)!
-          const model = getNode(node, context.tree, isModel)
-          if (model) void handleDrop(withAssetDir(model.asset.src))
-        },
-        canDrop: ({ value, context }: ProjectAssetDrop) => {
-          const node = context.tree.get(value)!
-          return !!getNode(node, context.tree, isModel)
-        },
-        collect: (monitor) => ({
-          isHover: monitor.canDrop() && monitor.isOver()
-        })
-      }),
-      [files]
-    )
 
     if (!hasGltf) return null
 
@@ -81,8 +57,13 @@ export default withSdk<Props>(
             <DeleteIcon /> Delete
           </Item>
         </Menu>
-        <Block label="Path" ref={drop}>
-          <TextField type="text" {...getInputProps('src')} drop={isHover} error={files && !isValid} />
+        <Block label="Path">
+          <FileUploadField
+            {...getInputProps('src')}
+            onDrop={handleDrop}
+            error={files && !isValid}
+            isValidFile={isModel}
+          />
         </Block>
         <Block label="Collision">
           <SelectField
--- packages/@dcl/inspector/src/components/ui/index.ts
+++ packages/@dcl/inspector/src/components/ui/index.ts
@@ -1,2 +1,3 @@
 export { TextField } from './TextField'
 export { CheckboxField } from './CheckboxField'
+export { FileUploadField } from './FileUploadField'
