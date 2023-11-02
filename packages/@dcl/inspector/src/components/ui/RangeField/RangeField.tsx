8ac5b9a809b658e60334cc819fc7f8fd52cac5a1
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-11-02 17:58:19 -0300

    fix: RangeField styles

--- packages/@dcl/inspector/src/components/ui/RangeField/RangeField.tsx
+++ packages/@dcl/inspector/src/components/ui/RangeField/RangeField.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import cx from 'classnames'
 
 import { TextField } from '../TextField'
