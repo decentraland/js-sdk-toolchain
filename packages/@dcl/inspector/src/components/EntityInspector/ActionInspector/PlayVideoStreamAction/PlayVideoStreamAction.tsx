a831016755aae5274b7bb3f57ca93f34bd4e7ff6
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-11-17 19:39:38 -0300

    feat: Add Play Video Stream Action component

---
+++ packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/PlayVideoStreamAction/PlayVideoStreamAction.tsx
@@ -0,0 +1,99 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { ActionPayload, ActionType } from '@dcl/asset-packs'
+import { recursiveCheck } from 'jest-matcher-deep-close-to/lib/recursiveCheck'
+
+import { isValidVolume, volumeFromMediaSource, volumeToMediaSource } from '../../../../lib/utils/media'
+import { Block } from '../../../Block'
+import { CheckboxField, Dropdown, RangeField, TextField } from '../../../ui'
+import { isValid } from './utils'
+import type { Props } from './types'
+
+import './PlayVideoStreamAction.css'
+
+enum PLAY_MODE {
+  PLAY_ONCE = 'play-once',
+  LOOP = 'loop'
+}
+
+const playModeOptions = [
+  {
+    label: 'Play Once',
+    value: PLAY_MODE.PLAY_ONCE
+  },
+  {
+    label: 'Loop',
+    value: PLAY_MODE.LOOP
+  }
+]
+
+const PlayVideoStreamAction: React.FC<Props> = ({ value, onUpdate }: Props) => {
+  const [payload, setPayload] = useState<Partial<ActionPayload<ActionType.PLAY_VIDEO_STREAM>>>({
+    ...value
+  })
+
+  useEffect(() => {
+    if (!recursiveCheck(payload, value, 2) || !isValid(payload)) return
+    onUpdate(payload)
+  }, [payload, onUpdate])
+
+  const handleChangeDclCast = useCallback(
+    ({ target: { checked } }: React.ChangeEvent<HTMLInputElement>) => {
+      setPayload({ ...payload, dclCast: checked })
+    },
+    [payload, setPayload]
+  )
+
+  const handleChangeSrc = useCallback(
+    ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+      setPayload({ ...payload, src: value })
+    },
+    [payload, setPayload]
+  )
+
+  const handleChangePlayMode = useCallback(
+    ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => {
+      setPayload({ ...payload, loop: value === PLAY_MODE.LOOP })
+    },
+    [payload, setPayload]
+  )
+
+  const handleChangeVolume = useCallback(
+    (e: React.ChangeEvent<HTMLElement>) => {
+      const { value } = e.target as HTMLInputElement
+
+      if (isValidVolume(value)) {
+        setPayload({ ...payload, volume: volumeToMediaSource(value) })
+      }
+    },
+    [payload, setPayload]
+  )
+
+  return (
+    <div className="PlayVideoStreamActionContainer">
+      <Block>
+        <CheckboxField label="Decentraland Cast" checked={payload.dclCast} onChange={handleChangeDclCast} />
+      </Block>
+      {!payload.dclCast ? (
+        <Block>
+          <TextField label="URL" value={payload.src} onChange={handleChangeSrc} />
+          <Dropdown
+            label="Play Mode"
+            value={payload.loop ? PLAY_MODE.LOOP : PLAY_MODE.PLAY_ONCE}
+            options={playModeOptions}
+            onChange={handleChangePlayMode}
+          />
+        </Block>
+      ) : null}
+      <Block>
+        <RangeField
+          label="Volume"
+          value={volumeFromMediaSource(value.volume)}
+          onChange={handleChangeVolume}
+          isValidValue={isValidVolume}
+        />
+      </Block>
+    </div>
+  )
+}
+
+export default React.memo(PlayVideoStreamAction)
