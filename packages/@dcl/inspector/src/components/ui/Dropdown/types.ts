d66bd49db7b3c82321b5324588ebc12eafada5d9
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-11-28 15:58:42 -0300

    feat: Update Dropdown component to handle multiple selected options
    Signed-off-by: Gabriel Diaz <gabrieldiaz31@gmail.com>

--- packages/@dcl/inspector/src/components/ui/Dropdown/types.ts
+++ packages/@dcl/inspector/src/components/ui/Dropdown/types.ts
@@ -1,12 +1,21 @@
 import { Props as OptionProp } from './Option/types'
 
+export type DropdownChangeEvent = React.ChangeEvent<HTMLSelectElement> & {
+  target: EventTarget &
+    HTMLSelectElement & {
+      value: string | string[]
+    }
+}
+
 export type Props = React.SelectHTMLAttributes<HTMLSelectElement> & {
   options: OptionProp[]
   className?: string
   disabled?: boolean
   empty?: string
   label?: React.ReactNode
-  value?: string | number | readonly string[]
+  value?: string | number | readonly string[] | any[]
   searchable?: boolean
   error?: string | boolean
+  multiple?: boolean
+  clearable?: boolean
 }
