296a8998ccf4a32e1be391435c44e934848455f0
Gonzalo DCL <gonzalo@decentraland.org>
2023-11-29 12:34:04 -0300

    rename entiy methodsC

--- packages/@dcl/sdk/src/network/index.ts
+++ packages/@dcl/sdk/src/network/index.ts
@@ -2,6 +2,6 @@ import { engine } from '@dcl/ecs'
 import { addSyncTransport } from './message-bus-sync'
 
 // initialize sync transport for sdk engine
-const { getChilds, getFirstChild, syncEntity, parentEntity, getParent, myProfile } = addSyncTransport(engine)
+const { getChildren, syncEntity, parentEntity, getParent, myProfile, removeParent } = addSyncTransport(engine)
 
-export { getChilds, getFirstChild, syncEntity, parentEntity, getParent, myProfile }
+export { getChildren, syncEntity, parentEntity, getParent, myProfile, removeParent }
