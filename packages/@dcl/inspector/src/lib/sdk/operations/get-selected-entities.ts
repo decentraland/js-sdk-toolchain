ae6c840b6dcb33c6151d9bf7d4f331c7958f1d2a
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-01-16 22:00:01 +0100

    refactor: Type method getSelectedEntities

--- packages/@dcl/inspector/src/lib/sdk/operations/get-selected-entities.ts
+++ packages/@dcl/inspector/src/lib/sdk/operations/get-selected-entities.ts
@@ -1,9 +1,9 @@
-import { IEngine } from '@dcl/ecs'
+import { Entity, IEngine } from '@dcl/ecs'
 import { EditorComponentNames, EditorComponents } from '../components'
 
 export function getSelectedEntities(engine: IEngine) {
-  return function getSelectedEntities() {
-    const entities = []
+  return function getSelectedEntities(): Entity[] {
+    const entities: Entity[] = []
     const Selection = engine.getComponent(EditorComponentNames.Selection) as EditorComponents['Selection']
     for (const [entity] of engine.getEntitiesWith(Selection)) {
       entities.push(entity)
