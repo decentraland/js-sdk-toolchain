9d75031789dbe582286e6576ab23023cb2f670b4
Juan Cazala <juancazala@gmail.com>
2024-01-24 20:23:33 -0300

    chore: fix test

--- packages/@dcl/inspector/src/lib/data-layer/host/utils/migrations/build-nodes-hierarchy.ts.spec.ts
+++ packages/@dcl/inspector/src/lib/data-layer/host/utils/migrations/build-nodes-hierarchy.ts.spec.ts
@@ -1,3 +1,4 @@
+import { Scene } from '@dcl/schemas'
 import {
   createTempEngineContext,
   generateFeededComposite,
@@ -5,13 +6,50 @@ import {
 } from '../../../client/feeded-local-fs'
 import { buildNodesHierarchy } from './build-nodes-hierarchy'
 
+const scene: Scene = {
+  main: '',
+  display: {
+    title: 'Feeded Scene',
+    description: 'This is a feeded scene.json into memory',
+    navmapThumbnail: 'assets/scene/feeded-thumbnail.png'
+  },
+  scene: {
+    parcels: ['2,2', '2,3', '3,2', '3,3'],
+    base: '2,2'
+  },
+  contact: {
+    name: 'John Doe',
+    email: 'johndoe@email.com'
+  },
+  tags: ['game', 'art', 'super fun', 'cool'],
+  spawnPoints: [
+    {
+      name: 'Feeded Spawn Point',
+      default: true,
+      position: {
+        x: [4, 6],
+        y: [1, 1],
+        z: [4, 6]
+      },
+      cameraTarget: {
+        x: 8,
+        y: 1,
+        z: 8
+      }
+    }
+  ],
+  featureToggles: {
+    voiceChat: 'disabled'
+  }
+}
+
 describe('Migration: Build Node component hierarchy', () => {
   let engineCtx: ReturnType<typeof createTempEngineContext>
   beforeEach(() => {
     engineCtx = createTempEngineContext()
   })
   it('should build same hierarchy as in main composite', () => {
-    generateFeededComposite(engineCtx) // create entities & components
+    generateFeededComposite(engineCtx, scene) // create entities & components
 
     const { engine, components } = engineCtx
 
