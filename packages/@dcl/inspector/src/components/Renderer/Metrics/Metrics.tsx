689aba9bca012a200b2460c8cf2b3bbfafdc5fbe
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-02-07 13:27:52 -0300

    fix: Parcels size and the counting of unique textures and materials

--- packages/@dcl/inspector/src/components/Renderer/Metrics/Metrics.tsx
+++ packages/@dcl/inspector/src/components/Renderer/Metrics/Metrics.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useEffect, useMemo } from 'react'
 import cx from 'classnames'
 import { IoGridOutline as SquaresGridIcon, IoAlertCircleOutline as AlertIcon } from 'react-icons/io5'
+import { CrdtMessageType } from '@dcl/ecs'
 
 import { withSdk, WithSdkProps } from '../../../hoc/withSdk'
 import { useChange } from '../../../hooks/sdk/useChange'
 import { useOutsideClick } from '../../../hooks/useOutsideClick'
+import type { Layout } from '../../../lib/utils/layout'
 import { Button } from '../../Button'
 import { getSceneLimits } from './utils'
 import type { Metrics } from './types'
@@ -12,42 +14,61 @@ import type { Metrics } from './types'
 import './Metrics.css'
 
 const ICON_SIZE = 18
+const PARCEL_SIZE = 16
 
 const Metrics = withSdk<WithSdkProps>(({ sdk }) => {
+  const ROOT = sdk.engine.RootEntity
   const [showMetrics, setShowMetrics] = React.useState(false)
-  const [metrics, setMetrics] = React.useState<Partial<Metrics>>({
+  const [metrics, setMetrics] = React.useState<Metrics>({
     triangles: 0,
     entities: 0,
     bodies: 0,
     materials: 0,
     textures: 0
   })
+  const [sceneLayout, setSceneLayout] = React.useState<Layout>({
+    base: { x: 0, y: 0 },
+    parcels: []
+  })
 
   const handleUpdateMetrics = useCallback(() => {
-    const ROOT_ENGINE = sdk.engine.RootEntity
     const triangles = sdk.scene.meshes.reduce((acc, mesh) => acc + mesh.getTotalVertices(), 0)
-    const entities = (sdk.components.Nodes.getOrNull(ROOT_ENGINE)?.value ?? [ROOT_ENGINE]).length - 1
+    const entities = (sdk.components.Nodes.getOrNull(ROOT)?.value ?? [ROOT]).length - 1
+    const uniqueTextures = new Set(sdk.scene.textures.map((texture) => texture.getInternalTexture()!.uniqueId))
+    const uniqueMaterials = new Set(sdk.scene.materials.map((material) => material.id))
     setMetrics({
       triangles: triangles,
       entities: entities,
       bodies: sdk.scene.meshes.length,
-      materials: sdk.scene.materials.length,
-      textures: sdk.scene.textures.length
+      materials: uniqueTextures.size,
+      textures: uniqueMaterials.size
     })
   }, [sdk])
 
-  useEffect(handleUpdateMetrics, [handleUpdateMetrics])
+  const handleUpdateSceneLayout = useCallback(() => {
+    const scene = sdk.components.Scene.get(ROOT)
+    setSceneLayout({ ...(scene.layout as Layout) })
+  }, [sdk, setSceneLayout])
 
-  useChange(handleUpdateMetrics, [handleUpdateMetrics])
+  useEffect(() => {
+    handleUpdateMetrics()
+    handleUpdateSceneLayout()
+  }, [])
 
-  const scene = useMemo(() => {
-    return sdk.components.Scene.get(sdk.engine.RootEntity)
-  }, [sdk])
+  useChange(
+    ({ operation, component }) => {
+      if (operation === CrdtMessageType.PUT_COMPONENT && component?.componentId === sdk.components.Nodes.componentId) {
+        handleUpdateMetrics()
+        handleUpdateSceneLayout()
+      }
+    },
+    [handleUpdateMetrics, handleUpdateSceneLayout]
+  )
 
   const limits = useMemo<Metrics>(() => {
-    const parcels = scene.layout.base.x * scene.layout.base.y
+    const parcels = sceneLayout.parcels.length
     return getSceneLimits(parcels)
-  }, [scene])
+  }, [sceneLayout])
 
   const limitsExceeded = useMemo<Record<string, boolean>>(() => {
     return Object.fromEntries(
@@ -87,9 +108,9 @@ const Metrics = withSdk<WithSdkProps>(({ sdk }) => {
       {showMetrics && (
         <div ref={overlayRef} className="Overlay">
           <h2 className="Header">
-            {scene.layout.base.x}x{scene.layout.base.y} LAND
+            {sceneLayout.parcels.length} Parcels
             <span className="secondary">
-              {scene.layout.base.x * 16}x{scene.layout.base.y * 16}m
+              {sceneLayout.parcels.length * PARCEL_SIZE}m<sup>2</sup>
             </span>
           </h2>
           <div className="Items">
@@ -97,7 +118,8 @@ const Metrics = withSdk<WithSdkProps>(({ sdk }) => {
               <div className="Item" key={key}>
                 <div className="Title">{key.toUpperCase()}</div>
                 <div className={cx('Description', { LimitExceeded: limitsExceeded[key] })}>
-                  <span className="primary">{value}</span>/
+                  <span className="primary">{value}</span>
+                  {'/'}
                   <span className="secondary">{limits[key as keyof Metrics]}</span>
                 </div>
               </div>
