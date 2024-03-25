776cdea9a09b14b157c1f040bdbf1f14dcb3be7a
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-03-25 11:06:42 -0300

    feat: Add SmartItemBasicView for the PointerEvent component

---
+++ packages/@dcl/inspector/src/components/EntityInspector/SmartItemBasicView/PointerEventView/PointerEventView.tsx
@@ -0,0 +1,69 @@
+import React, { useCallback, useEffect } from 'react'
+import { Entity, PBPointerEvents, PBPointerEvents_Entry } from '@dcl/ecs'
+import { withSdk, WithSdkProps } from '../../../../hoc/withSdk'
+import { Block } from '../../../Block'
+import { TextField, Dropdown } from '../../../ui'
+import { useComponentValue } from '../../../../hooks/sdk/useComponentValue'
+import { useArrayState } from '../../../../hooks/useArrayState'
+import { POINTER_EVENTS_TYPES, mapValueToPointerEvent } from '../../PointerEventsInspector/utils'
+
+export default React.memo(
+  withSdk<WithSdkProps & { entity: Entity }>(({ sdk, entity }) => {
+    const { PointerEvents } = sdk.components
+    const [pointerEventComponent, setPointerEventComponentValue, isComponentEqual] = useComponentValue<PBPointerEvents>(
+      entity,
+      PointerEvents
+    )
+    const [pointerEvents, , modifyPointerEvent] = useArrayState<PBPointerEvents_Entry>(
+      pointerEventComponent === null ? [] : pointerEventComponent.pointerEvents
+    )
+
+    useEffect(() => {
+      if (isComponentEqual({ pointerEvents })) return
+      setPointerEventComponentValue({ pointerEvents })
+    }, [pointerEvents])
+
+    const handleHoverTextChange = useCallback(
+      ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+        modifyPointerEvent(0, {
+          ...pointerEvents[0],
+          eventInfo: {
+            ...pointerEvents[0].eventInfo,
+            hoverText: value
+          }
+        })
+      },
+      [pointerEvents, modifyPointerEvent]
+    )
+
+    const handleHoverInteractionChange = useCallback(
+      ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => {
+        modifyPointerEvent(0, {
+          ...pointerEvents[0],
+          eventType: mapValueToPointerEvent(value)!
+        })
+      },
+      [pointerEvents, modifyPointerEvent]
+    )
+
+    return (
+      <>
+        <Block>
+          <TextField
+            label="Hover Text"
+            value={pointerEvents[0]?.eventInfo?.hoverText}
+            onChange={handleHoverTextChange}
+          />
+        </Block>
+        <Block>
+          <Dropdown
+            label="Interaction"
+            value={pointerEvents[0]?.eventType}
+            options={POINTER_EVENTS_TYPES}
+            onChange={handleHoverInteractionChange}
+          />
+        </Block>
+      </>
+    )
+  })
+)
