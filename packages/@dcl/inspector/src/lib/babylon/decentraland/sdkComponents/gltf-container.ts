886336d4f0e85e6fdce1e3c046af3de9744e66cf
Juan Cazala <juancazala@gmail.com>
2024-04-03 09:38:41 -0300

    fix: get entity absolute position

--- packages/@dcl/inspector/src/lib/babylon/decentraland/sdkComponents/gltf-container.ts
+++ packages/@dcl/inspector/src/lib/babylon/decentraland/sdkComponents/gltf-container.ts
@@ -393,19 +393,10 @@ function createLoadingSpinner(
   const CIRCLE = Math.PI * 2
   const LIGHT_GRAY = new BABYLON.Color3(0.9, 0.9, 0.9)
   const torusOutline = createSemiTorus(radius, tube, radialSegments, tubularSegments, CIRCLE, LIGHT_GRAY, scene)
-  torusOutline.position.copyFrom(entity.position)
+  torusOutline.position.copyFrom(entity.getAbsolutePosition())
   torusOutline.position.y += 1
   torusOutline.rotation.x = Math.PI
   torusOutline.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL
-
-  // reposition torus if nested
-  let parent = entity.parent
-  while (parent) {
-    if (parent instanceof BABYLON.TransformNode) {
-      torusOutline.position.addInPlace(parent.position)
-    }
-    parent = parent.parent
-  }
 
   const LIGHT_RED = new BABYLON.Color3(1, 45 / 255, 85 / 255)
   const semiTorus = createSemiTorus(radius, tube, radialSegments, tubularSegments, CIRCLE / 4, LIGHT_RED, scene)
