54fa86c16449e0465ee1dc1c36decd03869d849f
Gonzalo DCL <gonzalo@decentraland.org>
2024-04-19 14:17:40 -0300

    add tests

---
+++ test/ecs/components/RealmInfo.spec.ts
@@ -0,0 +1,25 @@
+import { Engine, components } from '../../../packages/@dcl/ecs/src'
+import { testComponentSerialization } from './assertion'
+
+describe('Generated RealmInfo ProtoBuf', () => {
+  it('should serialize/deserialize move RealmInfo', () => {
+    const newEngine = Engine()
+    const RealmInfo = components.RealmInfo(newEngine)
+
+    testComponentSerialization(RealmInfo, {
+      baseUrl: 'boedo://casla',
+      commsAdapter: 'boedo-casla',
+      networkId: 1,
+      realmName: 'boedo',
+      room: 'casla'
+    })
+
+    testComponentSerialization(RealmInfo, {
+      baseUrl: 'boedo://casla',
+      commsAdapter: 'boedo-casla',
+      networkId: 1,
+      realmName: 'boedo',
+      room: 'casla'
+    })
+  })
+})
