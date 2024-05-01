271786ad25874af9136f4f5f5829dbc077dd4f5c
Juan Cazala <juancazala@gmail.com>
2024-05-01 18:12:05 -0300

    feat: added icons

--- packages/@dcl/inspector/src/components/Hierarchy/Hierarchy.tsx
+++ packages/@dcl/inspector/src/components/Hierarchy/Hierarchy.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { Entity } from '@dcl/ecs'
 import { FiHexagon } from 'react-icons/fi'
 
@@ -7,19 +7,37 @@ import { useEntitiesWith } from '../../hooks/sdk/useEntitiesWith'
 import { useTree } from '../../hooks/sdk/useTree'
 import { Tree } from '../Tree'
 import { ContextMenu } from './ContextMenu'
+import { withSdk } from '../../hoc/withSdk'
 import './Hierarchy.css'
 
-function HierarchyIcon({ value }: { value: Entity }) {
+const HierarchyIcon = withSdk<{ value: Entity }>(({ sdk, value }) => {
+  const isSmart = useMemo(
+    () =>
+      sdk.components.Actions.has(value) ||
+      sdk.components.Triggers.has(value) ||
+      sdk.components.States.has(value) ||
+      sdk.components.TextShape.has(value) ||
+      sdk.components.NftShape.has(value) ||
+      sdk.components.VisibilityComponent.has(value),
+    [sdk]
+  )
+
+  const isGround = useMemo(() => sdk.components.Ground.has(value), [sdk])
+
   if (value === ROOT) {
     return <span style={{ marginRight: '4px' }}></span>
   } else if (value === PLAYER) {
     return <span className="tree-icon player-icon"></span>
   } else if (value === CAMERA) {
     return <span className="tree-icon camera-icon"></span>
+  } else if (isSmart) {
+    return <span className="tree-icon smart-icon"></span>
+  } else if (isGround) {
+    return <span className="tree-icon ground-icon"></span>
   } else {
     return <FiHexagon style={{ marginRight: '4px' }} />
   }
-}
+})
 
 const EntityTree = Tree<Entity>()
 
