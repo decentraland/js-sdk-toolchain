2f7bbb3542f25bc3d56808cfa81f7a3bebb6c3fa
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-05-06 12:55:02 -0300

    refactor: Move LayerOptions constant to types field

--- packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/TriggerProximityAction/types.ts
+++ packages/@dcl/inspector/src/components/EntityInspector/ActionInspector/TriggerProximityAction/types.ts
@@ -1,6 +1,21 @@
-import { ActionPayload, ActionType } from '@dcl/asset-packs'
+import { ActionPayload, ActionType, ProximityLayer } from '@dcl/asset-packs'
 
 export interface Props {
   value: Partial<ActionPayload<ActionType.DAMAGE>>
   onUpdate: (value: ActionPayload<ActionType.DAMAGE>) => void
 }
+
+export const LayerOptions = [
+  {
+    value: ProximityLayer.ALL,
+    label: 'All'
+  },
+  {
+    value: ProximityLayer.PLAYER,
+    label: 'Player'
+  },
+  {
+    value: ProximityLayer.NON_PLAYER,
+    label: 'Non Player'
+  }
+]
