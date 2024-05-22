b9154751a3c344e025d443aa7e4e18e9f6f2e6c9
Nicolas Echezarreta <nicoecheza@gmail.com>
2024-05-22 16:57:15 -0300

    add unique names for new Nodes

--- packages/@dcl/inspector/src/lib/sdk/operations/add-child.ts
+++ packages/@dcl/inspector/src/lib/sdk/operations/add-child.ts
@@ -1,6 +1,7 @@
-import { Entity, IEngine, Transform as TransformEngine, Name as NameEngine } from '@dcl/ecs'
+import { Entity, IEngine, Transform as TransformEngine, Name as NameEngine, NameComponent } from '@dcl/ecs'
+
 import { EditorComponentNames, EditorComponents } from '../components'
-import { pushChild } from '../nodes'
+import { getNodes, pushChild } from '../nodes'
 
 export function addChild(engine: IEngine) {
   return function addChild(parent: Entity, name: string): Entity {
@@ -10,13 +11,43 @@ export function addChild(engine: IEngine) {
     const Name = engine.getComponent(NameEngine.componentName) as typeof NameEngine
 
     // create new child components
-    Name.create(child, { value: name })
+    Name.create(child, { value: generateUniqueName(engine, Name, name) })
     Transform.create(child, { parent })
     // update Nodes component
     Nodes.createOrReplace(engine.RootEntity, { value: pushChild(engine, parent, child) })
 
     return child
   }
+}
+
+export function generateUniqueName(engine: IEngine, Name: NameComponent, value: string): string {
+  const pattern = new RegExp(`^${value.toLowerCase()}(?:_\\d+)*$`, 'i')
+  const nodes = getNodes(engine)
+
+  let isFirst = true
+  let max = 0
+  for (const $ of nodes) {
+    const name = (Name.getOrNull($.entity)?.value || '').toLowerCase()
+    if (pattern.test(name)) {
+      isFirst = false
+      const suffix = getSuffixDigits(name)
+      if (suffix !== -1) {
+        max = Math.max(max, suffix)
+      }
+    }
+  }
+
+  const suffix = isFirst ? '' : `_${max + 1}`
+
+  return `${value}${suffix}`
+}
+
+function getSuffixDigits(name: string): number {
+  const underscoreIndex = name.lastIndexOf('_')
+  if (underscoreIndex === -1 || underscoreIndex === name.length - 1) return -1
+
+  const digits = name.slice(underscoreIndex + 1)
+  return /^\d+$/.test(digits) ? parseInt(digits, 10) : -1
 }
 
 export default addChild
