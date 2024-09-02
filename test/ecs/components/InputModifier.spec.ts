cbd2ce600cebb817d4e46e1e0efcf95db4b648de
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2024-09-02 16:14:40 -0300

    Formatting cleanup

--- test/ecs/components/InputModifier.spec.ts
+++ test/ecs/components/InputModifier.spec.ts
@@ -4,18 +4,18 @@ import { testComponentSerialization } from './assertion'
 describe('Generated InputModifier ProtoBuf', () => {
   it('should serialize/deserialize InputModifier', () => {
     const newEngine = Engine()
-    const InputModifier= components.InputModifier(newEngine)
-    
+    const InputModifier = components.InputModifier(newEngine)
+
     testComponentSerialization(InputModifier, {
       mode: {
         $case: 'standard',
-        standard:{
-          disableAll:true,
-          disableWalk:true,
-          disableJog:true,
-          disableRun:true,
-          disableJump:true,
-          disableEmote:true
+        standard: {
+          disableAll: true,
+          disableWalk: true,
+          disableJog: true,
+          disableRun: true,
+          disableJump: true,
+          disableEmote: true
         }
       }
     })
