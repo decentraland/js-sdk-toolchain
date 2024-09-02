ce6281260608b043ad72f4f7a1b46af6d7e474fb
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2024-09-02 16:12:17 -0300

    Updated protocol to point to latest. Added MapPin component ID to protocol-buffer-generation non-exposed list. Rebuilt proto classes. Added test for serialization of InputModifier component

---
+++ test/ecs/components/InputModifier.spec.ts
@@ -0,0 +1,23 @@
+import { components, Engine } from '../../../packages/@dcl/ecs/src'
+import { testComponentSerialization } from './assertion'
+
+describe('Generated InputModifier ProtoBuf', () => {
+  it('should serialize/deserialize InputModifier', () => {
+    const newEngine = Engine()
+    const InputModifier= components.InputModifier(newEngine)
+    
+    testComponentSerialization(InputModifier, {
+      mode: {
+        $case: 'standard',
+        standard:{
+          disableAll:true,
+          disableWalk:true,
+          disableJog:true,
+          disableRun:true,
+          disableJump:true,
+          disableEmote:true
+        }
+      }
+    })
+  })
+})
