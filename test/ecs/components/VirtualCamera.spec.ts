3e2431e734aa49b95878c5d2291f412f3ff91cd7
Pravus <pravusjif@gmail.com>
2024-09-05 23:33:10 +0200

    added missing oneof case test

--- test/ecs/components/VirtualCamera.spec.ts
+++ test/ecs/components/VirtualCamera.spec.ts
@@ -2,7 +2,7 @@ import { Engine, components } from '../../../packages/@dcl/ecs/src'
 import { testComponentSerialization } from './assertion'
 
 describe('VirtualCamera component', () => {
-  it('should serialize', () => {
+  it('should serialize with time transition', () => {
     const newEngine = Engine()
     testComponentSerialization(components.VirtualCamera(newEngine), {
       defaultTransition: {
@@ -12,6 +12,19 @@ describe('VirtualCamera component', () => {
         }
       },
       lookAtEntity: 35
+    })
+  })
+
+  it('should serialize with speed transition', () => {
+    const newEngine = Engine()
+    testComponentSerialization(components.VirtualCamera(newEngine), {
+      defaultTransition: {
+        transitionMode: {
+          $case: "speed",
+          speed: 12.8
+        }
+      },
+      lookAtEntity: 86
     })
   })
 })
