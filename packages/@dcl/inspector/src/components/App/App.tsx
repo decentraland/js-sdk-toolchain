39953ab640c487f12ea57e4b8ce95222860c519c
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-10-09 20:05:58 -0300

    fix: Min size for Assets Panel

--- packages/@dcl/inspector/src/components/App/App.tsx
+++ packages/@dcl/inspector/src/components/App/App.tsx
@@ -1,15 +1,7 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { PanelGroup, Panel, PanelResizeHandle } from 'react-resizable-panels'
 import cx from 'classnames'
 
-import { EntityInspector } from '../EntityInspector'
-import { Hierarchy } from '../Hierarchy'
-import { Renderer } from '../Renderer'
-import { Box } from '../Box'
-import { Toolbar } from '../Toolbar'
-
-import './App.css'
-import Assets from '../Assets'
 import { useSelectedEntity } from '../../hooks/sdk/useSelectedEntity'
 import { useWindowSize } from '../../hooks/useWindowSize'
 import { useAppSelector } from '../../redux/hooks'
@@ -18,12 +10,33 @@ import { selectEngines } from '../../redux/sdk'
 import { getHiddenPanels } from '../../redux/ui'
 import { PanelName } from '../../redux/ui/types'
 
+import { EntityInspector } from '../EntityInspector'
+import { Hierarchy } from '../Hierarchy'
+import { Renderer } from '../Renderer'
+import { Box } from '../Box'
+import { Toolbar } from '../Toolbar'
+import Assets from '../Assets'
+
+import './App.css'
+
 const App = () => {
   const selectedEntity = useSelectedEntity()
+  const { height } = useWindowSize()
 
   const sdkInitialized = useAppSelector(selectEngines).inspector
 
   const hiddenPanels = useAppSelector(getHiddenPanels)
+
+  const [isAssetsPanelCollapsed, setIsAssetsPanelCollapsed] = useState(false)
+
+  const handleToggleAssetsPanel = useCallback((collapse: boolean) => {
+    setIsAssetsPanelCollapsed(collapse)
+  }, [])
+
+  // Collapse the panel at 75 pixels
+  const collapseAt = (75 / Math.max(1, height ?? 1)) * 100
+  // Footer's height is 48 pixels, so we need to calculate the percentage of the screen that it takes to pass as the minSize prop for the Panel
+  const footerMin = (48 / Math.max(1, height ?? 1)) * 100
 
   const disconnected = useAppSelector(selectDataLayerError)
   return (
@@ -73,9 +86,15 @@ const App = () => {
         {!hiddenPanels[PanelName.ASSETS] && (
           <>
             <PanelResizeHandle className="vertical-handle" />
-            <Panel defaultSize={30} style={{ minHeight: '48px' }}>
+            <Panel
+              id="assets"
+              defaultSize={30}
+              {...(height ? { collapsible: true, collapsedSize: footerMin, minSize: collapseAt } : {})}
+              onCollapse={() => handleToggleAssetsPanel(true)}
+              onExpand={() => handleToggleAssetsPanel(false)}
+            >
               <Box className="composite-renderer">
-                <Assets />
+                <Assets isAssetsPanelCollapsed={isAssetsPanelCollapsed} />
               </Box>
             </Panel>
           </>
