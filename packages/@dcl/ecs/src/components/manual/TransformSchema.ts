42260da6e56a90a60ffe975b9d44a76a97db27f4
Lean Mendoza <leandro@decentraland.org>
2024-11-10 08:57:48 -0300

    fix unexistent files

---
+++ packages/@dcl/ecs/src/components/manual/TransformSchema.ts
@@ -0,0 +1,110 @@
+import { Entity } from '../../engine/entity'
+import type { ISchema } from '../../schemas/ISchema'
+import { ByteBuffer } from '../../serialization/ByteBuffer'
+
+/**
+ * @internal
+ */
+export const COMPONENT_ID = 1
+
+/**
+ * @public
+ */
+export type TransformType = {
+  position: { x: number; y: number; z: number }
+  rotation: { x: number; y: number; z: number; w: number }
+  scale: { x: number; y: number; z: number }
+  parent?: Entity
+}
+
+/** @internal */
+export const TRANSFORM_LENGTH = 44
+
+/** @internal */
+export const TransformSchema: ISchema<TransformType> = {
+  serialize(value: TransformType, builder: ByteBuffer): void {
+    const ptr = builder.incrementWriteOffset(TRANSFORM_LENGTH)
+    builder.setFloat32(ptr, value.position.x)
+    builder.setFloat32(ptr + 4, value.position.y)
+    builder.setFloat32(ptr + 8, value.position.z)
+    builder.setFloat32(ptr + 12, value.rotation.x)
+    builder.setFloat32(ptr + 16, value.rotation.y)
+    builder.setFloat32(ptr + 20, value.rotation.z)
+    builder.setFloat32(ptr + 24, value.rotation.w)
+    builder.setFloat32(ptr + 28, value.scale.x)
+    builder.setFloat32(ptr + 32, value.scale.y)
+    builder.setFloat32(ptr + 36, value.scale.z)
+    builder.setUint32(ptr + 40, (value.parent as number) || 0)
+  },
+  deserialize(reader: ByteBuffer): TransformType {
+    const ptr = reader.incrementReadOffset(TRANSFORM_LENGTH)
+    return {
+      position: {
+        x: reader.getFloat32(ptr),
+        y: reader.getFloat32(ptr + 4),
+        z: reader.getFloat32(ptr + 8)
+      },
+      rotation: {
+        x: reader.getFloat32(ptr + 12),
+        y: reader.getFloat32(ptr + 16),
+        z: reader.getFloat32(ptr + 20),
+        w: reader.getFloat32(ptr + 24)
+      },
+      scale: {
+        x: reader.getFloat32(ptr + 28),
+        y: reader.getFloat32(ptr + 32),
+        z: reader.getFloat32(ptr + 36)
+      },
+      parent: reader.getUint32(ptr + 40) as Entity
+    }
+  },
+  create(): TransformType {
+    return {
+      position: { x: 0, y: 0, z: 0 },
+      scale: { x: 1, y: 1, z: 1 },
+      rotation: { x: 0, y: 0, z: 0, w: 1 },
+      parent: 0 as Entity
+    }
+  },
+  extend(value?: Partial<TransformType>) {
+    return {
+      position: { x: 0, y: 0, z: 0 },
+      scale: { x: 1, y: 1, z: 1 },
+      rotation: { x: 0, y: 0, z: 0, w: 1 },
+      parent: 0 as Entity,
+      ...value
+    }
+  },
+  jsonSchema: {
+    type: 'object',
+    properties: {
+      position: {
+        type: 'object',
+        properties: {
+          x: { type: 'number' },
+          y: { type: 'number' },
+          z: { type: 'number' }
+        }
+      },
+      scale: {
+        type: 'object',
+        properties: {
+          x: { type: 'number' },
+          y: { type: 'number' },
+          z: { type: 'number' }
+        }
+      },
+      rotation: {
+        type: 'object',
+        properties: {
+          x: { type: 'number' },
+          y: { type: 'number' },
+          z: { type: 'number' },
+          w: { type: 'number' }
+        }
+      },
+      parent: { type: 'integer' }
+    },
+    serializationType: 'transform'
+  }
+}
