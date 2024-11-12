422713a03a6e2c64d6fe8f4e67f97afd7e3c0a74
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2024-11-11 22:39:07 -0300

    Updated protocol, regenerated API and run a lint pass

--- packages/@dcl/ecs/src/components/extended/InputModifier.ts
+++ packages/@dcl/ecs/src/components/extended/InputModifier.ts
@@ -1,6 +1,9 @@
-import { ReadOnlyLastWriteWinElementSetComponentDefinition, IEngine } from "../../engine"
-import { InputModifier } from "../generated/index.gen"
-import { PBInputModifier_StandardInput, PBInputModifier } from "../generated/pb/decentraland/sdk/components/input_modifier.gen"
+import { ReadOnlyLastWriteWinElementSetComponentDefinition, IEngine } from '../../engine'
+import { InputModifier } from '../generated/index.gen'
+import {
+  PBInputModifier_StandardInput,
+  PBInputModifier
+} from '../generated/pb/decentraland/sdk/components/input_modifier.gen'
 
 export interface InputModifierHelper {
   /**
