3e57d307b071ef63a2b2a086d4d9453cc2ccef05
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2025-01-16 13:18:33 -0300

    Added test

--- test/ecs/components/LightSource.spec.ts
+++ test/ecs/components/LightSource.spec.ts
@@ -15,4 +15,21 @@ describe('Generated LightSource ProtoBuf', () => {
       shadow: components.PBLightSource_ShadowType.ST_NONE
     })
   })
+
+  it('should serialize/deserialize Spot LightSource', () => {
+    const newEngine = Engine()
+    const LightSource = components.LightSource(newEngine)
+
+    testComponentSerialization(LightSource, {
+      type: LightSource.Type.Spot({
+        innerAngle: 0,
+        outerAngle: 0
+      }),
+      active: true,
+      color: { r: 1, g: 1, b: 1 },
+      brightness: 1,
+      range: 10,
+      shadow: components.PBLightSource_ShadowType.ST_NONE
+    })
+  })
 })
