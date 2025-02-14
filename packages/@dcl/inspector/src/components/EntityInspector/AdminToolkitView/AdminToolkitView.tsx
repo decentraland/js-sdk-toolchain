99ad6fa3b6c3bd0e0f89301bf877f86de2104cf8
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-02-14 01:02:40 -0300

    feat: Refactor AdminToolkitView

--- packages/@dcl/inspector/src/components/EntityInspector/AdminToolkitView/AdminToolkitView.tsx
+++ packages/@dcl/inspector/src/components/EntityInspector/AdminToolkitView/AdminToolkitView.tsx
@@ -1,89 +1,24 @@
 import React, { useCallback } from 'react'
-import { AiOutlineInfoCircle as InfoIcon } from 'react-icons/ai'
-
 import { withSdk } from '../../../hoc/withSdk'
 import { useComponentValue } from '../../../hooks/sdk/useComponentValue'
 import { useHasComponent } from '../../../hooks/sdk/useHasComponent'
 
-import { Accordion, CheckboxField, CheckboxGroup, TextField, Vector3Field, Dropdown } from '../../ui'
+import { Accordion } from '../../ui'
 import { Container } from '../../Container'
 import { Block } from '../../Block'
-import { Button } from '../../Button'
 
 import { RewardsControl } from './RewardsControl'
 import { VideoControl } from './VideoControl'
 import { SmartItemControl } from './SmartItemControl'
+import { AdminAllowListControl } from './AdminAllowListControl'
+import { TextAnnouncementControl } from './TextAnnouncementControl'
 import { type Props } from './types'
 import './AdminToolkitView.css'
-
-enum DEFAULT_ADMIN_PERMISSIONS {
-  PUBLIC = 'PUBLIC',
-  PRIVATE = 'PRIVATE'
-}
 
 const AdminToolkitView = withSdk<Props>(({ sdk, entity }) => {
   const { AdminTools } = sdk.components
   const [adminComponent, setAdminComponent] = useComponentValue(entity, AdminTools)
   const hasAdmintoolkit = useHasComponent(entity, AdminTools)
-
-  const handleDefaultAdminPermissionChange = useCallback(
-    (field: string) => (event: React.ChangeEvent<HTMLSelectElement>) => {
-      if (!adminComponent) return
-      setAdminComponent({
-        ...adminComponent,
-        [field]: event.target.value
-      })
-    },
-    [adminComponent, setAdminComponent]
-  )
-
-  const handleTextChange = useCallback(
-    (field: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (!adminComponent) return
-      setAdminComponent({
-        ...adminComponent,
-        [field]: event.target.value
-      })
-    },
-    [adminComponent, setAdminComponent]
-  )
-
-  const handleBooleanChange = useCallback(
-    (field: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (!adminComponent) return
-      setAdminComponent({
-        ...adminComponent,
-        [field]: event.target.checked
-      })
-    },
-    [adminComponent, setAdminComponent]
-  )
-
-  const handleKickCoordinatesChange = useCallback(
-    (value: { x: number; y: number; z: number }) => {
-      if (!adminComponent) return
-      setAdminComponent({
-        ...adminComponent,
-        moderationControl: {
-          ...adminComponent.moderationControl,
-          kickCoordinates: value
-        }
-      })
-    },
-    [adminComponent, setAdminComponent]
-  )
-
-  const renderDefaultAdminPermissionsInfoMessage = useCallback(
-    () => (
-      <>
-        <InfoIcon size="10" />
-        {adminComponent.adminPermissions === DEFAULT_ADMIN_PERMISSIONS.PUBLIC
-          ? 'All scene visitors will have access to the Scene Tools panel.'
-          : 'Admin permissions are restricted to authorized users only.'}
-      </>
-    ),
-    [adminComponent.adminPermissions]
-  )
 
   const handleToggleEnabled = useCallback(
     (
@@ -108,41 +43,9 @@ const AdminToolkitView = withSdk<Props>(({ sdk, entity }) => {
 
   return (
     <Container label="Admin Tools" className="AdminToolkitViewInspector">
-      {/* <Block className="DefaultAdminPermissions">
-        <Dropdown
-          className="DefaultAdminPermissionsDropdown"
-          label="Default Admin Permissions"
-          value={adminComponent.adminPermissions || DEFAULT_ADMIN_PERMISSIONS.PUBLIC}
-          onChange={handleDefaultAdminPermissionChange('adminPermissions')}
-          options={[
-            { value: DEFAULT_ADMIN_PERMISSIONS.PUBLIC, label: 'Public' },
-            { value: DEFAULT_ADMIN_PERMISSIONS.PRIVATE, label: 'Private' }
-          ]}
-          info={renderDefaultAdminPermissionsInfoMessage()}
-        />
-        {adminComponent.adminPermissions === DEFAULT_ADMIN_PERMISSIONS.PRIVATE ? (
-          <>
-            <CheckboxGroup label="Authorized users" className="AuthorizedUsersCheckboxGroup">
-              <CheckboxField label="Me" checked={adminComponent.authorizedAdminUsers.me} disabled />
-              <CheckboxField
-                label="Scene Owners"
-                checked={adminComponent.authorizedAdminUsers.sceneOwners}
-                onChange={handleBooleanChange('showAuthorText')}
-              />
-              <CheckboxField
-                label="Allow List"
-                checked={adminComponent.authorizedAdminUsers.allowList}
-                onChange={handleBooleanChange('showAuthorText')}
-              />
-            </CheckboxGroup>
-            <TextField label="Admin Allow List" value={''} onChange={handleTextChange('adminAllowList')} />
-            <Button>Add</Button>
-            {adminComponent.authorizedAdminUsers.adminAllowList.map((user) => (
-              <span>{user}</span>
-            ))}
-          </>
-        ) : null}
-      </Block> */}
+      <Block>
+        <AdminAllowListControl entity={entity} />
+      </Block>
       {/* <Accordion
         label="MODERATION"
         className="PanelSection"
@@ -163,18 +66,7 @@ const AdminToolkitView = withSdk<Props>(({ sdk, entity }) => {
         enabled={!!adminComponent.textAnnouncementControl.isEnabled}
         onToggleEnabled={(enabled) => handleToggleEnabled('textAnnouncementControl', enabled)}
       >
-        <CheckboxGroup>
-          <CheckboxField
-            label="Play a sound each time a new announcement is made"
-            checked={adminComponent.textAnnouncementControl.playSoundOnEachAnnouncement || false}
-            onChange={handleBooleanChange('playSoundOnEachAnnouncement')}
-          />
-          <CheckboxField
-            label="Show author on each announcements"
-            checked={adminComponent.textAnnouncementControl.showAuthorOnEachAnnouncement || false}
-            onChange={handleBooleanChange('showAuthorOnEachAnnouncement')}
-          />
-        </CheckboxGroup>
+        <TextAnnouncementControl entity={entity} />
       </Accordion>
 
       <Accordion
