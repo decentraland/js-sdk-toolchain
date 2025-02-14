58536ec836aeef36b6c84df5bbbdc58a5901b0c7
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-02-14 01:01:47 -0300

    feat: Move TextAnnouncementControl section to a new component

---
+++ packages/@dcl/inspector/src/components/EntityInspector/AdminToolkitView/TextAnnouncementControl/TextAnnouncementControl.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback } from 'react'
+import { Entity } from '@dcl/ecs'
+import { withSdk } from '../../../../hoc/withSdk'
+import { useComponentValue } from '../../../../hooks/sdk/useComponentValue'
+import { CheckboxGroup, CheckboxField } from '../../../ui'
+import './TextAnnouncementControl.css'
+
+type Props = {
+  entity: Entity
+}
+
+type TextAnnouncementControlData = {
+  isEnabled: boolean
+  playSoundOnEachAnnouncement: boolean
+  showAuthorOnEachAnnouncement: boolean
+}
+
+const TextAnnouncementControl = withSdk<Props>(({ sdk, entity }) => {
+  const { AdminTools } = sdk.components
+  const [adminComponent, setAdminComponent] = useComponentValue(entity, AdminTools)
+
+  const handleBooleanChange = useCallback(
+    (field: keyof Omit<TextAnnouncementControlData, 'isEnabled'>) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (!adminComponent) return
+      setAdminComponent({
+        ...adminComponent,
+        textAnnouncementControl: {
+          ...adminComponent.textAnnouncementControl,
+          [field]: event.target.checked
+        }
+      })
+    },
+    [adminComponent, setAdminComponent]
+  )
+
+  if (!adminComponent) return null
+
+  const { textAnnouncementControl } = adminComponent
+
+  return (
+    <div className="TextAnnouncementControl">
+      <CheckboxGroup>
+        <CheckboxField
+          label="Play a sound each time a new announcement is made"
+          checked={textAnnouncementControl.playSoundOnEachAnnouncement || false}
+          onChange={handleBooleanChange('playSoundOnEachAnnouncement')}
+        />
+        <CheckboxField
+          label="Show author on each announcements"
+          checked={textAnnouncementControl.showAuthorOnEachAnnouncement || false}
+          onChange={handleBooleanChange('showAuthorOnEachAnnouncement')}
+        />
+      </CheckboxGroup>
+    </div>
+  )
+})
+
+export default React.memo(TextAnnouncementControl)
