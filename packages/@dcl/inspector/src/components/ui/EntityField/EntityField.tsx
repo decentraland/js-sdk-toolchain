8d0d66b137c068c3189886cfa77b3fb832e5a4ee
Alejandra Levy <alelevy15@gmail.com>
2025-05-07 14:42:18 +0200

    feat: show selected entity first on dropdown (#1112)
    * feat: show selected entity first on dropdown
    
    * feat: remove unused import and reorder import
    
    * Update packages/@dcl/inspector/src/components/ui/EntityField/EntityField.tsx
    
    Co-authored-by: Gabriel Díaz <git@subsme.slmail.me>
    Signed-off-by: Alejandra Levy <alelevy15@gmail.com>
    
    * feat: update method
    
    ---------
    
    Signed-off-by: Alejandra Levy <alelevy15@gmail.com>
    Co-authored-by: Gabriel Díaz <git@subsme.slmail.me>

--- packages/@dcl/inspector/src/components/ui/EntityField/EntityField.tsx
+++ packages/@dcl/inspector/src/components/ui/EntityField/EntityField.tsx
@@ -3,6 +3,7 @@ import cx from 'classnames'
 import { BiCube as EntityIcon } from 'react-icons/bi'
 import { Entity } from '@dcl/ecs'
 import { withSdk, WithSdkProps } from '../../../hoc/withSdk'
+import { useSelectedEntity } from '../../../hooks/sdk/useSelectedEntity'
 import { Dropdown } from '../Dropdown'
 import type { Props } from './types'
 
@@ -12,17 +13,23 @@ function componentHasValidValue(component: any) {
   } else if (typeof component.value === 'object') {
     return component.value.length > 0
   }
-
   return false
+}
+
+type EntityOption = {
+  label: string
+  value: Entity
+  leftIcon: React.ReactNode
 }
 
 const EntityField: React.FC<WithSdkProps & Props> = ({ sdk, ...props }) => {
   const { engine } = sdk
   const { Name, Nodes } = sdk.components
   const { className, components, disabled, label, value, onChange } = props
+  const selectedEntity = useSelectedEntity()
 
-  const options: Record<string, any>[] = useMemo(() => {
-    const uniqueEntities = new Map()
+  const options: EntityOption[] = useMemo(() => {
+    const uniqueEntities = new Map<Entity, EntityOption>()
 
     const mapEntity = (entity: Entity) => {
       uniqueEntities.set(entity, {
@@ -30,6 +37,22 @@ const EntityField: React.FC<WithSdkProps & Props> = ({ sdk, ...props }) => {
         value: entity,
         leftIcon: <EntityIcon />
       })
+    }
+
+    const reorderEntities = () => {
+      const shouldOrder = selectedEntity && uniqueEntities.has(selectedEntity)
+
+      if (!shouldOrder) return uniqueEntities
+
+      const orderedMap = new Map<Entity, EntityOption>()
+      orderedMap.set(selectedEntity, uniqueEntities.get(selectedEntity)!)
+
+      for (const [entity, option] of uniqueEntities.entries()) {
+        if (entity === selectedEntity) continue
+        orderedMap.set(entity, option)
+      }
+
+      return orderedMap
     }
 
     if (components && components.length > 0) {
@@ -51,8 +74,7 @@ const EntityField: React.FC<WithSdkProps & Props> = ({ sdk, ...props }) => {
         }
       }
     }
-
-    return Array.from(uniqueEntities.values())
+    return Array.from(reorderEntities().values())
   }, [components])
 
   const emptyMessage = useMemo(() => {
