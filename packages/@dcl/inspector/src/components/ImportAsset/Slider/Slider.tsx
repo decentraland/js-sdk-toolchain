5aeda1dcbdc29b6364bf51a2ce8a9436c9348f06
alejandralevy <alelevy15@gmail.com>
2025-06-11 13:04:52 +0200

    feat: take slider logic to other component

--- packages/@dcl/inspector/src/components/ImportAsset/Slider/Slider.tsx
+++ packages/@dcl/inspector/src/components/ImportAsset/Slider/Slider.tsx
@@ -1,13 +1,9 @@
 import { useCallback, useMemo, useState } from 'react'
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
-import cx from 'classnames'
 
-import { AssetPreview } from '../../AssetPreview'
 import { Button } from '../../Button'
-import { Input } from '../../Input'
-import { Loading } from '../../Loading'
+import { AssetSlides } from './AssetSlides'
 
-import { getAssetSize, getAssetResources, determineAssetType, formatFileName } from '../utils'
+import { determineAssetType, formatFileName } from '../utils'
 
 import { Asset } from '../types'
 import { PropTypes, Thumbnails } from './types'
@@ -15,7 +11,7 @@ import { PropTypes, Thumbnails } from './types'
 import './Slider.css'
 
 export function Slider({ assets, onSubmit, isNameValid }: PropTypes) {
-  const [value, setValue] = useState(assets)
+  const [uploadedAssets, setUploadedAssets] = useState(assets)
   const [slide, setSlide] = useState(0)
   const [screenshots, setScreenshots] = useState<Thumbnails>({})
 
@@ -29,53 +25,47 @@ export function Slider({ assets, onSubmit, isNameValid }: PropTypes) {
     [screenshots]
   )
 
-  const handlePrevClick = useCallback(() => {
-    setSlide(Math.max(0, slide - 1))
-  }, [slide])
-
-  const handleNextClick = useCallback(() => {
-    setSlide(Math.min(value.length - 1, slide + 1))
-  }, [slide])
-
   const handleNameChange = useCallback(
     (fileIdx: number) => (newName: string) => {
-      setValue(
-        value.map(($, i) => {
+      setUploadedAssets(
+        uploadedAssets.map(($, i) => {
           if (fileIdx !== i) return $
           return { ...$, name: newName }
         })
       )
     },
-    [value]
+    [uploadedAssets]
   )
 
   const handleSubmit = useCallback(() => {
     onSubmit(
-      value.map(($) => ({
+      uploadedAssets.map(($) => ({
         ...$,
         thumbnail: screenshots[$.blob.name]
       }))
     )
-  }, [value, screenshots])
+  }, [uploadedAssets, screenshots])
 
-  const manyAssets = useMemo(() => value.length > 1, [value])
-  const countText = useMemo(() => `${slide + 1}/${value.length}`, [slide, value])
-  const importText = useMemo(() => `IMPORT${manyAssets ? ' ALL' : ''}`, [manyAssets])
-  const leftArrowDisabled = useMemo(() => slide <= 0, [slide])
-  const rightArrowDisabled = useMemo(() => slide >= value.length - 1, [slide, value])
+  const manyAssets = useMemo(() => uploadedAssets.length > 1, [uploadedAssets])
+  const countText = useMemo(() => `${slide + 1}/${uploadedAssets.length}`, [slide, uploadedAssets])
+  const importText = useMemo(
+    () => `IMPORT${manyAssets ? ` ALL (${uploadedAssets.length})` : ''}`,
+    [manyAssets, uploadedAssets.length]
+  )
+
   const allScreenshotsTaken = useMemo(() => {
-    const neededScreenshots = value.filter(($) => {
+    const neededScreenshots = uploadedAssets.filter(($) => {
       const type = determineAssetType($.extension)
       return type === 'Models' || type === 'Images'
     })
     return neededScreenshots.length === Object.keys(screenshots).length
-  }, [value, screenshots])
+  }, [uploadedAssets, screenshots])
 
   const invalidNames = useMemo(() => {
     const all = new Set<string>()
     const invalid = new Set<string>()
 
-    for (const asset of value) {
+    for (const asset of uploadedAssets) {
       const name = formatFileName(asset)
       if (all.has(name) || !isNameValid(asset, name)) {
         invalid.add(name)
@@ -85,7 +75,7 @@ export function Slider({ assets, onSubmit, isNameValid }: PropTypes) {
     }
 
     return invalid
-  }, [value])
+  }, [uploadedAssets])
 
   const isNameUnique = useCallback(
     (asset: Asset) => {
@@ -95,40 +85,20 @@ export function Slider({ assets, onSubmit, isNameValid }: PropTypes) {
     [invalidNames]
   )
 
-  if (!value.length) return null
+  if (!uploadedAssets.length) return null
 
   return (
     <div className="Slider">
       {manyAssets && <span className="counter">{countText}</span>}
-      <div className="content">
-        {manyAssets && (
-          <span className={cx('left', { disabled: leftArrowDisabled })} onClick={handlePrevClick}>
-            <IoIosArrowBack />
-          </span>
-        )}
-        <div className="slides">
-          {value.map(($, i) => (
-            <div className={cx('asset', { active: slide === i })} key={i}>
-              <div>
-                <AssetPreview value={$.blob} resources={getAssetResources($)} onScreenshot={handleScreenshot($)} />
-                {screenshots[$.blob.name] ? (
-                  <div className="thumbnail" style={{ backgroundImage: `url(${screenshots[$.blob.name]})` }}></div>
-                ) : (
-                  <Loading dimmer={false} />
-                )}
-                <Input value={$.name} onChange={handleNameChange(i)} />
-                {!isNameUnique($) && <span className="name-error">Filename already exists</span>}
-              </div>
-              <span className="size">{getAssetSize($)}</span>
-            </div>
-          ))}
-        </div>
-        {manyAssets && (
-          <span className={cx('right', { disabled: rightArrowDisabled })} onClick={handleNextClick}>
-            <IoIosArrowForward />
-          </span>
-        )}
-      </div>
+      <AssetSlides
+        uploadedAssets={uploadedAssets}
+        currentSlide={slide}
+        screenshots={screenshots}
+        onSlideChange={setSlide}
+        onScreenshot={handleScreenshot}
+        onNameChange={handleNameChange}
+        isNameUnique={isNameUnique}
+      />
       <Button type="danger" size="big" onClick={handleSubmit} disabled={!allScreenshotsTaken || invalidNames.size > 0}>
         {importText}
       </Button>
