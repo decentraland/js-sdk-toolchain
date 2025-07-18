9b026d651a958687259fa072521ab2df04605664
Nicolas Echezarreta <nicoecheza@gmail.com>
2025-07-18 13:31:14 -0300

    feat: add free movement gizmo indicator

--- packages/@dcl/inspector/src/lib/babylon/decentraland/gizmos/FreeGizmo.ts
+++ packages/@dcl/inspector/src/lib/babylon/decentraland/gizmos/FreeGizmo.ts
@@ -1,38 +1,57 @@
-import { Vector3, TransformNode, Scene, UtilityLayerRenderer, PointerDragBehavior, AbstractMesh } from '@babylonjs/core'
+import {
+  Vector3,
+  TransformNode,
+  Scene,
+  UtilityLayerRenderer,
+  PointerDragBehavior,
+  AbstractMesh,
+  MeshBuilder,
+  StandardMaterial,
+  Color3,
+  DynamicTexture
+} from '@babylonjs/core'
 import { Entity } from '@dcl/ecs'
 import { EcsEntity } from '../EcsEntity'
 import { IGizmoTransformer } from './types'
 import { LEFT_BUTTON } from '../mouse-utils'
 import { snapVector } from '../snap-manager'
 
+interface GizmoManagerInterface {
+  calculateCentroid: () => Vector3
+  updateGizmoPosition: () => void
+}
+
 export class FreeGizmo implements IGizmoTransformer {
   private selectedEntities: EcsEntity[] = []
-  private dragBehavior: PointerDragBehavior
-  private pivotPosition: Vector3 | null = null
-  private entityOffsets = new Map<Entity, Vector3>()
   private isDragging = false
-  private onDragEndCallback: (() => void) | null = null
-  private updateEntityPosition: ((entity: EcsEntity) => void) | null = null
-  private dispatchOperations: (() => void) | null = null
-  // This property is not used in the free gizmo, but it is required by the IGizmoTransformer interface
+  private snapDistance = 0
   private isWorldAligned = true
+
+  private dragBehavior: PointerDragBehavior
   private dragStartObserver: any = null
   private dragObserver: any = null
   private dragEndObserver: any = null
-  private snapDistance = 0
+
+  private pivotPosition: Vector3 | null = null
   private lastSnappedPivotPosition: Vector3 | null = null
+  private entityOffsets = new Map<Entity, Vector3>()
+
+  private gizmoIndicator: AbstractMesh | null = null
+
+  private onDragEndCallback: (() => void) | null = null
+  private updateEntityPosition: ((entity: EcsEntity) => void) | null = null
+  private dispatchOperations: (() => void) | null = null
+
+  private gizmoManager: GizmoManagerInterface | null = null
 
   constructor(private scene: Scene, private utilityLayer: UtilityLayerRenderer = new UtilityLayerRenderer(scene)) {
-    this.dragBehavior = new PointerDragBehavior({ dragPlaneNormal: new Vector3(0, 1, 0) })
-    this.dragBehavior.useObjectOrientationForDragging = false
-    // Configure drag behavior to only work with left click
-    this.dragBehavior.dragButtons = [LEFT_BUTTON]
-    this.dragBehavior.moveAttached = false // disable object following the cursor...
+    this.dragBehavior = this.createDragBehavior()
   }
 
   setup(): void {
     this.cleanup()
     this.setupSceneObservers()
+    this.createGizmoIndicator()
   }
 
   enable(): void {
@@ -43,18 +62,13 @@ export class FreeGizmo implements IGizmoTransformer {
     this.removeSceneObservers()
     this.cleanupDragObservers()
     this.detachDragBehavior()
-    this.selectedEntities = []
-    this.pivotPosition = null
-    this.lastSnappedPivotPosition = null
-    this.entityOffsets.clear()
-    this.isDragging = false
-    this.onDragEndCallback = null
-    this.updateEntityPosition = null
-    this.dispatchOperations = null
+    this.removeGizmoIndicator()
+    this.resetState()
   }
 
   setEntities(entities: EcsEntity[]): void {
     this.selectedEntities = entities
+    this.updateGizmoIndicator()
   }
 
   setUpdateCallbacks(updateEntityPosition: (entity: EcsEntity) => void, dispatchOperations: () => void): void {
@@ -63,7 +77,6 @@ export class FreeGizmo implements IGizmoTransformer {
   }
 
   setWorldAligned(value: boolean): void {
-    // For free gizmo, world alignment is not used
     this.isWorldAligned = value
   }
 
@@ -75,13 +88,66 @@ export class FreeGizmo implements IGizmoTransformer {
     this.onDragEndCallback = callback
   }
 
+  setGizmoManager(gizmoManager: GizmoManagerInterface): void {
+    this.gizmoManager = gizmoManager
+  }
+
+  onDragStart(entities: EcsEntity[], _gizmoNode: TransformNode): void {
+    this.selectedEntities = entities
+    this.resetDragState()
+  }
+
+  update(entities: EcsEntity[], _gizmoNode: TransformNode): void {
+    if (entities !== this.selectedEntities) {
+      this.selectedEntities = entities
+      this.resetDragState()
+    }
+  }
+
+  onDragEnd(): void {
+    this.cleanup()
+  }
+
+  dispose(): void {
+    this.cleanup()
+    this.utilityLayer.dispose()
+  }
+
+  private createDragBehavior(): PointerDragBehavior {
+    const behavior = new PointerDragBehavior({ dragPlaneNormal: new Vector3(0, 1, 0) })
+    behavior.useObjectOrientationForDragging = false
+    behavior.dragButtons = [LEFT_BUTTON]
+    behavior.moveAttached = false
+    return behavior
+  }
+
+  private resetState(): void {
+    this.selectedEntities = []
+    this.pivotPosition = null
+    this.lastSnappedPivotPosition = null
+    this.entityOffsets.clear()
+    this.isDragging = false
+    this.onDragEndCallback = null
+    this.updateEntityPosition = null
+    this.dispatchOperations = null
+  }
+
+  private resetDragState(): void {
+    this.pivotPosition = null
+    this.entityOffsets.clear()
+    this.detachDragBehavior()
+  }
+
   private setupSceneObservers(): void {
     this.scene.onPointerDown = (_event, pickResult) => {
-      if (!pickResult.pickedMesh || this.selectedEntities.length === 0) return
-      const clickedEntity = this.findClickedEntity(pickResult.pickedMesh)
+      if (!this.canStartDrag(pickResult)) return
+
+      const clickedEntity = this.findClickedEntity(pickResult.pickedMesh!)
       if (!clickedEntity) return
-      this.startDrag(clickedEntity, pickResult.pickedMesh)
+
+      this.startDrag(clickedEntity, pickResult.pickedMesh!)
     }
+
     this.scene.onPointerUp = () => {
       if (this.isDragging) {
         this.endDrag()
@@ -94,26 +160,21 @@ export class FreeGizmo implements IGizmoTransformer {
     this.scene.onPointerUp = () => {}
   }
 
+  private canStartDrag(pickResult: any): boolean {
+    return pickResult.pickedMesh && this.selectedEntities.length > 0
+  }
+
   private setupDragObservers(): void {
     this.dragStartObserver = this.dragBehavior.onDragStartObservable.add(() => {
       this.isDragging = true
     })
 
     this.dragObserver = this.dragBehavior.onDragObservable.add((eventData) => {
-      if (!this.isDragging || !eventData.delta || !this.pivotPosition || !this.lastSnappedPivotPosition) return
-
-      this.updatePivotPosition(eventData.delta)
-
-      if (this.shouldMoveEntities()) {
-        this.moveEntitiesToPivot()
-      }
+      this.handleDrag(eventData)
     })
 
     this.dragEndObserver = this.dragBehavior.onDragEndObservable.add(() => {
-      this.isDragging = false
-      this.detachDragBehavior()
-      this.dispatchOperations?.()
-      this.onDragEndCallback?.()
+      this.handleDragEnd()
     })
   }
 
@@ -137,13 +198,31 @@ export class FreeGizmo implements IGizmoTransformer {
   private detachDragBehavior(): void {
     this.dragBehavior.detach()
   }
+
+  private handleDrag(eventData: any): void {
+    if (!this.isDragging || !eventData.delta || !this.pivotPosition || !this.lastSnappedPivotPosition) return
+
+    this.updatePivotPosition(eventData.delta)
+
+    if (this.shouldMoveEntities()) {
+      this.moveEntitiesToPivot()
+    }
+  }
+
+  private handleDragEnd(): void {
+    this.isDragging = false
+    this.detachDragBehavior()
+    this.updateGizmoIndicator()
+    this.dispatchOperations?.()
+    this.onDragEndCallback?.()
+  }
+
   private updatePivotPosition(delta: Vector3): void {
     const worldDelta = delta.clone()
     worldDelta.y = 0 // keep Y position unchanged for free gizmo
     this.pivotPosition!.addInPlace(worldDelta)
   }
 
-  // determines if entities should be moved based on snap distance
   private shouldMoveEntities(): boolean {
     if (this.snapDistance <= 0) return true
 
@@ -151,7 +230,6 @@ export class FreeGizmo implements IGizmoTransformer {
     return distanceFromLastSnapped >= this.snapDistance
   }
 
-  // moves all entities to the current pivot position with snapping applied
   private moveEntitiesToPivot(): void {
     const finalPivotPosition = this.getSnappedPivotPosition()
 
@@ -163,37 +241,51 @@ export class FreeGizmo implements IGizmoTransformer {
       this.applyWorldPositionToEntity(entity, newWorldPosition)
     }
 
-    if (this.updateEntityPosition) {
-      this.selectedEntities.forEach(this.updateEntityPosition)
-    }
+    this.updateEntityPosition && this.selectedEntities.forEach(this.updateEntityPosition)
+    this.updateGizmoIndicator()
   }
 
   private getSnappedPivotPosition(): Vector3 {
     if (this.snapDistance <= 0) return this.pivotPosition!
 
     const snappedPivotPosition = snapVector(this.pivotPosition!, this.snapDistance)
-    snappedPivotPosition.y = this.pivotPosition!.y // preserve Y position
+    snappedPivotPosition.y = this.pivotPosition!.y // Preserve Y position
     this.lastSnappedPivotPosition = snappedPivotPosition.clone()
 
     return snappedPivotPosition
   }
 
   private initializePivotPosition(): void {
-    this.pivotPosition = new Vector3()
-    for (const entity of this.selectedEntities) {
-      this.pivotPosition.addInPlace(entity.getAbsolutePosition())
-    }
-    this.pivotPosition.scaleInPlace(1 / this.selectedEntities.length)
+    this.pivotPosition = this.getCentroid()
     this.lastSnappedPivotPosition = this.pivotPosition.clone()
   }
 
-  // initializes entity offsets from the pivot position
   private initializeEntityOffsets(): void {
     this.entityOffsets.clear()
     for (const entity of this.selectedEntities) {
       const offset = entity.getAbsolutePosition().subtract(this.pivotPosition!)
       this.entityOffsets.set(entity.entityId, offset)
     }
+  }
+
+  private findClickedEntity(pickedMesh: AbstractMesh): EcsEntity | null {
+    for (const entity of this.selectedEntities) {
+      if (pickedMesh.isDescendantOf(entity)) {
+        return entity
+      }
+    }
+
+    return (
+      this.selectedEntities.find(
+        (entity) => entity.meshRenderer === pickedMesh || entity.gltfContainer === pickedMesh
+      ) || null
+    )
+  }
+
+  private startDrag(clickedEntity: EcsEntity, pickedMesh: AbstractMesh): void {
+    this.initializePivotPosition()
+    this.initializeEntityOffsets()
+    this.attachDragBehavior(clickedEntity, pickedMesh)
   }
 
   private attachDragBehavior(clickedEntity: EcsEntity, pickedMesh: AbstractMesh): void {
@@ -207,35 +299,22 @@ export class FreeGizmo implements IGizmoTransformer {
     } else if (clickedEntity.gltfContainer) {
       return clickedEntity.gltfContainer
     } else {
-      // fallback: find the first child mesh of the entity
       const childMeshes = clickedEntity.getChildMeshes()
       return childMeshes.length > 0 ? childMeshes[0] : pickedMesh
     }
   }
 
-  private findClickedEntity(pickedMesh: AbstractMesh): EcsEntity | null {
-    for (const entity of this.selectedEntities) {
-      if (pickedMesh.isDescendantOf(entity)) {
-        return entity
-      }
-    }
-
-    const meshEntity = this.selectedEntities.find((entity) => {
-      return entity.meshRenderer === pickedMesh || entity.gltfContainer === pickedMesh
-    })
-    if (meshEntity) return meshEntity
-
-    return null
-  }
-
-  private startDrag(clickedEntity: EcsEntity, pickedMesh: AbstractMesh): void {
-    this.initializePivotPosition()
-    this.initializeEntityOffsets()
-    this.attachDragBehavior(clickedEntity, pickedMesh)
+  private endDrag(): void {
+    this.isDragging = false
+    this.detachDragBehavior()
+    this.pivotPosition = null
+    this.entityOffsets.clear()
+    this.onDragEndCallback?.()
   }
 
   private applyWorldPositionToEntity(entity: EcsEntity, worldPosition: Vector3): void {
     const parent = entity.parent instanceof TransformNode ? entity.parent : null
+
     if (parent) {
       const parentWorldMatrix = parent.getWorldMatrix()
       const parentWorldMatrixInverse = parentWorldMatrix.invert()
@@ -245,6 +324,10 @@ export class FreeGizmo implements IGizmoTransformer {
       entity.position.copyFrom(worldPosition)
     }
 
+    this.updateEntityTransform(entity)
+  }
+
+  private updateEntityTransform(entity: EcsEntity): void {
     // Force immediate world matrix update
     entity.computeWorldMatrix(true)
 
@@ -253,7 +336,6 @@ export class FreeGizmo implements IGizmoTransformer {
       ;(entity as any).refreshBoundingInfo()
     }
 
-    // Update all child meshes to ensure proper synchronization
     if (typeof entity.getChildMeshes === 'function') {
       entity.getChildMeshes().forEach((mesh) => {
         if (typeof mesh.refreshBoundingInfo === 'function') {
@@ -277,38 +359,131 @@ export class FreeGizmo implements IGizmoTransformer {
     }
   }
 
-  private endDrag(): void {
-    this.isDragging = false
-    this.detachDragBehavior()
-    this.pivotPosition = null
-    this.entityOffsets.clear()
-    if (this.onDragEndCallback) {
-      this.onDragEndCallback()
+  private getCentroid(): Vector3 {
+    if (this.gizmoManager?.calculateCentroid) {
+      return this.gizmoManager.calculateCentroid()
+    }
+
+    if (this.selectedEntities.length === 0) return Vector3.Zero()
+
+    const centroid = new Vector3()
+    for (const entity of this.selectedEntities) {
+      centroid.addInPlace(entity.getAbsolutePosition())
+    }
+    return centroid.scale(1 / this.selectedEntities.length)
+  }
+
+  private createGizmoIndicator(): void {
+    if (this.gizmoIndicator) return
+
+    this.gizmoIndicator = MeshBuilder.CreateSphere('freeGizmoIndicator', { diameter: 0.4 }, this.scene)
+    this.gizmoIndicator.material = this.createIndicatorMaterial()
+    this.gizmoIndicator.renderingGroupId = 1
+    this.gizmoIndicator.alwaysSelectAsActiveMesh = true
+  }
+
+  private createIndicatorMaterial(): StandardMaterial {
+    const material = new StandardMaterial('freeGizmoMaterial', this.scene)
+    material.diffuseTexture = this.createThreeColorTexture()
+    material.alpha = 1.0
+    material.emissiveColor = new Color3(0.1, 0.1, 0.1)
+    return material
+  }
+
+  private createThreeColorTexture(): DynamicTexture {
+    const textureSize = 256
+    const dynamicTexture = new DynamicTexture('freeGizmoTexture', textureSize, this.scene)
+    const context = dynamicTexture.getContext()
+
+    context.fillStyle = '#000000'
+    context.fillRect(0, 0, textureSize, textureSize)
+
+    const sectionWidth = textureSize / 3
+
+    context.fillStyle = '#FF0000'
+    context.fillRect(0, 0, sectionWidth, textureSize)
+
+    context.fillStyle = '#00FF00'
+    context.fillRect(sectionWidth, 0, sectionWidth, textureSize)
+
+    context.fillStyle = '#0000FF'
+    context.fillRect(sectionWidth * 2, 0, sectionWidth, textureSize)
+
+    dynamicTexture.update()
+    return dynamicTexture
+  }
+
+  private updateGizmoIndicator(): void {
+    if (!this.gizmoIndicator || this.selectedEntities.length === 0) return
+
+    const center = this.getEntityCenter()
+    this.gizmoIndicator.position = center
+  }
+
+  private getEntityCenter(): Vector3 {
+    if (this.selectedEntities.length === 0) return Vector3.Zero()
+
+    if (this.selectedEntities.length === 1) {
+      const entity = this.selectedEntities[0]
+      const boundingInfo = this.getEntityBoundingInfo(entity)
+      if (boundingInfo) {
+        return boundingInfo.boundingBox.centerWorld
+      }
+    }
+
+    return this.calculateBoundingBoxCenter()
+  }
+
+  private calculateBoundingBoxCenter(): Vector3 {
+    let minX = Infinity,
+      minY = Infinity,
+      minZ = Infinity
+    let maxX = -Infinity,
+      maxY = -Infinity,
+      maxZ = -Infinity
+
+    for (const entity of this.selectedEntities) {
+      const boundingInfo = this.getEntityBoundingInfo(entity)
+      if (boundingInfo) {
+        const box = boundingInfo.boundingBox
+        minX = Math.min(minX, box.minimumWorld.x)
+        minY = Math.min(minY, box.minimumWorld.y)
+        minZ = Math.min(minZ, box.minimumWorld.z)
+        maxX = Math.max(maxX, box.maximumWorld.x)
+        maxY = Math.max(maxY, box.maximumWorld.y)
+        maxZ = Math.max(maxZ, box.maximumWorld.z)
+      }
+    }
+
+    return new Vector3((minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2)
+  }
+
+  private getEntityBoundingInfo(entity: EcsEntity): any {
+    if ((entity as any).getBoundingInfo) {
+      return (entity as any).getBoundingInfo()
+    }
+
+    if (entity.meshRenderer && (entity.meshRenderer as any).getBoundingInfo) {
+      return (entity.meshRenderer as any).getBoundingInfo()
+    }
+
+    if (entity.gltfContainer && (entity.gltfContainer as any).getBoundingInfo) {
+      return (entity.gltfContainer as any).getBoundingInfo()
+    }
+
+    return {
+      boundingBox: {
+        centerWorld: entity.getAbsolutePosition(),
+        minimumWorld: entity.getAbsolutePosition().subtract(new Vector3(0.5, 0.5, 0.5)),
+        maximumWorld: entity.getAbsolutePosition().add(new Vector3(0.5, 0.5, 0.5))
+      }
     }
   }
 
-  onDragStart(entities: EcsEntity[], _gizmoNode: TransformNode): void {
-    this.selectedEntities = entities
-    this.pivotPosition = null
-    this.entityOffsets.clear()
-    this.detachDragBehavior()
-  }
-
-  update(entities: EcsEntity[], _gizmoNode: TransformNode): void {
-    if (entities !== this.selectedEntities) {
-      this.selectedEntities = entities
-      this.pivotPosition = null
-      this.entityOffsets.clear()
-      this.detachDragBehavior()
+  private removeGizmoIndicator(): void {
+    if (this.gizmoIndicator) {
+      this.gizmoIndicator.dispose()
+      this.gizmoIndicator = null
     }
-  }
-
-  onDragEnd(): void {
-    this.cleanup()
-  }
-
-  dispose(): void {
-    this.cleanup()
-    this.utilityLayer.dispose()
   }
 }
