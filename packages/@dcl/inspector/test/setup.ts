2e408e2ca0d34ffb3b2c0a9f4c4edb644f99f707
Nicolas Echezarreta <nicoecheza@gmail.com>
2025-07-21 11:16:49 -0300

    fix tests

--- packages/@dcl/inspector/test/setup.ts
+++ packages/@dcl/inspector/test/setup.ts
@@ -1,3 +1,18 @@
 import { TextEncoder, TextDecoder } from 'util'
 
 Object.assign(global, { TextDecoder, TextEncoder })
+
+// Mock DynamicTexture to prevent canvas context errors in tests
+jest.mock('@babylonjs/core', () => {
+  const originalModule = jest.requireActual('@babylonjs/core')
+  return {
+    ...originalModule,
+    DynamicTexture: jest.fn().mockImplementation(() => ({
+      getContext: jest.fn().mockReturnValue({
+        fillStyle: '#000000',
+        fillRect: jest.fn()
+      }),
+      update: jest.fn()
+    }))
+  }
+})
