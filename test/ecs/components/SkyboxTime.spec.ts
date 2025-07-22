bd910a5b4d45fd8db62e74a4821b656fbb711d74
Pravus <pravusjif@gmail.com>
2025-07-22 04:27:51 +0200

    added skyboxtime test

---
+++ test/ecs/components/SkyboxTime.spec.ts
@@ -0,0 +1,13 @@
+import { engine, components, TransitionMode } from '../../../packages/@dcl/ecs/src'
+import { testComponentSerialization } from './assertion'
+
+describe('Generated SkyboxTime ProtoBuf', () => {
+  it('should serialize/deserialize SkyboxTime', () => {
+    const skyboxTime = components.SkyboxTime(engine)
+
+    testComponentSerialization(skyboxTime, {
+      fixedTime: 0,
+      transitionMode: TransitionMode.TM_FORWARD
+    })
+  })
+})
