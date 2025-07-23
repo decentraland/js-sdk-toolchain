8a0180bd821eb14b540f8bf021fb78b2f8002869
gon boedo <gonzalo@decentraland.org>
2025-07-22 23:04:28 -0300

    wip

--- packages/@dcl/sdk/src/network/message-bus-sync.ts
+++ packages/@dcl/sdk/src/network/message-bus-sync.ts
@@ -1,4 +1,4 @@
-import { IEngine, Transport, RealmInfo, PlayerIdentityData } from '@dcl/ecs'
+import { IEngine, Transport, RealmInfo, PlayerIdentityData, Entity, NetworkEntity, AppendValueOperation, CrdtMessage, CrdtMessageHeader, CrdtMessageProtocol, CrdtMessageType, DeleteComponent, DeleteComponentNetwork, DeleteEntity, DeleteEntityNetwork, PutComponentOperation, PutNetworkComponentOperation, DeleteComponentNetworkMessage, DeleteEntityNetworkMessage, PutNetworkComponentMessage } from '@dcl/ecs'
 import { type SendBinaryRequest, type SendBinaryResponse } from '~system/CommunicationsController'
 
 import { syncFilter } from './filter'
@@ -9,6 +9,10 @@ import { entityUtils } from './entities'
 import { GetUserDataRequest, GetUserDataResponse } from '~system/UserIdentity'
 import { definePlayerHelper } from '../players'
 import { serializeCrdtMessages } from '../internal/transports/logger'
+import { isServer as isServerFn } from '~system/EngineApi'
+import { Atom } from '../atom'
+import { ReadWriteByteBuffer } from '@dcl/ecs/dist/serialization/ByteBuffer'
+
 
 export type IProfile = { networkId: number; userId: string }
 // user that we asked for the inital crdt state
@@ -17,10 +21,15 @@ export function addSyncTransport(
   sendBinary: (msg: SendBinaryRequest) => Promise<SendBinaryResponse>,
   getUserData: (value: GetUserDataRequest) => Promise<GetUserDataResponse>
 ) {
+  const AUTH_SERVER_PEER_ID = 'authorative-server'
   const DEBUG_NETWORK_MESSAGES = () => (globalThis as any).DEBUG_NETWORK_MESSAGES ?? false
+  const CLIENT_SERVER_ARCH = () => (globalThis as any).CLIENT_SERVER_ARCH ?? true
   // Profile Info
   const myProfile: IProfile = {} as IProfile
   fetchProfile(myProfile!, getUserData)
+
+  const isServerAtom = Atom<boolean>()
+  isServerFn({}).then($ => isServerAtom.swap(!!$.isServer))
 
   // Entity utils
   const entityDefinitions = entityUtils(engine, myProfile)
@@ -73,7 +82,8 @@ export function addSyncTransport(
   // End add sync transport
 
   // Receive & Process CRDT_STATE
-  binaryMessageBus.on(CommsMessage.RES_CRDT_STATE, (value) => {
+  binaryMessageBus.on(CommsMessage.RES_CRDT_STATE, async (value, fromPeer) => {
+    if (isServerAtom.getOrNull() || fromPeer !== AUTH_SERVER_PEER_ID) return
     const { sender, data } = decodeCRDTState(value)
     if (sender !== myProfile.userId) return
     DEBUG_NETWORK_MESSAGES() && console.log('[Processing CRDT State]', data.byteLength / 1024, 'KB')
@@ -83,6 +93,7 @@ export function addSyncTransport(
 
   // Answer to REQ_CRDT_STATE
   binaryMessageBus.on(CommsMessage.REQ_CRDT_STATE, async (_, userId) => {
+    if (await isServerAtom) return
     DEBUG_NETWORK_MESSAGES() && console.log(`Sending CRDT State to: ${userId}`)
 
     for (const chunk of engineToCrdt(engine)) {
@@ -91,13 +102,65 @@ export function addSyncTransport(
   })
 
   // Process CRDT messages here
-  binaryMessageBus.on(CommsMessage.CRDT, (value) => {
+  binaryMessageBus.on(CommsMessage.CRDT_SERVER, (value, sender) => {
+    const isServer = isServerAtom.getOrNull()
+    if (isServer || sender !== AUTH_SERVER_PEER_ID) return
+    
+    for (const message of readMessages(value)) {
+    }
+  })
+
+  function readMessages(data: Uint8Array) {
+    const buffer = new ReadWriteByteBuffer(data)
+    const messages: ((PutNetworkComponentMessage | DeleteComponentNetworkMessage | DeleteEntityNetworkMessage) & { messageBuffer: Uint8Array })[] = []
+    let header: CrdtMessageHeader | null
+    while ((header = CrdtMessageProtocol.getHeader(buffer))) {
+      const offset = buffer.currentReadOffset()
+      let message: CrdtMessage | undefined = undefined
+      if (header.type === CrdtMessageType.DELETE_COMPONENT_NETWORK) {
+        message = DeleteComponentNetwork.read(buffer)!
+      } else if (header.type === CrdtMessageType.PUT_COMPONENT_NETWORK) {
+        message = PutNetworkComponentOperation.read(buffer)!
+      } else if (header.type === CrdtMessageType.DELETE_ENTITY_NETWORK) {
+        message = DeleteEntityNetwork.read(buffer)!
+      } else {
+        // consume the message
+        buffer.incrementReadOffset(header.length)
+      }
+      if (message) {
+        messages.push({
+          ...message,
+          messageBuffer: buffer.buffer().subarray(offset, buffer.currentReadOffset())
+        })
+      }
+    }
+    return messages
+  }
+
+  binaryMessageBus.on(CommsMessage.CRDT, (value, sender) => {
     DEBUG_NETWORK_MESSAGES() &&
       console.log(Array.from(serializeCrdtMessages('[NetworkMessage received]:', value, engine)))
-    transport.onmessage!(value)
+    const isServer = isServerAtom.getOrNull()
+    if (isServer) {
+      // 1. read each message
+      // 2. if there is no network entity, create the entity
+      // 3. get the component and run the validation
+      // 4. if its a DELETE_ENTITY check if that user has permissions to delete sync: 'all' | 'owner'  | 'server' ?
+      // 5. if its an update component check if it has permissions, and also run the custom validation. component.runValidation(entity, componenet.getOrNull(entity), value, address)
+      //    5.1 if this validations returns true, then broadcast that message to every peer.
+      //    5.2 if its invalid, then ignore, or send the correct CRDT to the peer.
+      // we need to run validations and broadcast the message if its valid.
+    } else {
+      if (sender !== AUTH_SERVER_PEER_ID) return
+      transport.onmessage!(value)
+    }
   })
 
   async function requestState(retryCount: number = 1) {
+    if (CLIENT_SERVER_ARCH()) {
+      return
+    }
+
     let players = Array.from(engine.getEntitiesWith(PlayerIdentityData))
     DEBUG_NETWORK_MESSAGES() && console.log(`Requesting state. Players connected: ${players.length - 1}`)
 
@@ -127,6 +190,10 @@ export function addSyncTransport(
 
   players.onEnterScene((player) => {
     DEBUG_NETWORK_MESSAGES() && console.log('[onEnterScene]', player.userId)
+    if (!isServerAtom.getOrNull()) return
+    for (const chunk of engineToCrdt(engine)) {
+      binaryMessageBus.emit(CommsMessage.RES_CRDT_STATE, encodeCRDTState(player.userId, chunk), [player.userId])
+    }
   })
 
   // Asks for the REQ_CRDT_STATE when its connected to comms
