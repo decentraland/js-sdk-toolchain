b188398e91bcc7ecc94d82ef84e998572f6c08f2
gon boedo <gonzalo@decentraland.org>
2025-07-24 02:32:56 -0300

    update snapshots
        pick f9ee3fde go back to req_crt_statate cause unity takes more time to connect to livekit

--- packages/@dcl/ecs/src/components/index.ts
+++ packages/@dcl/ecs/src/components/index.ts
@@ -104,7 +104,6 @@ export const SyncComponents: (
 /**
  * @alpha
  */
-/* @__PURE__ */
 export const NetworkEntity: (
   engine: Pick<IEngine, 'defineComponent'>
 ) => LastWriteWinElementSetComponentDefinition<INetowrkEntityType> = (engine) => defineNetworkEntity(engine)
@@ -112,7 +111,6 @@ export const NetworkEntity: (
 /**
  * @alpha
  */
-/* @__PURE__ */
 export const NetworkParent: (
   engine: Pick<IEngine, 'defineComponent'>
 ) => LastWriteWinElementSetComponentDefinition<INetowrkParentType> = (engine) => defineNetworkParent(engine)
