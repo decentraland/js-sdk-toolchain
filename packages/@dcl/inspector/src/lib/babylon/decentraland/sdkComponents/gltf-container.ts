6ded5fd315fdd81ba477d2ba699e9f06cd208ff8
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-07-24 17:44:24 -0300

    fix: Loading spinner animatino when importing assets to the scene

--- packages/@dcl/inspector/src/lib/babylon/decentraland/sdkComponents/gltf-container.ts
+++ packages/@dcl/inspector/src/lib/babylon/decentraland/sdkComponents/gltf-container.ts
@@ -452,7 +452,7 @@ function createLoadingSpinner(
     { frame: 100, value: CIRCLE }
   ])
 
-  const animationGroup = new BABYLON.AnimationGroup('loadingSpinnerAnimation')
+  const animationGroup = new BABYLON.AnimationGroup('loadingSpinnerAnimation', scene)
   animationGroup.addTargetedAnimation(rotationAnimation, semiTorus)
   animationGroup.play(true)
 
