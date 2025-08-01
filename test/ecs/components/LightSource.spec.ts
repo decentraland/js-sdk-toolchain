7ee9efedeb245f7d5a2c4811d98fd8e35287c948
Pravus <pravusjif@gmail.com>
2025-08-01 14:39:31 +0200

    ran 'make lint-fix'

--- test/ecs/components/LightSource.spec.ts
+++ test/ecs/components/LightSource.spec.ts
@@ -1,7 +1,4 @@
-import {
-  Engine,
-  components,
-} from '../../../packages/@dcl/ecs/src'
+import { Engine, components } from '../../../packages/@dcl/ecs/src'
 import { testComponentSerialization } from './assertion'
 
 describe('Generated LightSource ProtoBuf', () => {
@@ -9,41 +6,35 @@ describe('Generated LightSource ProtoBuf', () => {
     const newEngine = Engine()
     const LightSource = components.LightSource(newEngine)
 
-    testComponentSerialization(
-      LightSource,
-      {
-        active: true,
-        color: { r: 1, g: 1, b: 1 },
-        intensity: 1,
-        range: 10,
-        shadowMaskTexture: undefined,
-        shadow: true,
-        type: { $case: 'point', point: { } }
-      }
-    )
+    testComponentSerialization(LightSource, {
+      active: true,
+      color: { r: 1, g: 1, b: 1 },
+      intensity: 1,
+      range: 10,
+      shadowMaskTexture: undefined,
+      shadow: true,
+      type: { $case: 'point', point: {} }
+    })
   })
 
   it('should serialize/deserialize Spot LightSource', () => {
     const newEngine = Engine()
     const LightSource = components.LightSource(newEngine)
 
-    testComponentSerialization(
-      LightSource,
-      {
-        active: false,
-        color: { r: 1, g: 1, b: 1 },
-        intensity: 1,
-        range: 10,
-        shadowMaskTexture: undefined,
-        shadow: true,
-        type: {
-          $case: 'spot',
-          spot: {
-            innerAngle: 21.8,
-            outerAngle: 30,
-          }
+    testComponentSerialization(LightSource, {
+      active: false,
+      color: { r: 1, g: 1, b: 1 },
+      intensity: 1,
+      range: 10,
+      shadowMaskTexture: undefined,
+      shadow: true,
+      type: {
+        $case: 'spot',
+        spot: {
+          innerAngle: 21.8,
+          outerAngle: 30
         }
       }
-    )
+    })
   })
 })
