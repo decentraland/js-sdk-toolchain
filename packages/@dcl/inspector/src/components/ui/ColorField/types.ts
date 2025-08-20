aad4cd2309ac39feba7a52d6b71c9fe688da7d6b
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-08-20 09:53:16 -0300

    feat: Update ColorField component to handle a new basic version

--- packages/@dcl/inspector/src/components/ui/ColorField/types.ts
+++ packages/@dcl/inspector/src/components/ui/ColorField/types.ts
@@ -2,4 +2,14 @@ import React from 'react'
 
 export type Props = React.InputHTMLAttributes<HTMLElement> & {
   label?: React.ReactNode
+  basic?: boolean
+}
+
+export type ColorOptions = {
+  label: string
+  value?: React.InputHTMLAttributes<HTMLElement>['value']
+  leftContent?: React.ReactNode
+  secondaryText?: string
+  selected?: boolean
+  disabled?: boolean
 }
