15950d214c000129db8b18671d9a38cc5ab6ef57
alejandralevy <alelevy15@gmail.com>
2025-10-03 19:57:48 +0200

    feat: update git ignore and postinstall logs

--- packages/@dcl/sdk-commands/scripts/postinstall.js
+++ packages/@dcl/sdk-commands/scripts/postinstall.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
 if (process.cwd().includes('node_modules')) {
-  const path = require('path')
   const { exec } = require('child_process')
 
-  const scenePath = path.resolve(process.cwd(), '../../..')
-
+  //exec script on the root scene
+  const scenePath = process.cwd().split('/node_modules')[0]
   exec('npx @dcl/sdk-commands get-context-files', { cwd: scenePath }, (e, stdout, stderr) => {
     if (e) {
-      console.log('stderr:', stderr)
+      console.log('Error: not able to execute get-context-files', stderr)
     } else {
       console.log(stdout)
     }
   })
 } else {
-  console.log('Not executing postinstall')
+  console.log('Not executing postinstall from this directory')
 }
