88215febd9612f4bccca501c460196729a12dbb5
daniele-dcl <daniele.marotta@decentraland.org>
2025-11-26 15:02:52 +0100

    feat: locomotion settings (#1238)
    * set locomotion settings protocol branch and rebuilt
    
    * serialization tests

---
+++ test/ecs/components/AvatarLocomotionSettings.spec.ts
@@ -0,0 +1,18 @@
+import { Engine, components } from '../../../packages/@dcl/ecs/src'
+import { testComponentSerialization } from './assertion'
+
+describe('Generated AvatarLocomotionSettings ProtoBuf', () => {
+  it('should serialize/deserialize AvatarLocomotionSettings', () => {
+    const newEngine = Engine()
+    const AvatarLocomotionSettings = components.AvatarLocomotionSettings(newEngine)
+
+    testComponentSerialization(AvatarLocomotionSettings, {
+      walkSpeed: 1,
+      jogSpeed: 2,
+      runSpeed: 3,
+      jumpHeight: 4,
+      runJumpHeight: 5,
+      hardLandingCooldown: 6
+    })
+  })
+})
