aa3ba733b41debb5178f7b6a91d4cb3ca7760c1d
Lean Mendoza <8042536+leanmendoza@users.noreply.github.com>
2026-01-05 11:32:28 -0300

    feat: add coords and print deeplink when --mobile (#1268)
    * feat: add coords and print deeplink when --mobile
    
    * typo

--- packages/@dcl/sdk-commands/src/commands/start/index.ts
+++ packages/@dcl/sdk-commands/src/commands/start/index.ts
@@ -264,11 +264,12 @@ export async function main(options: Options) {
       }
 
       if (options.args['--mobile'] && lanUrl) {
-        const deepLink = `decentraland://open?preview=${lanUrl}`
+        const deepLink = `decentraland://open?preview=${lanUrl}&position=${baseCoords.x}%2C${baseCoords.y}`;
         QRCode.toString(deepLink, { type: 'terminal', small: true }, (err, qr) => {
           if (!err) {
             components.logger.log(colors.bold('\nScan to preview on mobile: \n'))
             components.logger.log(qr)
+            components.logger.log(`This QR redirects to ${deepLink} in your phone.`)
           }
         })
       }
