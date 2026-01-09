2d3da9cef92d1e2aef5dd7774cc5ca9afe5635cc
gon boedo <gonzalo@decentraland.org>
2026-01-09 12:29:06 -0300

    do not merge

---
+++ packages/@dcl/ecs/src/serialization/crdt/boedoTest.ts
@@ -0,0 +1,1 @@
+export const boedo = 'casla'
--- packages/@dcl/sdk/src/network/index.ts
+++ packages/@dcl/sdk/src/network/index.ts
@@ -2,9 +2,11 @@ import { sendBinary } from '~system/CommunicationsController'
 import { engine } from '@dcl/ecs'
 import { addSyncTransport } from './message-bus-sync'
 import { getUserData } from '~system/UserIdentity'
+import { boedo } from '@dcl/ecs/dist/serialization/crdt/boedoTest'
 
 // initialize sync transport for sdk engine
 const { getChildren, syncEntity, parentEntity, getParent, myProfile, removeParent, getFirstChild, isStateSyncronized } =
   addSyncTransport(engine, sendBinary, getUserData)
 
 export { getFirstChild, getChildren, syncEntity, parentEntity, getParent, myProfile, removeParent, isStateSyncronized }
+export { boedo }
