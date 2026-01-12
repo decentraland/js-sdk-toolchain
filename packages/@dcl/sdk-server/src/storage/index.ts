6b6dccbe2d9e8a6ea32bc4da4d0feee55f2b7060
Gabriel Diaz <gabrieldiaz31@gmail.com>
2026-01-12 15:00:54 -0300

    feat: Add Storage.world interface to get/set/delete data from the server side storage

---
+++ packages/@dcl/sdk-server/src/storage/index.ts
@@ -0,0 +1,28 @@
+import { createWorldStorage, IWorldStorage } from './world'
+
+// Re-export interfaces
+export { IWorldStorage } from './world'
+
+/**
+ * Storage interface containing all storage scopes.
+ */
+export interface IStorage {
+  /** World-scoped storage for key-value pairs */
+  world: IWorldStorage
+}
+
+/**
+ * Creates the Storage module with all storage scopes.
+ */
+const createStorage = (): IStorage => {
+  return {
+    world: createWorldStorage()
+  }
+}
+
+/**
+ * Storage provides methods to store and retrieve key-value data from the
+ * Server Side Storage service. This module only works when running
+ * on server-side scenes.
+ */
+export const Storage: IStorage = createStorage()
