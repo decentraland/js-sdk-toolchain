aba484d49fcc4174ff7609b92e4599e2079bb730
Gabriel Diaz <gabrieldiaz31@gmail.com>
2026-01-20 17:33:10 -0300

    refactor: Move sdk-server package implementation into sdk/server

--- scripts/build.spec.ts
+++ scripts/build.spec.ts
@@ -11,8 +11,7 @@ import {
   PLAYGROUND_ASSETS_PATH,
   REACT_ECS,
   SDK_PATH,
-  SDK_COMMANDS_PATH,
-  SDK_SERVER_PATH
+  SDK_COMMANDS_PATH
 } from './common'
 import {
   copyFile,
@@ -57,16 +56,6 @@ flow('build-all', () => {
     it('check file exists', () => {
       ensureFileExists('apis.d.ts', JS_RUNTIME)
       ensureFileExists('index.d.ts', JS_RUNTIME)
-    })
-  })
-
-  flow('@dcl/sdk-server build', () => {
-    itExecutes('npm i --silent', SDK_SERVER_PATH)
-    itExecutes(`npm install --silent ${JS_RUNTIME}`, SDK_SERVER_PATH)
-    itExecutes('npm run build --silent', SDK_SERVER_PATH)
-    it('check file exists', () => {
-      ensureFileExists('dist/index.js', SDK_SERVER_PATH)
-      ensureFileExists('dist/index.d.ts', SDK_SERVER_PATH)
     })
   })
 
