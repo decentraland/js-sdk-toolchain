00fe8718691e9139e5000724933a85f81f30218b
Vitaly Popuzin <popuzin@gmail.com>
2026-02-26 14:49:40 +0100

    made helpers for vector operations

--- packages/@dcl/ecs/src/runtime/helpers/index.ts
+++ packages/@dcl/ecs/src/runtime/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './coordinates'
+export * from './math'
 export * from './tree'
 export { createTimers, Timers, TimerId, TimerCallback } from './timers'
--- packages/@dcl/ecs/src/runtime/helpers/math.ts
+++ packages/@dcl/ecs/src/runtime/helpers/math.ts
@@ -1,28 +1,28 @@
-import { Vector3Type } from '../schemas/custom/Vector3'
+import { Vector3Type } from '../../schemas'
 
-/** @internal */
+/** @public */
 export function isZeroVector(v: Vector3Type): boolean {
   return v.x === 0 && v.y === 0 && v.z === 0
 }
 
-/** @internal */
+/** @public */
 export function normalizeVector(v: Vector3Type): Vector3Type {
   const len = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
   if (len === 0) return { x: 0, y: 0, z: 0 }
   return { x: v.x / len, y: v.y / len, z: v.z / len }
 }
 
-/** @internal */
+/** @public */
 export function scaleVector(v: Vector3Type, s: number): Vector3Type {
   return { x: v.x * s, y: v.y * s, z: v.z * s }
 }
 
-/** @internal */
+/** @public */
 export function addVectors(a: Vector3Type, b: Vector3Type): Vector3Type {
   return { x: a.x + b.x, y: a.y + b.y, z: a.z + b.z }
 }
 
-/** @internal */
+/** @public */
 export function vectorsEqual(a: Vector3Type, b: Vector3Type): boolean {
   return a.x === b.x && a.y === b.y && a.z === b.z
 }
