f858f905dbd907f8dc9b1da88fe0591eb3b105fa
Pravus <pravusjif@gmail.com>
2026-08-12 16:45:00 +0200

    feat: audioEventSystem + avatarEmoteCommand.state (#1530)

--- test/ecs/components/AvatarEmoteCommand.spec.ts
+++ test/ecs/components/AvatarEmoteCommand.spec.ts
@@ -1,4 +1,4 @@
-﻿import { AvatarMask, Engine, components } from '../../../packages/@dcl/ecs/src'
+﻿import { AvatarMask, EmoteState, Engine, components } from '../../../packages/@dcl/ecs/src'
 import { testSchemaSerializationIdentity } from './assertion'
 
 describe('Generated AvatarEmoteCommand ProtoBuf', () => {
@@ -10,13 +10,36 @@ describe('Generated AvatarEmoteCommand ProtoBuf', () => {
       emoteUrn: 'boedo',
       loop: false,
       timestamp: 1,
-      mask: undefined
+      mask: undefined,
+      state: undefined
     })
     testSchemaSerializationIdentity(AvatarEmoteCommand.schema, {
       emoteUrn: 'boedo',
       loop: false,
       timestamp: 1,
-      mask: AvatarMask.AM_UPPER_BODY
+      mask: AvatarMask.AM_UPPER_BODY,
+      state: undefined
+    })
+    testSchemaSerializationIdentity(AvatarEmoteCommand.schema, {
+      emoteUrn: 'boedo',
+      loop: false,
+      timestamp: 1,
+      mask: AvatarMask.AM_UPPER_BODY,
+      state: EmoteState.ES_STARTED
+    })
+    testSchemaSerializationIdentity(AvatarEmoteCommand.schema, {
+      emoteUrn: 'boedo',
+      loop: false,
+      timestamp: 1,
+      mask: undefined,
+      state: EmoteState.ES_FINISHED
+    })
+    testSchemaSerializationIdentity(AvatarEmoteCommand.schema, {
+      emoteUrn: 'boedo',
+      loop: false,
+      timestamp: 1,
+      mask: undefined,
+      state: EmoteState.ES_INTERRUPTED
     })
   })
 })
