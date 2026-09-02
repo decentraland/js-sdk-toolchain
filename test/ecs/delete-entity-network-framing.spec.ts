efc5a300703bc17a5d55e9c7f277d2fcccbe4b60
LautaroPetaccio <lausuper@gmail.com>
2026-09-01 22:31:28 -0300

    test: cover the read paths of the network entity delete
    The first spec exercised writing and framing but left read's two guards
    untested: nothing left to read, and a message of another type.

--- test/ecs/delete-entity-network-framing.spec.ts
+++ test/ecs/delete-entity-network-framing.spec.ts
@@ -1,6 +1,7 @@
 import { Entity } from '../../packages/@dcl/ecs/src/engine'
 import { ReadWriteByteBuffer } from '../../packages/@dcl/ecs/src/serialization/ByteBuffer'
 import { CrdtMessageProtocol } from '../../packages/@dcl/ecs/src/serialization/crdt'
+import { DeleteEntity } from '../../packages/@dcl/ecs/src/serialization/crdt/deleteEntity'
 import { DeleteEntityNetwork } from '../../packages/@dcl/ecs/src/serialization/crdt/network/deleteEntityNetwork'
 import { CrdtMessageType } from '../../packages/@dcl/ecs/src/serialization/crdt/types'
 
@@ -32,5 +33,30 @@ describe('when a network entity delete is written', () => {
     const message = DeleteEntityNetwork.read(buffer)
 
     expect(message).toMatchObject({ entityId: ENTITY, networkId: NETWORK_ID })
+  })
+})
+
+describe('when there is no message left to read', () => {
+  let buffer: ReadWriteByteBuffer
+
+  beforeEach(() => {
+    buffer = new ReadWriteByteBuffer()
+  })
+
+  it('should return nothing rather than read past the end', () => {
+    expect(DeleteEntityNetwork.read(buffer)).toBeNull()
+  })
+})
+
+describe('when the next message is of another type', () => {
+  let buffer: ReadWriteByteBuffer
+
+  beforeEach(() => {
+    buffer = new ReadWriteByteBuffer()
+    DeleteEntity.write(ENTITY, buffer)
+  })
+
+  it('should refuse to read it as a network delete', () => {
+    expect(() => DeleteEntityNetwork.read(buffer)).toThrow('tried to read another message type')
   })
 })
